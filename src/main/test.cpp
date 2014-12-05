bc64a2afbd0896b0917aadbc0730e3b5125a5c89
Graydon Hoare <graydon@stellar.org>
2014-12-04 20:07:58 -0800

    Add test that makes 2 applications connected together.

--- src/main/test.cpp
+++ src/main/test.cpp
@@ -1,9 +1,63 @@
+#include "main/Application.h"
+#include "util/StellardVersion.h"
+#include "lib/util/Logging.h"
+#include "util/make_unique.h"
+#include <time.h>
+
 namespace stellar
 {
+
+
+typedef std::unique_ptr<Application> appPtr;
+
+bool
+allStopped(std::vector<appPtr>& apps)
+{
+    for (appPtr &app : apps) {
+        if (!app->getMainIOService().stopped()) {
+            return false;
+        }
+    }
+    return true;
+}
+
+void testHelloGoodbye(Config const& cfg){
+
+    std::vector<appPtr> apps;
+    apps.emplace_back(make_unique<Application>(cfg));
+    apps.emplace_back(make_unique<Application>(cfg));
+
+    LoopbackPeer::connectApplications(*apps[0], *apps[1]);
+
+    while (!allStopped(apps)) {
+        for (appPtr &app : apps) {
+            app->getMainIOService().poll_one();
+            app->getWorkerIOService().poll_one();
+        }
+    }
+}
 
 int
 test()
 {
+    std::ostringstream oss;
+    oss << "stellard-test-" << getpid() << "-" << time(nullptr) << ".log";
+    Config cfg;
+    cfg.LOG_FILE_PATH = oss.str();
+
+    // Tests are run in standalone and single-step mode by default, meaning that
+    // no external listening interfaces are opened (all sockets must be manually
+    // created and connected loopback sockets), no external connections are
+    // attempted, and the event loops must be manually cranked.
+    cfg.RUN_STANDALONE = true;
+    cfg.SINGLE_STEP_MODE = true;
+
+    Logging::setUpLogging(cfg.LOG_FILE_PATH);
+    LOG(INFO) << "Testing stellard-hayashi " << STELLARD_VERSION;
+    LOG(INFO) << "Logging to " << cfg.LOG_FILE_PATH;
+
+    testHelloGoodbye(cfg);
+
     return 0;
 }
 
