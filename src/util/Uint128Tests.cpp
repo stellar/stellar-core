c7a99fef83e9b4252fbd901208b69ae6e416d650
Graydon Hoare <graydon@stellar.org>
2015-01-08 19:43:29 -0800

    Add missing bits of uint128_t, plus random cross-check test.

---
+++ src/util/Uint128Tests.cpp
@@ -0,0 +1,82 @@
+// Copyright 2015 Stellar Development Foundation and contributors. Licensed
+// under the ISC License. See the COPYING file at the top-level directory of
+// this distribution or at http://opensource.org/licenses/ISC
+
+#include "autocheck/autocheck.hpp"
+#include "main/test.h"
+#include "lib/catch.hpp"
+#include "util/Logging.h"
+#include "uint128_t.h"
+#include <limits>
+#include <ostream>
+
+// This file just cross-checks a selection of operators in the uint128_t class
+// against the values produced by native (compiler-provided) __int128 types,
+// when applied to random values. It's to help convince us that the class
+// is implemented correctly.
+
+#if defined(__SIZEOF_INT128__) || defined(_GLIBCXX_USE_INT128)
+
+const uint64_t full = std::numeric_limits<uint64_t>::max();
+
+uint128_t
+fromNative(unsigned __int128 x)
+{
+    return uint128_t(x >> 64, x & full);
+}
+
+unsigned __int128
+toNative(uint128_t x)
+{
+    return ((unsigned __int128)x.upper()) << 64 | x.lower();
+}
+
+struct gen128
+{
+    typedef unsigned __int128 result_type;
+    result_type operator() (size_t size = 0) {
+        std::uniform_int_distribution<uint64_t> dist(1, full);
+        auto a = dist(autocheck::rng());
+        auto b = dist(autocheck::rng());
+        return (((result_type)a) << 64) | b;
+    }
+};
+
+namespace std
+{
+std::ostream&
+operator<<(std::ostream& out, unsigned __int128 const& x)
+{
+    return out << std::hex << "0x" << uint64_t(x >> 64) << uint64_t(x);
+}
+}
+
+TEST_CASE("uint128_t", "[uint128]")
+{
+    auto arb = autocheck::make_arbitrary(gen128(), gen128());
+    autocheck::check<unsigned __int128, unsigned __int128>(
+        [](unsigned __int128 x, unsigned __int128 y)
+        {
+            bool b = true;
+#define BINOP(op) (b = b && ((x op y) == toNative(fromNative(x) op fromNative(y))));
+            BINOP(+);
+            BINOP(-);
+            BINOP(*);
+            BINOP(/);
+            BINOP(^);
+            BINOP(&);
+            BINOP(|);
+            BINOP(%);
+            BINOP(<);
+            BINOP(<=);
+            BINOP(==);
+            BINOP(!=);
+            BINOP(>=);
+            BINOP(>);
+            BINOP(||);
+            BINOP(&&);
+            return b;
+        }, 100000, arb);
+}
+
+#endif
