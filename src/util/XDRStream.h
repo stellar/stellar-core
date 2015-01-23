b626a7d7e206ee80b2d71f3dd8f74a2caff22063
Graydon Hoare <graydon@stellar.org>
2015-01-23 15:23:57 -0800

    Add helper streams for incremental XDR file I/O.

---
+++ src/util/XDRStream.h
@@ -0,0 +1,140 @@
+#pragma once
+
+// Copyright 2015 Stellar Development Foundation and contributors. Licensed
+// under the ISC License. See the COPYING file at the top-level directory of
+// this distribution or at http://opensource.org/licenses/ISC
+
+#include <string>
+#include <fstream>
+#include <vector>
+#include "xdrpp/marshal.h"
+
+namespace stellar
+{
+
+/**
+ * Helper for loading a sequence of XDR objects from a file one at a time,
+ * rather than all at once.
+ */
+class
+XDRInputFileStream
+{
+    std::ifstream mIn;
+    std::vector<char> mBuf;
+public:
+
+    void
+    close()
+    {
+        mIn.close();
+    }
+
+    void
+    open(std::string const& filename)
+    {
+        mIn.open(filename, std::ifstream::binary);
+        if (!mIn)
+        {
+            std::string msg("failed to open XDR file: ");
+            throw std::runtime_error(msg + filename);
+        }
+    }
+
+    operator bool() const
+    {
+        return mIn.good();
+    }
+
+    template <typename T> bool
+    readOne(T& out)
+    {
+        char szBuf[4];
+        if (!mIn.read(szBuf, 4))
+        {
+            return false;
+        }
+
+        // Read 4 bytes of size, big-endian, with XDR 'continuation' bit cleared
+        // (high bit of high byte).
+        uint32_t sz = 0;
+        sz |= static_cast<uint8_t>(szBuf[0] & '\x7f'); sz <<= 8;
+        sz |= static_cast<uint8_t>(szBuf[1]); sz <<= 8;
+        sz |= static_cast<uint8_t>(szBuf[2]); sz <<= 8;
+        sz |= static_cast<uint8_t>(szBuf[3]);
+
+        if (sz > mBuf.size())
+        {
+            mBuf.resize(sz);
+        }
+        if (!mIn.read(mBuf.data(), sz))
+        {
+            throw std::runtime_error("malformed XDR file");
+        }
+        xdr::xdr_get g(mBuf.data(), mBuf.data() + sz);
+        xdr::xdr_argpack_archive(g, out);
+        return true;
+    }
+};
+
+class
+XDROutputFileStream
+{
+    std::ofstream mOut;
+    std::vector<char> mBuf;
+public:
+
+    void
+    close()
+    {
+        mOut.close();
+    }
+
+    void
+    open(std::string const& filename)
+    {
+        mOut.open(filename,
+                  std::ofstream::binary|
+                  std::ofstream::trunc);
+        if (!mOut)
+        {
+            std::string msg("failed to open XDR file: ");
+            throw std::runtime_error(msg + filename);
+        }
+    }
+
+    operator bool() const
+    {
+        return mOut.good();
+    }
+
+    template <typename T> bool
+    writeOne(T const& t)
+    {
+        uint32_t sz = xdr::xdr_size(t);
+        assert(sz < 0x80000000);
+
+        if (mBuf.size() < sz + 4)
+        {
+            mBuf.resize(sz + 4);
+        }
+
+        // Write 4 bytes of size, big-endian, with XDR 'continuation' bit set on
+        // high bit of high byte.
+        mBuf[0] = static_cast<char>(sz >> 24) | '\x80';
+        mBuf[1] = static_cast<char>(sz >> 16);
+        mBuf[2] = static_cast<char>(sz >> 8);
+        mBuf[3] = static_cast<char>(sz);
+
+        xdr::xdr_put p(mBuf.data() + 4, mBuf.data() + 4 + sz);
+        xdr_argpack_archive(p, t);
+
+        if (!mOut.write(mBuf.data(), sz + 4))
+        {
+            return false;
+        }
+        return true;
+    }
+};
+
+
+}
