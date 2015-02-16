8ba4e681cd3c10c3d3d2f4d3d6e24f27ed4ad87f
Graydon Hoare <graydon@stellar.org>
2015-02-15 18:53:40 -0800

    Add ledger/LedgerTests.cpp

---
+++ src/ledger/LedgerTests.cpp
@@ -0,0 +1,17 @@
+// Copyright 2015 Stellar Development Foundation and contributors. Licensed
+// under the ISC License. See the COPYING file at the top-level directory of
+// this distribution or at http://opensource.org/licenses/ISC
+
+#include "main/Application.h"
+#include "main/test.h"
+#include "main/Config.h"
+#include "lib/catch.hpp"
+#include "util/Logging.h"
+#include <xdrpp/autocheck.h>
+
+using namespace stellar;
+
+TEST_CASE("Ledger entry db lifecycle", "[ledger]")
+{
+    
+}
