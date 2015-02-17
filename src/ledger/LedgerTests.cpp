51229ac155f8dee557d168551f1f465232af034e
Graydon Hoare <graydon@stellar.org>
2015-02-16 17:18:12 -0800

    Add ledger/LedgerTests.cpp

--- src/ledger/LedgerTests.cpp
+++ src/ledger/LedgerTests.cpp
@@ -6,12 +6,84 @@
 #include "main/test.h"
 #include "main/Config.h"
 #include "lib/catch.hpp"
+#include "ledger/LedgerDelta.h"
+#include "ledger/EntryFrame.h"
 #include "util/Logging.h"
+#include "util/Timer.h"
+#include "util/types.h"
 #include <xdrpp/autocheck.h>
 
 using namespace stellar;
 
+template<typename T> void
+clampLow(T low, T& v)
+{
+    if (v < low)
+    {
+        v = low;
+    }
+}
+
+template<typename T> void
+clampHigh(T high, T& v)
+{
+    if (v > high)
+    {
+        v = high;
+    }
+}
+
+auto
+validLedgerEntryGenerator =
+    autocheck::map([](LedgerEntry&& le, size_t s) {
+            switch (le.type())
+            {
+            case TRUSTLINE:
+            {
+                le.trustLine().currency.type(ISO4217);
+                strToCurrencyCode(le.trustLine().currency.isoCI().currencyCode, "USD");
+                clampLow<int64_t>(0, le.trustLine().balance);
+                clampLow<int64_t>(0, le.trustLine().limit);
+                clampHigh<int64_t>(le.trustLine().limit, le.trustLine().balance);
+            }
+            break;
+
+            case OFFER:
+            {
+                le.offer().takerGets.type(ISO4217);
+                strToCurrencyCode(le.offer().takerGets.isoCI().currencyCode, "CAD");
+
+                le.offer().takerPays.type(ISO4217);
+                strToCurrencyCode(le.offer().takerPays.isoCI().currencyCode, "EUR");
+
+                clampLow(0, le.offer().price.n);
+                clampLow(1, le.offer().price.d);
+            }
+            break;
+
+            case ACCOUNT:
+            break;
+            }
+
+            return le;
+        }, autocheck::generator<LedgerEntry>());
+
+
 TEST_CASE("Ledger entry db lifecycle", "[ledger]")
 {
-    
+    Config cfg(getTestConfig());
+    VirtualClock clock;
+    Application app(clock, cfg);
+
+    LedgerDelta delta;
+    auto& db = app.getDatabase();
+    for (size_t i = 0; i < 100; ++i)
+    {
+        auto le = EntryFrame::FromXDR(validLedgerEntryGenerator(3));
+        CHECK(!EntryFrame::exists(db, le->getKey()));
+        le->storeAddOrChange(delta, db);
+        CHECK(EntryFrame::exists(db, le->getKey()));
+        le->storeDelete(delta, db);
+        CHECK(!EntryFrame::exists(db, le->getKey()));
+    }
 }
