e2cc0fa497d188e67c4056629a0688cfc4c4c3b4
Graydon Hoare <graydon@stellar.org>
2015-03-06 11:11:15 -0800

    Make VirtualTimer nocopy/nomove, fix herder misuse of it.

---
+++ src/util/NonCopyable.h
@@ -0,0 +1,24 @@
+#pragma once
+namespace stellar
+{
+
+struct NonCopyable
+{
+    NonCopyable() = default;
+    NonCopyable(NonCopyable const&) = delete;
+    NonCopyable& operator=(NonCopyable const&) = delete;
+};
+
+struct NonMovable
+{
+    NonMovable() = default;
+    NonMovable(NonMovable &&) = delete;
+    NonMovable& operator=(NonMovable &&) = delete;
+};
+
+struct NonMovableOrCopyable : private NonCopyable, NonMovable
+{
+    NonMovableOrCopyable() = default;
+};
+
+}
