3cbd1323787a0f94c2592ba4e74fc9f3c3daaf27
Graydon Hoare <graydon@stellar.org>
2015-03-13 12:56:02 -0700

    Mention clock concept.

--- src/util/Timer.h
+++ src/util/Timer.h
@@ -55,6 +55,9 @@ public:
     // We don't want to deal with systems that have not moved to 64bit time_t yet.
     static_assert(sizeof(uint64_t) == sizeof(std::time_t),
                   "Require 64bit time_t");
+
+    // These model most of the std::chrono clock concept, with the exception of now()
+    // which is non-static.
     typedef std::chrono::nanoseconds    duration;
     typedef duration::rep               rep;
     typedef duration::period            period;
