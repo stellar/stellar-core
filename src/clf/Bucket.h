5022c8916c90d84924b5cebf74486e0d8e73c005
Graydon Hoare <graydon@stellar.org>
2015-03-25 14:23:14 -0700

    Document Bucket.h

--- src/clf/Bucket.h
+++ src/clf/Bucket.h
@@ -32,28 +32,63 @@ class Bucket : public std::enable_shared_from_this<Bucket>
     bool mRetain {false};
 
   public:
+
+    // Helper class that reads through the entries in a bucket, used internally
+    // during merging.
     class InputIterator;
+
+    // Helper class that writes new elements to a file and returns a bucket
+    // when finished, used internally during merging.
     class OutputIterator;
 
+    // Create an empty bucket. The empty bucket has hash '000000...' and its
+    // filename is the empty string.
     Bucket();
+
+    // Destroy a bucket, deleting its underlying file if the bucket is not
+    // 'retained'. See `setRetain`.
     ~Bucket();
 
+    // Construct a bucket with a given filename and hash. Asserts that the file
+    // exists, but does not check that the hash is the bucket's hash. Caller
+    // needs to ensure that.
     Bucket(std::string const& filename, uint256 const& hash);
 
     uint256 const& getHash() const;
     std::string const& getFilename() const;
 
+    // Sets or clears the `retain` flag on the bucket. A retained bucket will
+    // not be deleted (from the filesystem) when the Bucket object is deleted. A
+    // non-retained bucket _will_ delete the underlying file. Buckets should
+    // only be retained if you want them to survive process-exit / restart; in
+    // particular the contents of the CLFManager's current BucketList should be
+    // retained.
     void setRetain(bool r);
 
+    // Returns true if a CLFEntry that is key-wise identical to the given
+    // CLFEntry exists in the bucket. For testing.
     bool containsCLFIdentity(CLFEntry const& id) const;
+
+    // Return the count of live and dead CLFEntries in the bucket. For testing.
     std::pair<size_t, size_t> countLiveAndDeadEntries() const;
 
+    // "Applies" the bucket to the database. For each entry in the bucket, if
+    // the entry is live, creates or updates the corresponding entry in the
+    // database; if the entry is dead (a tombstone), deletes the corresponding
+    // entry in the database.
     void apply(Database& db) const;
 
+    // Create a fresh bucket from a given vector of live LedgerEntries and
+    // dead LedgerEntryKeys. The bucket will be sorted, hashed, and adopted
+    // in the provided CLFManager.
     static std::shared_ptr<Bucket>
     fresh(CLFManager& clfManager, std::vector<LedgerEntry> const& liveEntries,
           std::vector<LedgerKey> const& deadEntries);
 
+    // Merge two buckets together, producing a fresh one. Entries in `oldBucket`
+    // are overridden in the fresh bucket by keywise-equal entries in
+    // `newBucket`. Entries are inhibited from the fresh bucket by keywise-equal
+    // entries in any of the buckets in the provided `shadows` vector.
     static std::shared_ptr<Bucket>
     merge(CLFManager& clfManager, std::shared_ptr<Bucket> const& oldBucket,
           std::shared_ptr<Bucket> const& newBucket,
