3a5ac0f5cf4786db9d6543a20f025bc132494fc2
Guillaume Marceau <guillaume@stellar.org>
2015-04-06 17:48:35 -0400

    Turn Herder's constant macros into real constants.

---
+++ src/herder/Herder.cpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "herder/Herder.h"
+
+namespace stellar
+{
+
+std::chrono::seconds const Herder::EXP_LEDGER_TIMESPAN_SECONDS(5);
+std::chrono::seconds const Herder::MAX_SCP_TIMEOUT_SECONDS(240);
+std::chrono::seconds const Herder::MAX_TIME_SLIP_SECONDS(60);
+std::chrono::seconds const Herder::NODE_EXPIRATION_SECONDS(240);
+uint32 const Herder::LEDGER_VALIDITY_BRACKET = 1000;
+
+
+}
