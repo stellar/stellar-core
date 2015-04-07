19d824559cc6746214e417ea64ef93f56b936ddc
Guillaume Marceau <guillaume@stellar.org>
2015-04-07 14:02:20 -0400

    core topology test at multiple scales, hierarchical topology

--- src/simulation/CoreTests.cpp
+++ src/simulation/CoreTests.cpp
@@ -20,7 +20,7 @@ typedef std::unique_ptr<Application> appPtr;
 
 TEST_CASE("core4 topology", "[simulation]")
 {
-    Simulation::pointer simulation = Topologies::core4();
+    Simulation::pointer simulation = Topologies::core(4, 1.0, Simulation::OVER_LOOPBACK);
     simulation->startAllNodes();
 
     simulation->crankUntil(
@@ -33,9 +33,43 @@ TEST_CASE("core4 topology", "[simulation]")
     REQUIRE(simulation->haveAllExternalized(3));
 }
 
-TEST_CASE("core4 topology long", "[simulation][long][hide]")
+TEST_CASE("hierarchical topology at multiple scales", "[simulation][hide]")
 {
-    Simulation::pointer simulation = Topologies::core3(Simulation::OVER_TCP);
+    int const nLedgers = 3;
+    std::vector<std::chrono::seconds> times;
+
+    for (int nBranches = 0; nBranches < 30; nBranches++)
+    {
+        auto tBegin = std::chrono::system_clock::now();
+
+        Simulation::pointer sim = Topologies::hierarchicalQuorum(nBranches);
+        sim->startAllNodes();
+
+        sim->crankUntil(
+            [&sim, nLedgers]()
+        {
+            return sim->haveAllExternalized(nLedgers);
+        },
+            100 * nLedgers * Herder::EXP_LEDGER_TIMESPAN_SECONDS);
+
+        REQUIRE(sim->haveAllExternalized(3));
+
+        auto t = std::chrono::duration_cast<std::chrono::seconds>(
+            std::chrono::system_clock::now() - tBegin);
+        times.push_back(t);
+
+        LOG(INFO) << "Time spent closing " << nLedgers << " ledgers with " << sim->getNodes().size() << " nodes : "
+            << t.count() << " seconds";
+
+        LOG(INFO) << sim->metricsSummary("scp");
+    }
+}
+
+
+
+TEST_CASE("core4 topology long, over tcp", "[simulation][long][hide]")
+{
+    Simulation::pointer simulation = Topologies::core(3, 1.0, Simulation::OVER_TCP);
     simulation->startAllNodes();
 
     simulation->crankUntil(
@@ -47,6 +81,7 @@ TEST_CASE("core4 topology long", "[simulation][long][hide]")
 
     REQUIRE(simulation->haveAllExternalized(3));
 }
+
 
 TEST_CASE("cycle4 topology", "[simulation]")
 {
