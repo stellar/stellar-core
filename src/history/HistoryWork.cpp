5142b1635ad81d3d09f63539e4acf376e61435f7
Graydon Hoare <graydon@stellar.org>
2015-12-29 12:36:23 -0800

    Fix some boundary conditions in HistoryWork.cpp : fmtProgress

--- src/history/HistoryWork.cpp
+++ src/history/HistoryWork.cpp
@@ -33,16 +33,24 @@ fmtProgress(Application& app, std::string const& task, uint32_t first,
             uint32_t last, uint32_t curr)
 {
     auto step = app.getHistoryManager().getCheckpointFrequency();
+    if (curr > last)
+    {
+        curr = last;
+    }
+    if (curr < first)
+    {
+        curr = first;
+    }
     if (step == 0)
     {
         step = 1;
     }
-    auto done = (curr - first) / step;
-    auto total = (last - first) / step;
-    if (total == 0)
+    if (last < first)
     {
-        total = 1;
+        last = first;
     }
+    auto done = 1 + ((curr - first) / step);
+    auto total = 1 + ((last - first) / step);
     auto pct = (100 * done) / total;
     return fmt::format("{:s} {:d}/{:d} ({:d}%)", task, done, total, pct);
 }
