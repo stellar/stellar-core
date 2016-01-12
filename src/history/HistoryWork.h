8e3ee805a3068a05720fc3595c5115dd2b11a893
Graydon Hoare <graydon@stellar.org>
2016-01-12 13:37:23 -0800

    Add FetchRecentQsetsWork.

--- src/history/HistoryWork.h
+++ src/history/HistoryWork.h
@@ -450,4 +450,25 @@ class RepairMissingBucketsWork : public BucketDownloadWork
     void onFailureRaise() override;
     Work::State onSuccess() override;
 };
+
+class FetchRecentQsetsWork : public Work
+{
+
+    typedef std::function<void(asio::error_code const& ec)> handler;
+    handler mEndHandler;
+    std::unique_ptr<TmpDir> mDownloadDir;
+    InferredQuorum& mInferredQuorum;
+    HistoryArchiveState mRemoteState;
+    std::shared_ptr<Work> mGetHistoryArchiveStateWork;
+    std::shared_ptr<Work> mDownloadSCPMessagesWork;
+
+  public:
+    FetchRecentQsetsWork(Application& app, WorkParent& parent,
+                         InferredQuorum& iq,
+                         handler endHandler);
+    void onReset() override;
+    void onFailureRaise() override;
+    Work::State onSuccess() override;
+};
+
 }
