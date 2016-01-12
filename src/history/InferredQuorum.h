7f1535bdfba767e93725bf6df1b45beafb9eb83e
Graydon Hoare <graydon@stellar.org>
2016-01-12 13:36:51 -0800

    Add InferredQuorum.

---
+++ src/history/InferredQuorum.h
@@ -0,0 +1,31 @@
+#pragma once
+
+// Copyright 2016 Stellar Development Foundation and contributors. Licensed
+// under the Apache License, Version 2.0. See the COPYING file at the root
+// of this distribution or at http://www.apache.org/licenses/LICENSE-2.0
+
+#include <unordered_map>
+#include <string>
+#include "overlay/StellarXDR.h"
+#include "crypto/SecretKey.h"
+#include "util/HashOfHash.h"
+#include "main/Config.h"
+
+namespace stellar {
+
+struct InferredQuorum
+{
+    std::unordered_map<Hash, SCPQuorumSet> mQsets;
+    std::unordered_multimap<PublicKey, Hash> mQsetHashes;
+    std::unordered_map<PublicKey, size_t> mPubKeys;
+    void noteSCPHistory(SCPHistoryEntry const& hist);
+    void noteQset(SCPQuorumSet const& qset);
+    void noteQsetHash(PublicKey const& pk, Hash const& hash);
+    void notePubKey(PublicKey const& pk);
+    std::string toString(Config const& cfg) const;
+    void writeQuorumGraph(Config const& cfg,
+                          std::string const& filename) const;
+    bool checkQuorumIntersection(Config const& cfg) const;
+};
+
+}
