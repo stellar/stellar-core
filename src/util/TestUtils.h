45c03955dfd4f524a5bc645612d3dc1d13844a82
Rafał Malinowski <rafal.przemyslaw.malinowski@gmail.com>
2016-10-06 12:36:44 +0200

    allow testing with different protocol versions
    Signed-off-by: Rafał Malinowski <rafal.przemyslaw.malinowski@gmail.com>

---
+++ src/util/TestUtils.h
@@ -0,0 +1,34 @@
+#pragma once
+
+// Copyright 2016 Stellar Development Foundation and contributors. Licensed
+// under the Apache License, Version 2.0. See the COPYING file at the root
+// of this distribution or at http://www.apache.org/licenses/LICENSE-2.0
+
+#include "ledger/LedgerManagerImpl.h"
+#include "main/ApplicationImpl.h"
+
+namespace stellar
+{
+
+class LedgerManagerEditableVersion : public LedgerManagerImpl
+{
+  public:
+    LedgerManagerEditableVersion(Application& app);
+
+    uint32_t getCurrentLedgerVersion() const override;
+    void setCurrentLedgerVersion(uint32_t currentLedgerVersion);
+
+  private:
+    uint32_t mCurrentLedgerVersion;
+};
+
+class ApplicationEditableVersion : public ApplicationImpl
+{
+  public:
+    ApplicationEditableVersion(VirtualClock& clock, Config const& cfg);
+
+    virtual LedgerManagerEditableVersion& getLedgerManager() override;
+
+};
+
+}
