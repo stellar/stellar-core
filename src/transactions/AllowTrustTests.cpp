94cd64869488267379f09fa9f35ce78eaa806bd0
Rafał Malinowski <rafal.przemyslaw.malinowski@gmail.com>
2016-10-18 10:26:41 +0200

    fix licence from ISC to Apache in two files
    Signed-off-by: Rafał Malinowski <rafal.przemyslaw.malinowski@gmail.com>

--- src/transactions/AllowTrustTests.cpp
+++ src/transactions/AllowTrustTests.cpp
@@ -1,6 +1,6 @@
-// Copyright 2014 Stellar Development Foundation and contributors. Licensed
-// under the ISC License. See the COPYING file at the top-level directory of
-// this distribution or at http://opensource.org/licenses/ISC
+// Copyright 2016 Stellar Development Foundation and contributors. Licensed
+// under the Apache License, Version 2.0. See the COPYING file at the root
+// of this distribution or at http://www.apache.org/licenses/LICENSE-2.0
 
 #include "lib/catch.hpp"
 #include "main/Application.h"
