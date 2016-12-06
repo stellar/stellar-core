e82037a2f30d992ba74b512b1691d774da4f916b
Rafał Malinowski <rafal.przemyslaw.malinowski@gmail.com>
2016-12-06 15:20:28 +0100

    add tests for exchange function
    Signed-off-by: Rafał Malinowski <rafal.przemyslaw.malinowski@gmail.com>

--- src/transactions/OfferTests.cpp
+++ src/transactions/OfferTests.cpp
@@ -13,6 +13,7 @@
 #include "test/TestAccount.h"
 #include "test/TestExceptions.h"
 #include "test/TxTests.h"
+#include "transactions/OfferExchange.h"
 #include "util/Timer.h"
 #include "database/Database.h"
 
@@ -1001,6 +1002,282 @@ TEST_CASE("create offer", "[tx][offers]")
                                  line->getBalance());
                 }
             }
+        }
+    }
+}
+
+TEST_CASE("Exchange", "[offers]")
+{
+    auto validate = [](ExchangeResult const& x, ExchangeResult const &y){
+        REQUIRE(x.numWheatReceived == y.numWheatReceived);
+        REQUIRE(x.numSheepSend == y.numSheepSend);
+        REQUIRE(x.reduced == y.reduced);
+    };
+
+    SECTION("normal prices")
+    {
+        SECTION("no limits")
+        {
+            SECTION("1000")
+            {
+                validate(exchange(1000, Price{3, 2}, INT64_MAX, INT64_MAX), {1000, 1500, false});
+                validate(exchange(1000, Price{1, 1}, INT64_MAX, INT64_MAX), {1000, 1000, false});
+                // invalid, should receive 1000
+                validate(exchange(1000, Price{2, 3}, INT64_MAX, INT64_MAX), {999, 666, false});
+            }
+
+            SECTION("999")
+            {
+                // invalid, should receive 999
+                validate(exchange(999, Price{3, 2}, INT64_MAX, INT64_MAX), {998, 1498, false});
+                validate(exchange(999, Price{1, 1}, INT64_MAX, INT64_MAX), {999, 999, false});
+                validate(exchange(999, Price{2, 3}, INT64_MAX, INT64_MAX), {999, 666, false});
+            }
+
+            SECTION("1")
+            {
+                // invalid, should receive 1, not sure how much should send
+                validate(exchange(1, Price{3, 2}, INT64_MAX, INT64_MAX), {0, 1, false});
+                validate(exchange(1, Price{1, 1}, INT64_MAX, INT64_MAX), {1, 1, false});
+                // invalid, should receive 1, not sure how much should send
+                validate(exchange(1, Price{2, 3}, INT64_MAX, INT64_MAX), {0, 0, false});
+            }
+
+            SECTION("0")
+            {
+                validate(exchange(0, Price{3, 2}, INT64_MAX, INT64_MAX), {0, 0, false});
+                validate(exchange(0, Price{1, 1}, INT64_MAX, INT64_MAX), {0, 0, false});
+                validate(exchange(0, Price{2, 3}, INT64_MAX, INT64_MAX), {0, 0, false});
+            }
+        }
+
+        SECTION("send limits")
+        {
+            SECTION("1000 limited to 500")
+            {
+                validate(exchange(1000, Price{3, 2}, INT64_MAX, 750), {500, 750, true});
+                validate(exchange(1000, Price{1, 1}, INT64_MAX, 500), {500, 500, true});
+                // rounding is OK here
+                validate(exchange(1000, Price{2, 3}, INT64_MAX, 333), {499, 333, true});
+            }
+
+            SECTION("999 limited to 499")
+            {
+                validate(exchange(999, Price{3, 2}, INT64_MAX, 749), {499, 749, true});
+                validate(exchange(999, Price{1, 1}, INT64_MAX, 499), {499, 499, true});
+                validate(exchange(999, Price{2, 3}, INT64_MAX, 333), {499, 333, true});
+            }
+
+            SECTION("20 limited to 10")
+            {
+                validate(exchange(20, Price{3, 2}, INT64_MAX, 15), {10, 15, true});
+                validate(exchange(20, Price{1, 1}, INT64_MAX, 10), {10, 10, true});
+                validate(exchange(20, Price{2, 3}, INT64_MAX, 7), {10, 7, true});
+            }
+
+            SECTION("2 limited to 1")
+            {
+                validate(exchange(2, Price{3, 2}, INT64_MAX, 2), {1, 2, true});
+                validate(exchange(2, Price{1, 1}, INT64_MAX, 1), {1, 1, true});
+                // value of reduced is false but there was a reduction and we need a way to know about it
+                // or it should return 2 as received
+                validate(exchange(2, Price{2, 3}, INT64_MAX, 1), {1, 1, false});
+            }
+        }
+
+        SECTION("receive limits")
+        {
+            SECTION("1000 limited to 500")
+            {
+                validate(exchange(1000, Price{3, 2}, 500, INT64_MAX), {500, 750, true});
+                validate(exchange(1000, Price{1, 1}, 500, INT64_MAX), {500, 500, true});
+                // rounding is OK here
+                validate(exchange(1000, Price{2, 3}, 500, INT64_MAX), {499, 333, true});
+            }
+
+            SECTION("999 limited to 499")
+            {
+                // should receive 499
+                validate(exchange(999, Price{3, 2}, 499, INT64_MAX), {498, 748, true});
+                validate(exchange(999, Price{1, 1}, 499, INT64_MAX), {499, 499, true});
+                // should receive 499
+                validate(exchange(999, Price{2, 3}, 499, INT64_MAX), {498, 332, true});
+            }
+
+            SECTION("20 limited to 10")
+            {
+                validate(exchange(20, Price{3, 2}, 10, INT64_MAX), {10, 15, true});
+                validate(exchange(20, Price{1, 1}, 10, INT64_MAX), {10, 10, true});
+                // should receive 10
+                validate(exchange(20, Price{2, 3}, 10, INT64_MAX), {9, 6, true});
+            }
+
+            SECTION("2 limited to 1")
+            {
+                // should receive 1 and pay for it
+                validate(exchange(2, Price{3, 2}, 1, INT64_MAX), {0, 1, true});
+                validate(exchange(2, Price{1, 1}, 1, INT64_MAX), {1, 1, true});
+                // should receive 1 and pay for it
+                validate(exchange(2, Price{2, 3}, 1, INT64_MAX), {0, 0, true});
+            }
+        }
+    }
+
+    SECTION("extra big prices")
+    {
+        SECTION("no limits")
+        {
+            validate(exchange(1000, Price{INT32_MAX, 1}, INT64_MAX, INT64_MAX), {1000, 1000ull * INT32_MAX, false});
+            validate(exchange(999, Price{INT32_MAX, 1}, INT64_MAX, INT64_MAX), {999, 999ull * INT32_MAX, false});
+            validate(exchange(1, Price{INT32_MAX, 1}, INT64_MAX, INT64_MAX), {1, INT32_MAX, false});
+            validate(exchange(0, Price{INT32_MAX, 1}, INT64_MAX, INT64_MAX), {0, 0, false});
+        }
+
+        SECTION("send limits")
+        {
+            SECTION("750")
+            {
+                // nothing received but 750 sent
+                validate(exchange(1000, Price{INT32_MAX, 1}, INT64_MAX, 750), {0, 750, true});
+                // nothing received but 750 sent
+                validate(exchange(999, Price{INT32_MAX, 1}, INT64_MAX, 750), {0, 750, true});
+                // nothing received but 750 sent
+                validate(exchange(1, Price{INT32_MAX, 1}, INT64_MAX, 750), {0, 750, true});
+                validate(exchange(0, Price{INT32_MAX, 1}, INT64_MAX, 750), {0, 0, false});
+            }
+
+            SECTION("INT32_MAX")
+            {
+                validate(exchange(1000, Price{INT32_MAX, 1}, INT64_MAX, INT32_MAX), {1, INT32_MAX, true});
+                validate(exchange(999, Price{INT32_MAX, 1}, INT64_MAX, INT32_MAX), {1, INT32_MAX, true});
+                // nothing received but 750 sent
+                validate(exchange(1, Price{INT32_MAX, 1}, INT64_MAX, INT32_MAX), {1, INT32_MAX, false});
+                validate(exchange(0, Price{INT32_MAX, 1}, INT64_MAX, INT32_MAX), {0, 0, false});
+            }
+
+            SECTION("750 * INT32_MAX")
+            {
+                validate(exchange(1000, Price{INT32_MAX, 1}, INT64_MAX, 750ull * INT32_MAX), {750, 750ull * INT32_MAX, true});
+                validate(exchange(999, Price{INT32_MAX, 1}, INT64_MAX, 750ull * INT32_MAX), {750, 750ull * INT32_MAX, true});
+                validate(exchange(1, Price{INT32_MAX, 1}, INT64_MAX, 750ull * INT32_MAX), {1, INT32_MAX, false});
+                validate(exchange(0, Price{INT32_MAX, 1}, INT64_MAX, 750ull * INT32_MAX), {0, 0, false});
+            }
+        }
+
+        SECTION("receive limits")
+        {
+            SECTION("750")
+            {
+                validate(exchange(1000, Price{INT32_MAX, 1}, 750, INT64_MAX), {750, 750ull * INT32_MAX, true});
+                validate(exchange(999, Price{INT32_MAX, 1}, 750, INT64_MAX), {750, 750ull * INT32_MAX, true});
+                validate(exchange(1, Price{INT32_MAX, 1}, 750, INT64_MAX), {1, INT32_MAX, false});
+                validate(exchange(0, Price{INT32_MAX, 1}, 750, INT64_MAX), {0, 0, false});
+            }
+
+            SECTION("INT32_MAX")
+            {
+                validate(exchange(1000, Price{INT32_MAX, 1}, INT32_MAX, INT64_MAX), {1000, 1000ull * INT32_MAX, false});
+                validate(exchange(999, Price{INT32_MAX, 1}, INT32_MAX, INT64_MAX), {999, 999ull * INT32_MAX, false});
+                validate(exchange(1, Price{INT32_MAX, 1}, INT32_MAX, INT64_MAX), {1, INT32_MAX, false});
+                validate(exchange(0, Price{INT32_MAX, 1}, INT32_MAX, INT64_MAX), {0, 0, false});
+            }
+        }
+    }
+
+    SECTION("extra small prices")
+    {
+        SECTION("no limits")
+        {
+            validate(exchange(1000ull * INT32_MAX, Price{1, INT32_MAX}, INT64_MAX, INT64_MAX), {1000ull * INT32_MAX, 1000, false});
+            validate(exchange(999ull * INT32_MAX, Price{1, INT32_MAX}, INT64_MAX, INT64_MAX), {999ull * INT32_MAX, 999, false});
+            validate(exchange(INT32_MAX, Price{1, INT32_MAX}, INT64_MAX, INT64_MAX), {INT32_MAX, 1, false});
+            validate(exchange(0, Price{1, INT32_MAX}, INT64_MAX, INT64_MAX), {0, 0, false});
+        }
+
+        SECTION("send limits")
+        {
+            SECTION("750")
+            {
+                validate(exchange(1000ull * INT32_MAX, Price{1, INT32_MAX}, INT64_MAX, 750), {750ull * INT32_MAX, 750, true});
+                validate(exchange(999ull * INT32_MAX, Price{1, INT32_MAX}, INT64_MAX, 750), {750ull * INT32_MAX, 750, true});
+                validate(exchange(INT32_MAX, Price{1, INT32_MAX}, INT64_MAX, 750), {INT32_MAX, 1, false});
+                validate(exchange(0, Price{1, INT32_MAX}, INT64_MAX, 750), {0, 0, false});
+            }
+
+            SECTION("INT32_MAX")
+            {
+                validate(exchange(1000ull * INT32_MAX, Price{1, INT32_MAX}, INT64_MAX, INT32_MAX), {1000ull * INT32_MAX, 1000, false});
+                validate(exchange(999ull * INT32_MAX, Price{1, INT32_MAX}, INT64_MAX, INT32_MAX), {999ul * INT32_MAX, 999, false});
+                validate(exchange(INT32_MAX, Price{1, INT32_MAX}, INT64_MAX, INT32_MAX), {INT32_MAX, 1, false});
+                validate(exchange(0, Price{1, INT32_MAX}, INT64_MAX, INT32_MAX), {0, 0, false});
+            }
+        }
+
+        SECTION("receive limits")
+        {
+            SECTION("750")
+            {
+                validate(exchange(1000ull * INT32_MAX, Price{1, INT32_MAX}, 750, INT64_MAX), {0, 0, true});
+                validate(exchange(999ull * INT32_MAX, Price{1, INT32_MAX}, 750, INT64_MAX), {0, 0, true});
+                validate(exchange(INT32_MAX, Price{1, INT32_MAX}, 750, INT64_MAX), {0, 0, true});
+                validate(exchange(750, Price{1, INT32_MAX}, 750, INT64_MAX), {0, 0, false});
+            }
+
+            SECTION("INT32_MAX")
+            {
+                validate(exchange(1000ull * INT32_MAX, Price{1, INT32_MAX}, 750ull * INT32_MAX, INT64_MAX), {750ull * INT32_MAX, 750, true});
+                validate(exchange(999ull * INT32_MAX, Price{1, INT32_MAX}, 750ull * INT32_MAX, INT64_MAX), {750ull * INT32_MAX, 750, true});
+                validate(exchange(INT32_MAX, Price{1, INT32_MAX}, 750ull * INT32_MAX, INT64_MAX), {INT32_MAX, 1, false});
+                validate(exchange(750, Price{1, INT32_MAX}, 750ull * INT32_MAX, INT64_MAX), {0, 0, false});
+            }
+
+            SECTION("750 * INT32_MAX")
+            {
+                validate(exchange(1000ull * INT32_MAX, Price{1, INT32_MAX}, 750ul * INT32_MAX, INT64_MAX), {750ull * INT32_MAX, 750, true});
+                validate(exchange(999ull * INT32_MAX, Price{1, INT32_MAX}, 750ul * INT32_MAX, INT64_MAX), {750ull * INT32_MAX, 750, true});
+                validate(exchange(INT32_MAX, Price{1, INT32_MAX}, 750ul * INT32_MAX, INT64_MAX), {INT32_MAX, 1, false});
+                validate(exchange(750, Price{1, INT32_MAX}, 750ul * INT32_MAX, INT64_MAX), {0, 0, false});
+            }
+        }
+    }
+
+    SECTION("exchange with big limits")
+    {
+        SECTION("INT32_MAX send")
+        {
+            validate(exchange(INT32_MAX, Price{3, 2}, INT64_MAX, INT32_MAX), {1431655764, INT32_MAX, true});
+            validate(exchange(INT32_MAX, Price{1, 1}, INT64_MAX, INT32_MAX), {INT32_MAX, INT32_MAX, false});
+            // should receive INT32_MAX
+            validate(exchange(INT32_MAX, Price{2, 3}, INT64_MAX, INT32_MAX), {INT32_MAX - 1, 1431655764, false});
+            validate(exchange(INT32_MAX, Price{1, INT32_MAX}, INT64_MAX, INT32_MAX), {INT32_MAX, 1, false});
+            validate(exchange(INT32_MAX, Price{INT32_MAX, 1}, INT64_MAX, INT32_MAX), {1, INT32_MAX, true});
+            validate(exchange(INT32_MAX, Price{INT32_MAX, INT32_MAX}, INT64_MAX, INT32_MAX), {INT32_MAX, INT32_MAX, false});
+        }
+
+        SECTION("INT32_MAX receive")
+        {
+            // should receive INT32_MAX
+            validate(exchange(INT32_MAX, Price{3, 2}, INT32_MAX, INT64_MAX), {INT32_MAX - 1, 3221225470, false});
+            validate(exchange(INT32_MAX, Price{1, 1}, INT32_MAX, INT64_MAX), {INT32_MAX, INT32_MAX, false});
+            // should receive INT32_MAX
+            validate(exchange(INT32_MAX, Price{2, 3}, INT32_MAX, INT64_MAX), {INT32_MAX - 1, 1431655764, false});
+            validate(exchange(INT32_MAX, Price{1, INT32_MAX}, INT32_MAX, INT64_MAX), {INT32_MAX, 1, false});
+            validate(exchange(INT32_MAX, Price{INT32_MAX, 1}, INT32_MAX, INT64_MAX), {INT32_MAX, 4611686014132420609, false});
+            validate(exchange(INT32_MAX, Price{INT32_MAX, INT32_MAX}, INT32_MAX, INT64_MAX), {INT32_MAX, INT32_MAX, false});
+        }
+
+        SECTION("INT64_MAX")
+        {
+            // should be marked as reduced
+            validate(exchange(INT64_MAX, Price{3, 2}, INT64_MAX, INT64_MAX), {6148914691236517204, INT64_MAX, false});
+            validate(exchange(INT64_MAX, Price{1, 1}, INT64_MAX, INT64_MAX), {INT64_MAX, INT64_MAX, false});
+            // should receive INT64_MAX
+            validate(exchange(INT64_MAX, Price{2, 3}, INT64_MAX, INT64_MAX), {INT64_MAX - 1, 6148914691236517204, false});
+            // should receive INT64_MAX
+            validate(exchange(INT64_MAX, Price{1, INT32_MAX}, INT64_MAX, INT64_MAX), {INT64_MAX - 1, 4294967298, false});
+            // should be marked as reduced
+            validate(exchange(INT64_MAX, Price{INT32_MAX, 1}, INT64_MAX, INT64_MAX), {4294967298, INT64_MAX, false});
+            validate(exchange(INT64_MAX, Price{INT32_MAX, INT32_MAX}, INT64_MAX, INT64_MAX), {INT64_MAX, INT64_MAX, false});
         }
     }
 }
