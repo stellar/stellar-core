1468597764e1ffe7a91c3d28eb98555ce0e7ad64
Rafał Malinowski <rafal.przemyslaw.malinowski@gmail.com>
2017-03-31 14:07:32 +0200

    make LedgerCloseData properly encapsulated
    Signed-off-by: Rafał Malinowski <rafal.przemyslaw.malinowski@gmail.com>

--- src/herder/LedgerCloseData.h
+++ src/herder/LedgerCloseData.h
@@ -21,12 +21,29 @@ namespace stellar
 class LedgerCloseData
 {
   public:
+    LedgerCloseData(uint32_t ledgerSeq, TxSetFramePtr txSet,
+                    StellarValue const& v);
+
+    uint32_t
+    getLedgerSeq() const
+    {
+        return mLedgerSeq;
+    }
+    TxSetFramePtr
+    getTxSet() const
+    {
+        return mTxSet;
+    }
+    StellarValue const&
+    getValue() const
+    {
+        return mValue;
+    }
+
+  private:
     uint32_t mLedgerSeq;
     TxSetFramePtr mTxSet;
     StellarValue mValue;
-
-    LedgerCloseData(uint32_t ledgerSeq, TxSetFramePtr txSet,
-                    StellarValue const& v);
 };
 
 std::string stellarValueToString(StellarValue const& sv);
