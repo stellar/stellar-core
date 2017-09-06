stellar/stellar-core

commit e48175f7778fa27b65cb9105476f117bf1105c1a

do not load buckets from public queue from database everytime
Signed-off-by: Rafał Malinowski <rafal.przemyslaw.malinowski@gmail.com>

src/bucket/PublishQueueBuckets.h
@@ -1,0 +1,29 @@
+#pragma once
+
+// Copyright 2017 Stellar Development Foundation and contributors. Licensed
+// under the Apache License, Version 2.0. See the COPYING file at the root
+// of this distribution or at http://www.apache.org/licenses/LICENSE-2.0
+
+#include <map>
+#include <vector>
+
+namespace stellar
+{
+
+class PublishQueueBuckets
+{
+public:
+    void addBuckets(std::vector<std::string> const& buckets);
+    void addBucket(std::string const& bucket);
+
+    void removeBuckets(std::vector<std::string> const& buckets);
+    void removeBucket(std::string const& bucket);
+
+    std::map<std::string, int> const& map() const { return mBucketUsage; }
+
+private:
+    std::map<std::string, int> mBucketUsage;
+
+};
+
+}

src/bucket/PublishQueueBuckets.cpp
@@ -1,0 +1,46 @@
+// Copyright 2017 Stellar Development Foundation and contributors. Licensed
+// under the Apache License, Version 2.0. See the COPYING file at the root
+// of this distribution or at http://www.apache.org/licenses/LICENSE-2.0
+
+#include "bucket/PublishQueueBuckets.h"
+
+namespace stellar
+{
+
+void PublishQueueBuckets::addBuckets(std::vector<std::string> const& buckets)
+{
+    for (auto const& bucket : buckets)
+    {
+        addBucket(bucket);
+    }
+}
+
+void PublishQueueBuckets::addBucket(std::string const& bucket)
+{
+    mBucketUsage[bucket]++;
+}
+
+void PublishQueueBuckets::removeBuckets(std::vector<std::string> const& buckets)
+{
+    for (auto const& bucket : buckets)
+    {
+        removeBucket(bucket);
+    }
+}
+
+void PublishQueueBuckets::removeBucket(std::string const& bucket)
+{
+    auto it = mBucketUsage.find(bucket);
+    if (it == std::end(mBucketUsage))
+    {
+        return;
+    }
+
+    it->second--;
+    if (it->second == 0)
+    {
+        mBucketUsage.erase(it);
+    }
+}
+
+}

src/history/HistoryManager.h
@@ -308,7 +308,7 @@ class HistoryManager
     // configured archives published correctly; if so the snapshot
     // can be dequeued, otherwise it should remain and be tried again
     // later.
-    virtual void historyPublished(uint32_t ledgerSeq, bool success) = 0;
+    virtual void historyPublished(uint32_t ledgerSeq, std::vector<std::string> const& originalBuckets, bool success) = 0;
 
     virtual void downloadMissingBuckets(
         HistoryArchiveState desiredState,

src/history/HistoryManagerImpl.cpp
@@ -514,6 +514,7 @@ HistoryManagerImpl::queueCurrentHistory()
     // merges-in-progress, avoid restarting them.
 
     mPublishQueue.Mark();
+    mPublishQueueBuckets.addBuckets(has.allBuckets());
     takeSnapshotAndPublish(has);
 }
 
@@ -577,7 +578,7 @@ HistoryManagerImpl::getPublishQueueStates()
 }
 
 std::vector<std::string>
-HistoryManagerImpl::getBucketsReferencedByPublishQueue()
+HistoryManagerImpl::loadBucketsReferencedByPublishQueue()
 {
     auto states = getPublishQueueStates();
     std::set<std::string> buckets;
@@ -587,6 +588,24 @@ HistoryManagerImpl::getBucketsReferencedByPublishQueue()
         buckets.insert(sb.begin(), sb.end());
     }
     return std::vector<std::string>(buckets.begin(), buckets.end());
+}
+
+std::vector<std::string>
+HistoryManagerImpl::getBucketsReferencedByPublishQueue()
+{
+    if (!mPublishQueueBucketsFilled)
+    {
+        mPublishQueueBuckets.addBuckets(loadBucketsReferencedByPublishQueue());
+        mPublishQueueBucketsFilled = true;
+    }
+
+    std::vector<std::string> buckets;
+    for (auto const& s : mPublishQueueBuckets.map())
+    {
+        buckets.push_back(s.first);
+    }
+
+    return buckets;
 }
 
 std::vector<std::string>
@@ -603,7 +622,7 @@ HistoryManagerImpl::getMissingBucketsReferencedByPublishQueue()
 }
 
 void
-HistoryManagerImpl::historyPublished(uint32_t ledgerSeq, bool success)
+HistoryManagerImpl::historyPublished(uint32_t ledgerSeq, std::vector<std::string> const& originalBuckets, bool success)
 {
     if (success)
     {
@@ -615,6 +634,8 @@ HistoryManagerImpl::historyPublished(uint32_t ledgerSeq, bool success)
         st.exchange(soci::use(ledgerSeq));
         st.define_and_bind();
         st.execute(true);
+
+        mPublishQueueBuckets.removeBuckets(originalBuckets);
     }
     else
     {

src/history/HistoryManagerImpl.h
@@ -4,6 +4,7 @@
 // under the Apache License, Version 2.0. See the COPYING file at the root
 // of this distribution or at http://www.apache.org/licenses/LICENSE-2.0
 
+#include "bucket/PublishQueueBuckets.h"
 #include "history/HistoryManager.h"
 #include "util/TmpDir.h"
 #include <memory>
@@ -24,6 +25,8 @@ class HistoryManagerImpl : public HistoryManager
     Application& mApp;
     std::unique_ptr<TmpDir> mWorkDir;
     std::shared_ptr<Work> mPublishWork;
+    PublishQueueBuckets mPublishQueueBuckets;
+    bool mPublishQueueBucketsFilled {false};
 
     medida::Meter& mPublishSkip;
     medida::Meter& mPublishQueue;
@@ -31,6 +34,8 @@ class HistoryManagerImpl : public HistoryManager
     medida::Meter& mPublishStart;
     medida::Meter& mPublishSuccess;
     medida::Meter& mPublishFailure;
+
+    std::vector<std::string> loadBucketsReferencedByPublishQueue();
 
   public:
     HistoryManagerImpl(Application& app);
@@ -69,7 +74,7 @@ class HistoryManagerImpl : public HistoryManager
 
     std::vector<HistoryArchiveState> getPublishQueueStates();
 
-    void historyPublished(uint32_t ledgerSeq, bool success) override;
+    void historyPublished(uint32_t ledgerSeq, std::vector<std::string> const& originalBuckets, bool success) override;
 
     void downloadMissingBuckets(
         HistoryArchiveState desiredState,

src/historywork/PublishWork.cpp
@@ -20,6 +20,7 @@ PublishWork::PublishWork(Application& app, WorkParent& parent,
     : Work(app, parent,
            fmt::format("publish-{:08x}", snapshot->mLocalState.currentLedger))
     , mSnapshot(snapshot)
+    , mOriginalBuckets(mSnapshot->mLocalState.allBuckets())
 {
 }
 
@@ -87,15 +88,19 @@ PublishWork::onSuccess()
         return WORK_PENDING;
     }
 
+    // use mOriginalBuckets as mSnapshot->mLocalState.allBuckets() could change
+    // in meantime
     mApp.getHistoryManager().historyPublished(
-        mSnapshot->mLocalState.currentLedger, true);
+        mSnapshot->mLocalState.currentLedger, mOriginalBuckets, true);
     return WORK_SUCCESS;
 }
 
 void
 PublishWork::onFailureRaise()
 {
+    // use mOriginalBuckets as mSnapshot->mLocalState.allBuckets() could change
+    // in meantime
     mApp.getHistoryManager().historyPublished(
-        mSnapshot->mLocalState.currentLedger, false);
+        mSnapshot->mLocalState.currentLedger, mOriginalBuckets, false);
 }
 }

src/historywork/PublishWork.h
@@ -14,6 +14,7 @@ struct StateSnapshot;
 class PublishWork : public Work
 {
     std::shared_ptr<StateSnapshot> mSnapshot;
+    std::vector<std::string> mOriginalBuckets;
 
     std::shared_ptr<Work> mResolveSnapshotWork;
     std::shared_ptr<Work> mWriteSnapshotWork;

commit c1bf7f6916fd7cb9947f47c0638015cfd144146f

Fixed a compilation issue on Mac introduced in commit e48175f

src/bucket/PublishQueueBuckets.h
@@ -5,6 +5,7 @@
 // of this distribution or at http://www.apache.org/licenses/LICENSE-2.0
 
 #include <map>
+#include <string>
 #include <vector>
 
 namespace stellar