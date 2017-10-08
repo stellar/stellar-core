5408b05e3398f3f7f60a7daa6583c8a2acd7c174
Rafał Malinowski <rafal.przemyslaw.malinowski@gmail.com>
2017-10-08 23:14:10 +0200

    add some documentation for ApplyLedgerChainWork
    Signed-off-by: Rafał Malinowski <rafal.przemyslaw.malinowski@gmail.com>

--- src/catchup/ApplyLedgerChainWork.h
+++ src/catchup/ApplyLedgerChainWork.h
@@ -17,6 +17,26 @@ namespace stellar
 struct LedgerHeaderHistoryEntry;
 class TmpDir;
 
+/**
+ * This class is responsible for applying transactions stored in files on
+ * temporary directory (downloadDir) to local ledger. It requires two sets of
+ * files - ledgers and transactions - int .xdr format. Transaction files are
+ * used to read transactions that will be used and ledger files are used to
+ * check if ledger hashes are matching.
+ *
+ * In each run it skips or applies transactions from one ledger. Skipping occurs
+ * when ledger to by applied is older than LCL from local ledger. At LCL
+ * boundary checks are made
+ * to confirm that ledgers from files are knot up with LCL. If everything is OK,
+ * an apply ledger operation is performed. Then another check is made - if new
+ * local ledger matches corresponding ledger from file.
+ *
+ * Contructor of this class takes some important parameters:
+ * * downloadDir - directory containing ledger and transaction files
+ * * range - range of ledgers to apply (low boundary can overlap with local
+ * history)
+ * * lastApplied - reference to last applied ledger header (which is LCL)
+ */
 class ApplyLedgerChainWork : public Work
 {
     TmpDir const& mDownloadDir;
