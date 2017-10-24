7c388ead9161c760af6fc6cdebedafe26bbc0163
MonsieurNicolas <nicolas@stellar.org>
2017-10-24 16:33:16 -0700

    clang-format

--- src/herder/PendingEnvelopesTests.cpp
+++ src/herder/PendingEnvelopesTests.cpp
@@ -2,9 +2,9 @@
 // under the Apache License, Version 2.0. See the COPYING file at the root
 // of this distribution or at http://www.apache.org/licenses/LICENSE-2.0
 
-#include "herder/PendingEnvelopes.h"
 #include "crypto/SHA.h"
 #include "herder/HerderImpl.h"
+#include "herder/PendingEnvelopes.h"
 #include "lib/catch.hpp"
 #include "main/Application.h"
 #include "test/TestAccount.h"
