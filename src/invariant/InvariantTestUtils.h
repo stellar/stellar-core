6830f19a1d9f240b83374efd6090761ee33344dd
Jon Jove <jon@stellar.org>
2017-12-05 11:21:23 -0600

    Created InvariantTestUtils namespace with utilities to test invariants

---
+++ src/invariant/InvariantTestUtils.h
@@ -0,0 +1,26 @@
+#pragma once
+
+// Copyright 2017 Stellar Development Foundation and contributors. Licensed
+// under the Apache License, Version 2.0. See the COPYING file at the root
+// of this distribution or at http://www.apache.org/licenses/LICENSE-2.0
+
+#include "ledger/EntryFrame.h"
+#include <vector>
+
+namespace stellar
+{
+
+class Application;
+
+namespace InvariantTestUtils
+{
+
+LedgerEntry generateRandomAccount(uint32_t ledgerSeq);
+
+typedef std::vector<std::tuple<EntryFrame::pointer, EntryFrame::pointer>>
+    UpdateList;
+bool store(Application& app, UpdateList const& apply,
+           LedgerDelta* ldPtr = nullptr,
+           OperationResult const* resPtr = nullptr);
+}
+}
