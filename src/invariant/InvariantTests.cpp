f82cf31389b29ab468f7133f6c87ba4c6b052753
Jon Jove <jon@stellar.org>
2017-12-05 11:26:40 -0600

    Fixed incorrect copyright date in BucketListIsConsistentWithDatabaseTests.cpp and InvariantTests.cpp

--- src/invariant/InvariantTests.cpp
+++ src/invariant/InvariantTests.cpp
@@ -1,4 +1,4 @@
-// Copyright 2014 Stellar Development Foundation and contributors. Licensed
+// Copyright 2017 Stellar Development Foundation and contributors. Licensed
 // under the Apache License, Version 2.0. See the COPYING file at the root
 // of this distribution or at http://www.apache.org/licenses/LICENSE-2.0
 
