5e1f141e5dff00f9c7bd6c5672dd17d00d65d1d5
marta-lokhova <marta@stellar.org>
2018-03-26 16:46:13 -0700

    Branched-cycle scalability test: remove unnecessary increment

--- src/simulation/CoreTests.cpp
+++ src/simulation/CoreTests.cpp
@@ -621,7 +621,7 @@ TEST_CASE("Branched-cycle nodes vs. network traffic", "[scalability][hide]")
             numNodes, 1.0, Simulation::OVER_LOOPBACK,
             sha256(fmt::format("nodes-{:d}", numNodes)),
             [](int cfgCount) -> Config {
-                Config res = getTestConfig(cfgCount++);
+                Config res = getTestConfig(cfgCount);
                 res.ARTIFICIALLY_ACCELERATE_TIME_FOR_TESTING = true;
                 res.MAX_PEER_CONNECTIONS = 1000;
                 return res;
