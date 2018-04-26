12adce2046e568d690f3e0f03da5311ef95abc8f
MonsieurNicolas <nicolas@stellar.org>
2018-04-25 17:45:56 -0700

    SCP: walk slots from most recent when computing "isNodeInQuorum"
    most recent slot should take precedence when it comes to quorum information.

--- src/scp/SCP.cpp
+++ src/scp/SCP.cpp
@@ -258,9 +258,11 @@ SCP::TriBool
 SCP::isNodeInQuorum(NodeID const& node)
 {
     TriBool res = TB_MAYBE;
-    for (auto& s : mKnownSlots)
+    // iterate in reverse order as the most recent slots are authoritative over
+    // older ones
+    for (auto it = mKnownSlots.rbegin(); it != mKnownSlots.rend(); it++)
     {
-        auto slot = s.second;
+        auto slot = it->second;
         res = slot->isNodeInQuorum(node);
         if (res == TB_TRUE || res == TB_FALSE)
         {
