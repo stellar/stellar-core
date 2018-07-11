b76e667a78165068e93ba691499b67588d620d27
marta-lokhova <marta@stellar.org>
2018-07-10 17:04:38 -0700

    Added work abort tests

--- src/work/WorkTests.cpp
+++ src/work/WorkTests.cpp
@@ -221,7 +221,7 @@ class WorkWith2Subworks : public Work
     bool mCalledSuccessWithPendingSubwork{false};
 };
 
-TEST_CASE("sub-subwork items succed at the same time", "[work]")
+TEST_CASE("sub-subwork items succeed at the same time", "[work]")
 {
     VirtualClock clock;
     auto const& cfg = getTestConfig();
@@ -253,4 +253,125 @@ TEST_CASE("sub-subwork items succed at the same time", "[work]")
     }
 
     REQUIRE(!work1->mCalledSuccessWithPendingSubwork);
+}
+
+TEST_CASE("subwork triggers abort", "[work][workabort]")
+{
+    VirtualClock clock;
+    auto const& cfg = getTestConfig();
+    auto app = createTestApplication(clock, cfg);
+    auto& wm = app->getWorkManager();
+    auto w = wm.addWork<Work>("main-work");
+
+    SECTION("other child pending")
+    {
+        auto failWork = w->addWork<FailingWork>("work-1");
+        auto normalWork = w->addWork<WorkDoNothing>("work-2");
+        failWork->advance();
+        while (!wm.allChildrenDone())
+        {
+            clock.crank();
+        }
+
+        REQUIRE(normalWork->getState() == Work::WORK_FAILURE_ABORTED);
+        REQUIRE(w->getState() == Work::WORK_FAILURE_RAISE);
+    }
+    SECTION("other child already succeeded")
+    {
+        auto failWork = w->addWork<FailingWork>("work-1");
+        auto normalWork = w->addWork<WorkDoNothing>("work-2");
+        normalWork->forceSuccess();
+
+        while (!wm.allChildrenDone())
+        {
+            clock.crank();
+        }
+
+        REQUIRE(normalWork->getState() == Work::WORK_SUCCESS);
+        REQUIRE(w->getState() == Work::WORK_FAILURE_RAISE);
+    }
+    SECTION("other child running")
+    {
+        auto work1 = w->addWork<CountDownWork>(10);
+        wm.advanceChildren();
+        auto work2 = w->addWork<FailingWork>("work-2");
+        wm.advanceChildren();
+
+        while (!wm.allChildrenDone())
+        {
+            clock.crank();
+        }
+
+        REQUIRE(work1->getState() == Work::WORK_SUCCESS);
+        REQUIRE(work2->getState() == Work::WORK_FAILURE_RAISE);
+        REQUIRE(w->getState() == Work::WORK_FAILURE_RAISE);
+    }
+}
+
+TEST_CASE("work manual abort", "[work][workabort]")
+{
+    VirtualClock clock;
+    auto const& cfg = getTestConfig();
+    auto app = createTestApplication(clock, cfg);
+    auto& wm = app->getWorkManager();
+    auto w = wm.addWork<Work>("main-work");
+
+    auto work1 = w->addWork<WorkDoNothing>("work-1");
+    auto work2 = w->addWork<WorkDoNothing>("work-2");
+
+    SECTION("main work aborts")
+    {
+        w->abort();
+        while (!wm.allChildrenDone())
+        {
+            clock.crank();
+        }
+
+        REQUIRE(work1->getState() == Work::WORK_FAILURE_ABORTED);
+        REQUIRE(work2->getState() == Work::WORK_FAILURE_ABORTED);
+        REQUIRE(w->getState() == Work::WORK_FAILURE_ABORTED);
+    }
+    SECTION("abort propagates to the top")
+    {
+        work2->abort();
+        while (!wm.allChildrenDone())
+        {
+            clock.crank();
+        }
+
+        REQUIRE(work1->getState() == Work::WORK_FAILURE_ABORTED);
+        REQUIRE(work2->getState() == Work::WORK_FAILURE_ABORTED);
+        REQUIRE(w->getState() == Work::WORK_FAILURE_ABORTED);
+    }
+}
+
+TEST_CASE("work abort with complex structure", "[work][workabort]")
+{
+    VirtualClock clock;
+    auto const& cfg = getTestConfig();
+    auto app = createTestApplication(clock, cfg);
+    auto& wm = app->getWorkManager();
+    auto w = wm.addWork<Work>("main-work");
+
+    auto work1 = w->addWork<Work>("work-1");
+    auto work2 = w->addWork<Work>("work-2");
+    auto work3 = w->addWork<Work>("work-3");
+
+    auto w1_work4 = work1->addWork<Work>("work-4");
+    auto w1_work5 = work1->addWork<Work>("work-5");
+
+    auto w2_work6 = work2->addWork<FailingWork>("work-6");
+
+    work2->advance();
+    while (!wm.allChildrenDone())
+    {
+        clock.crank();
+    }
+
+    REQUIRE(work1->getState() == Work::WORK_FAILURE_ABORTED);
+    REQUIRE(w1_work4->getState() == Work::WORK_FAILURE_ABORTED);
+    REQUIRE(w1_work5->getState() == Work::WORK_FAILURE_ABORTED);
+    REQUIRE(work2->getState() == Work::WORK_FAILURE_RAISE);
+    REQUIRE(w2_work6->getState() == Work::WORK_FAILURE_RAISE);
+    REQUIRE(work3->getState() == Work::WORK_FAILURE_ABORTED);
 }
