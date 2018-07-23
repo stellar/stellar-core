b8b1a357219d6f3f2b9d5c771088c55ca9dd02f2
Rafał Malinowski <rafal.przemyslaw.malinowski@gmail.com>
2018-07-23 11:00:08 +0200

    move command line handling to DeprecatedCommandLine file
    Signed-off-by: Rafał Malinowski <rafal.przemyslaw.malinowski@gmail.com>

---
+++ src/main/DeprecatedCommandLine.h
@@ -0,0 +1,11 @@
+#pragma once
+
+// Copyright 2018 Stellar Development Foundation and contributors. Licensed
+// under the Apache License, Version 2.0. See the COPYING file at the root
+// of this distribution or at http://www.apache.org/licenses/LICENSE-2.0
+
+namespace stellar
+{
+
+int handleDeprecatedCommandLine(int argc, char* const* argv);
+}
