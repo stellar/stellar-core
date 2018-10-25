9783a6547a7073ef881ea516b4d5d47bf4699e0b
ashevin <avi.shevin@kik.com>
2018-10-25 09:44:16 +0300

    Sort and filter by whitelisted versus unwhitelisted.
    A hard-coded reserve for unwhitelisted is honored.
    When the amount of whitelisted does not use available capacity, unused space is used for unwhitelisted.

--- src/herder/TxSetFrame.cpp
+++ src/herder/TxSetFrame.cpp
@@ -137,7 +137,9 @@ TxSetFrame::sortForApply()
 struct SurgeSorter
 {
     map<AccountID, double>& mAccountFeeMap;
-    SurgeSorter(map<AccountID, double>& afm) : mAccountFeeMap(afm)
+    bool mWhitelisted;
+    SurgeSorter(map<AccountID, double>& afm, bool whitelisted)
+        : mAccountFeeMap(afm), mWhitelisted(whitelisted)
     {
     }
 
@@ -146,6 +148,10 @@ struct SurgeSorter
     {
         if (tx1->getSourceID() == tx2->getSourceID())
             return tx1->getSeqNum() < tx2->getSeqNum();
+
+		if (mWhitelisted)
+            return tx1->getSourceID() < tx2->getSourceID();
+        
         double fee1 = mAccountFeeMap[tx1->getSourceID()];
         double fee2 = mAccountFeeMap[tx2->getSourceID()];
         if (fee1 == fee2)
@@ -157,11 +163,33 @@ struct SurgeSorter
 void
 TxSetFrame::surgePricingFilter(LedgerManager const& lm, Application& app)
 {
+    auto whitelist = Whitelist(app);
+
     size_t max = lm.getMaxTxSetSize();
     if (mTransactions.size() > max)
     { // surge pricing in effect!
         CLOG(WARNING, "Herder")
             << "surge pricing in effect! " << mTransactions.size();
+
+		// to be configurable
+        auto UNWHITELISTED_RATIO = 0.05;
+
+		// number of entries reserved for unwhitelisted txs
+        size_t reserveCapacity =
+            std::min((size_t)1, (size_t)trunc(UNWHITELISTED_RATIO * max));
+
+        // partition by whitelisting
+        std::vector<TransactionFramePtr> whitelisted;
+        std::vector<TransactionFramePtr> unwhitelisted;
+
+        for (auto& tx : mTransactions)
+        {
+            if (whitelist.isWhitelisted(tx->getEnvelope().signatures,
+                                        tx->getContentsHash()))
+                whitelisted.emplace_back(tx);
+            else
+                unwhitelisted.emplace_back(tx);
+        }
 
         // determine the fee ratio for each account
         map<AccountID, double> accountFeeMap;
@@ -175,12 +203,35 @@ TxSetFrame::surgePricingFilter(LedgerManager const& lm, Application& app)
                 accountFeeMap[tx->getSourceID()] = r;
         }
 
-        // sort tx by amount of fee they have paid
-        // remove the bottom that aren't paying enough
-        std::vector<TransactionFramePtr> tempList = mTransactions;
-        std::sort(tempList.begin(), tempList.end(), SurgeSorter(accountFeeMap));
+		// sort whitelisted by sourceID and seqNum
+        std::vector<TransactionFramePtr> tempWhitelisted = whitelisted;
+        std::sort(tempWhitelisted.begin(), tempWhitelisted.end(),
+                  SurgeSorter(accountFeeMap, true));
 
-        for (auto iter = tempList.begin() + max; iter != tempList.end(); iter++)
+		if (tempWhitelisted.size() > (max - reserveCapacity))
+            for (auto iter = tempWhitelisted.begin() + (max - reserveCapacity); iter != tempWhitelisted.end();
+                 iter++)
+            {
+                removeTx(*iter);
+            }
+
+		// calculate available unwhitelisted capacity
+        size_t extraWhitelistCapacity = std::max(
+			(size_t)0, (max - reserveCapacity) - whitelisted.size());
+        size_t totalCapacity = reserveCapacity + extraWhitelistCapacity;
+
+        // exit early, if the count of unwhitelisted is within the
+        // available capacity
+		if (unwhitelisted.size() <= totalCapacity)
+			return;
+
+		// sort unwhitelisted txs by amount of fee they have paid
+        // remove the bottom that aren't paying enough
+        std::vector<TransactionFramePtr> tempList = unwhitelisted;
+        std::sort(tempList.begin(), tempList.end(),
+                  SurgeSorter(accountFeeMap, false));
+
+        for (auto iter = tempList.begin() + totalCapacity; iter != tempList.end(); iter++)
         {
             removeTx(*iter);
         }
