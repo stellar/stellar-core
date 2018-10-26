53db5c6bcd9019e3fe745889b55e9ab16ef1904a
marta-lokhova <marta@stellar.org>
2018-10-26 16:22:26 -0700

    Refactor RunCommandWork and its implementers to support new Work interface

--- src/historywork/RunCommandWork.cpp
+++ src/historywork/RunCommandWork.cpp
@@ -9,36 +9,51 @@
 namespace stellar
 {
 
-RunCommandWork::RunCommandWork(Application& app, WorkParent& parent,
-                               std::string const& uniqueName, size_t maxRetries)
-    : Work(app, parent, uniqueName, maxRetries)
+RunCommandWork::RunCommandWork(Application& app, std::string const& name,
+                               size_t maxRetries)
+    : BasicWork(app, name, maxRetries)
 {
 }
 
-RunCommandWork::~RunCommandWork()
+BasicWork::State
+RunCommandWork::onRun()
 {
-    clearChildren();
-}
-
-void
-RunCommandWork::onStart()
-{
-    std::string cmd, outfile;
-    getCommand(cmd, outfile);
-    if (!cmd.empty())
+    if (mDone)
     {
-        auto exit = mApp.getProcessManager().runProcess(cmd, outfile);
-        exit.async_wait(callComplete());
+        return mEc ? State::WORK_FAILURE : State::WORK_SUCCESS;
     }
     else
     {
-        scheduleSuccess();
+        std::string cmd, outfile;
+        std::tie(cmd, outfile) = getCommand();
+        if (!cmd.empty())
+        {
+            auto exit = mApp.getProcessManager().runProcess(cmd, outfile);
+
+            std::weak_ptr<RunCommandWork> weak(
+                std::static_pointer_cast<RunCommandWork>(shared_from_this()));
+            exit.async_wait([weak](asio::error_code const& ec) {
+                auto self = weak.lock();
+                if (self)
+                {
+                    self->mEc = ec;
+                    self->mDone = true;
+                    self->wakeUp();
+                }
+            });
+            return State::WORK_WAITING;
+        }
+        else
+        {
+            return State::WORK_SUCCESS;
+        }
     }
 }
 
 void
-RunCommandWork::onRun()
+RunCommandWork::onReset()
 {
-    // Do nothing: we ran the command in onStart().
+    mDone = false;
+    mEc = asio::error_code();
 }
 }
