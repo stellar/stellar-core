55cbfd7e05b5bc7b04dd4e04b874848695418e38
marta-lokhova <marta@stellar.org>
2018-10-26 16:10:58 -0700

    Introduce BasicWork class to handle work state transitions

---
+++ src/work/BasicWork.cpp
@@ -0,0 +1,351 @@
+// Copyright 2018 Stellar Development Foundation and contributors. Licensed
+// under the Apache License, Version 2.0. See the COPYING file at the root
+// of this distribution or at http://www.apache.org/licenses/LICENSE-2.0
+
+#include "work/BasicWork.h"
+#include "lib/util/format.h"
+#include "util/Logging.h"
+#include "util/Math.h"
+
+namespace stellar
+{
+
+size_t const BasicWork::RETRY_NEVER = 0;
+size_t const BasicWork::RETRY_ONCE = 1;
+size_t const BasicWork::RETRY_A_FEW = 5;
+size_t const BasicWork::RETRY_A_LOT = 32;
+size_t const BasicWork::RETRY_FOREVER = 0xffffffff;
+
+std::set<BasicWork::Transition> const BasicWork::ALLOWED_TRANSITIONS = {
+    Transition(InternalState::PENDING, InternalState::RUNNING),
+    Transition(InternalState::PENDING, InternalState::DESTRUCTING),
+    Transition(InternalState::RUNNING, InternalState::RUNNING),
+    Transition(InternalState::RUNNING, InternalState::WAITING),
+    Transition(InternalState::RUNNING, InternalState::SUCCESS),
+    Transition(InternalState::RUNNING, InternalState::FAILURE),
+    Transition(InternalState::RUNNING, InternalState::RETRYING),
+    Transition(InternalState::RUNNING, InternalState::DESTRUCTING),
+    Transition(InternalState::WAITING, InternalState::RUNNING),
+    Transition(InternalState::WAITING, InternalState::DESTRUCTING),
+    Transition(InternalState::RETRYING, InternalState::WAITING),
+    Transition(InternalState::SUCCESS, InternalState::DESTRUCTING),
+    Transition(InternalState::SUCCESS, InternalState::PENDING),
+    Transition(InternalState::FAILURE, InternalState::DESTRUCTING),
+    Transition(InternalState::FAILURE, InternalState::PENDING),
+};
+
+BasicWork::BasicWork(Application& app, std::string name, size_t maxRetries)
+    : mApp(app), mName(std::move(name)), mMaxRetries(maxRetries)
+{
+}
+
+BasicWork::~BasicWork()
+{
+}
+
+void
+BasicWork::shutdown()
+{
+    mState = InternalState::DESTRUCTING;
+    reset();
+}
+
+std::string const&
+BasicWork::getName() const
+{
+    return mName;
+}
+
+std::string
+BasicWork::getStatus() const
+{
+    // Work is in `WAITING` state when retrying
+    auto state = mRetryTimer ? InternalState::RETRYING : mState;
+
+    switch (state)
+    {
+    case InternalState::PENDING:
+        return fmt::format("Ready to run: {:s}", getName());
+    case InternalState::RUNNING:
+        return fmt::format("Running: {:s}", getName());
+    case InternalState::WAITING:
+        return fmt::format("Waiting: {:s}", getName());
+    case InternalState::SUCCESS:
+        return fmt::format("Succeeded: {:s}", getName());
+    case InternalState::RETRYING:
+    {
+        auto eta = getRetryETA();
+        return fmt::format("Retrying in {:d} sec: {:s}", eta, getName());
+    }
+    case InternalState::FAILURE:
+        return fmt::format("Failed: {:s}", getName());
+    case InternalState::DESTRUCTING:
+        return fmt::format("Destructing: {:s}", getName());
+    }
+}
+
+bool
+BasicWork::isDone() const
+{
+    return mState == InternalState::SUCCESS || mState == InternalState::FAILURE;
+}
+
+std::string
+BasicWork::stateName(InternalState st)
+{
+    switch (st)
+    {
+    case InternalState::WAITING:
+        return "WORK_WAITING";
+    case InternalState::RUNNING:
+        return "WORK_RUNNING";
+    case InternalState::SUCCESS:
+        return "WORK_SUCCESS";
+    case InternalState::FAILURE:
+        return "WORK_FAILURE";
+    case InternalState::PENDING:
+        return "WORK_PENDING";
+    case InternalState::DESTRUCTING:
+        return "WORK_DESTRUCTING";
+    case InternalState::RETRYING:
+        return "WORK_RETRYING";
+    }
+}
+
+void
+BasicWork::reset()
+{
+    CLOG(DEBUG, "Work") << "resetting " << getName();
+
+    if (mRetryTimer)
+    {
+        mRetryTimer->cancel();
+        mRetryTimer.reset();
+    }
+
+    onReset();
+}
+
+void
+BasicWork::startWork(std::function<void()> wakeUpParent)
+{
+    if (mState != InternalState::PENDING)
+    {
+        // Only restart if work is in terminal state
+        assert(isDone());
+        setState(InternalState::PENDING);
+    }
+
+    mNotifyCallback = wakeUpParent;
+    setState(InternalState::RUNNING);
+}
+
+void
+BasicWork::waitForRetry()
+{
+    if (mRetryTimer)
+    {
+        throw std::runtime_error(
+            fmt::format("Retry timer for {} already exists!", getName()));
+    }
+
+    mRetryTimer = std::make_unique<VirtualTimer>(mApp.getClock());
+    std::weak_ptr<BasicWork> weak = shared_from_this();
+    auto t = getRetryDelay();
+    mRetryTimer->expires_from_now(t);
+    CLOG(WARNING, "Work")
+        << "Scheduling retry #" << (mRetries + 1) << "/" << mMaxRetries
+        << " in " << std::chrono::duration_cast<std::chrono::seconds>(t).count()
+        << " sec, for " << getName();
+    setState(InternalState::WAITING);
+    mRetryTimer->async_wait([weak](asio::error_code const& ec) {
+        auto self = weak.lock();
+        if (!self)
+        {
+            return;
+        }
+        assert(self->mState == InternalState::WAITING ||
+               self->mState == InternalState::DESTRUCTING);
+        if (self->mState == InternalState::WAITING)
+        {
+            self->mRetries++;
+            self->mRetryTimer.reset();
+            self->wakeUp();
+        }
+    });
+}
+
+void
+BasicWork::onReset()
+{
+}
+
+void
+BasicWork::onSuccess()
+{
+}
+
+void
+BasicWork::onFailureRetry()
+{
+}
+
+void
+BasicWork::onFailureRaise()
+{
+}
+
+void
+BasicWork::onWakeUp()
+{
+}
+
+BasicWork::State
+BasicWork::getState() const
+{
+    switch (mState)
+    {
+    case InternalState::RUNNING:
+    case InternalState::PENDING:
+    case InternalState::DESTRUCTING:
+        return State::WORK_RUNNING;
+    case InternalState::WAITING:
+    case InternalState::RETRYING:
+        return State::WORK_WAITING;
+    case InternalState::SUCCESS:
+        return State::WORK_SUCCESS;
+    case InternalState::FAILURE:
+        return State::WORK_FAILURE;
+    }
+}
+
+void
+BasicWork::setState(InternalState st)
+{
+    auto maxR = getMaxRetries();
+    if (st == InternalState::FAILURE && (mRetries < maxR))
+    {
+        st = InternalState::RETRYING;
+    }
+
+    assertValidTransition(Transition(mState, st));
+    if (mState == InternalState::PENDING && st == InternalState::RUNNING)
+    {
+        reset();
+    }
+
+    if (mState != st)
+    {
+        CLOG(DEBUG, "Work") << "work " << getName() << " : "
+                            << stateName(mState) << " -> " << stateName(st);
+        mState = st;
+    }
+
+    switch (mState)
+    {
+    case InternalState::SUCCESS:
+        onSuccess();
+        break;
+    case InternalState::FAILURE:
+        onFailureRaise();
+        reset();
+        break;
+    case InternalState::RETRYING:
+        onFailureRetry();
+        reset();
+        waitForRetry();
+        break;
+    default:
+        break;
+    }
+}
+
+void
+BasicWork::wakeUp()
+{
+    if (mState == InternalState::RUNNING ||
+        mState == InternalState::DESTRUCTING)
+    {
+        return;
+    }
+    setState(InternalState::RUNNING);
+    onWakeUp();
+    if (mNotifyCallback)
+    {
+        mNotifyCallback();
+    }
+}
+
+std::function<void()>
+BasicWork::wakeUpCallback()
+{
+    std::weak_ptr<BasicWork> weak = shared_from_this();
+    auto callback = [weak]() {
+        auto self = weak.lock();
+        if (self)
+        {
+            self->wakeUp();
+        }
+    };
+    return callback;
+}
+
+void
+BasicWork::crankWork()
+{
+    assert(!isDone() && mState != InternalState::WAITING);
+
+    auto nextState = onRun();
+    setState(getInternalState(nextState));
+}
+
+size_t
+BasicWork::getMaxRetries() const
+{
+    return mMaxRetries;
+}
+
+VirtualClock::duration
+BasicWork::getRetryDelay() const
+{
+    // Cap to 4096sec == a little over an hour.
+    uint64_t m = 2 << std::min(uint64_t(12), uint64_t(mRetries));
+    return std::chrono::seconds(rand_uniform<uint64_t>(1ULL, m));
+}
+
+uint64_t
+BasicWork::getRetryETA() const
+{
+    uint64_t now = mApp.timeNow();
+    uint64_t retry =
+        mRetryTimer ? VirtualClock::to_time_t(mRetryTimer->expiry_time()) : 0;
+    return now > retry ? 0 : retry - now;
+}
+
+void
+BasicWork::assertValidTransition(Transition const& t) const
+{
+    if (ALLOWED_TRANSITIONS.find(t) == ALLOWED_TRANSITIONS.end())
+    {
+        throw std::runtime_error(
+            fmt::format("BasicWork error: illegal state transition {} -> {}",
+                        stateName(t.first), stateName(t.second)));
+    }
+}
+
+BasicWork::InternalState
+BasicWork::getInternalState(State s) const
+{
+    switch (s)
+    {
+    case State::WORK_SUCCESS:
+        return InternalState::SUCCESS;
+    case State::WORK_FAILURE:
+        return InternalState::FAILURE;
+    case State::WORK_WAITING:
+        return InternalState::WAITING;
+    case State::WORK_RUNNING:
+        return InternalState::RUNNING;
+    }
+}
+}
---
+++ src/work/BasicWork.h
@@ -0,0 +1,153 @@
+// Copyright 2018 Stellar Development Foundation and contributors. Licensed
+// under the Apache License, Version 2.0. See the COPYING file at the root
+// of this distribution or at http://www.apache.org/licenses/LICENSE-2.0
+#pragma once
+
+#include "main/Application.h"
+#include <set>
+
+namespace stellar
+{
+
+class Application;
+
+/** BasicWork is an implementation of a finite state machine,
+ * that is used for async or long-running tasks that:
+ *  - May need to be broken into steps, so as not to block the main thread
+ *  - May fail and need to retry, after some delay
+ *
+ *  BasicWork manages all state transitions via `crankWork` and `setState`.
+ *  It also supports a retry mechanism internal to BasicWork.
+ *  While customers can trigger cranking and check on BasicWork's status,
+ *  it is implementers' responsibility to implement `onRun`, that
+ *  hints the next desired state.
+ */
+
+class BasicWork : public std::enable_shared_from_this<BasicWork>,
+                  private NonMovableOrCopyable
+{
+  public:
+    static size_t const RETRY_NEVER;
+    static size_t const RETRY_ONCE;
+    static size_t const RETRY_A_FEW;
+    static size_t const RETRY_A_LOT;
+    static size_t const RETRY_FOREVER;
+
+    // Publicly exposed state of work:
+    // WORK_RUNNING - work has been created and is currently in progress
+    // (note that this might mean that work is being destructed also).
+    // Implementers return RUNNING when work needs to be scheduled to run more.
+    // WORK_WAITING - work should not be scheduled to run, as it's waiting
+    // for some event (process exit, retry timeout, etc)
+    // Implementers return WAITING when they don't want the work to be
+    // scheduled. Note that it is implementers' responsibility then to correctly
+    // use `wakeUp` to exit WAITING state. WORK_SUCCESS - terminal successful
+    // state WORK_FAILURE - terminal unsuccessful state. When this state is
+    // returned, depending on the Work's retry strategy, either a retry will be
+    // scheduled or work will cease execution.
+    enum class State
+    {
+        WORK_RUNNING,
+        WORK_WAITING,
+        WORK_SUCCESS,
+        WORK_FAILURE,
+    };
+
+    BasicWork(Application& app, std::string name, size_t maxRetries);
+    virtual ~BasicWork();
+
+    std::string const& getName() const;
+    virtual std::string getStatus() const;
+    State getState() const;
+    bool isDone() const;
+
+    size_t getMaxRetries() const;
+    uint64_t getRetryETA() const;
+
+    // Main method for state transition, mostly dictated by `onRun`
+    void crankWork();
+
+    // Reset work to its initial state (only if work is in terminal state)
+    void startWork(std::function<void()> wakeUpParent);
+    virtual void shutdown();
+
+  protected:
+    // Implementers can override these callbacks to customize functionality
+    // `onReset` is expected to restore work's state to its initial condition;
+    // this includes cleaning up side effects (e.g. produced files), restoring
+    // member variables etc.
+    // `onRun` performs necessary work logic, and hints the
+    // next state transition. `onSuccess` is called when work transitions into
+    // WORK_SUCCESS state. Similarly, `onFailure*` methods are called upon
+    // transitioning into appropriate failure state. An example usage would be
+    // cleaning up side effects, like downloaded files in these methods. If you
+    // want to force failure (and reset / retry) you can return
+    // WORK_FAILURE_RETRY or WORK_FAILURE_RAISE. After a retry count is
+    // passed, WORK_FAILURE_RETRY means WORK_FAILURE_RAISE anyways.
+
+    virtual void onReset();
+    virtual State onRun() = 0;
+    virtual void onWakeUp();
+    virtual void onFailureRetry();
+    virtual void onFailureRaise();
+    virtual void onSuccess();
+
+    // A helper method that implementers can use if they plan to
+    // utilize WAITING state. This tells the work to return to RUNNING
+    // state, and propagate the notification up to the scheduler
+    // An example use of this would be RunCommandWork:
+    // a timer is used to async_wait for a process to exit,
+    // with a call to `wakeUp` upon completion.
+    void wakeUp();
+
+    // Default wakeUp callback that implementers can use
+    std::function<void()> wakeUpCallback();
+
+    Application& mApp;
+
+  private:
+    // Internally, there are a few more states that aid with
+    // state transitions. Specifically:
+    // PENDING - work has been created but hasn't started yet
+    // RETRYING - `onRun` returned WORK_FAILURE, if there are retries
+    // left, go into RETRYING state
+    // DESTRUCTING - WorkScheduler is shutting down, this state prevents
+    // wakeUp callbacks from messing with BasicWork's state while it is
+    // shutting down
+    enum class InternalState
+    {
+        PENDING,
+        RUNNING,
+        WAITING,
+        DESTRUCTING,
+        RETRYING,
+        SUCCESS,
+        FAILURE,
+    };
+    using Transition = std::pair<InternalState, InternalState>;
+
+    // `reset` is called on transitions like retry, failure, before
+    // restarting successful work and during destruction. This method ensures
+    // that all the references (including children) are properly cleaned up for
+    // safe destruction.
+    void reset();
+
+    VirtualClock::duration getRetryDelay() const;
+    void setState(InternalState s);
+    void waitForRetry();
+    InternalState getInternalState(State s) const;
+    void assertValidTransition(Transition const& t) const;
+    static std::string stateName(InternalState st);
+
+    std::function<void()> mNotifyCallback;
+    std::string const mName;
+    std::unique_ptr<VirtualTimer> mRetryTimer;
+
+    InternalState mState{InternalState::PENDING};
+    size_t mRetries{0};
+    size_t const mMaxRetries{RETRY_A_FEW};
+
+    // Whitelist legal state transitions in work state machine
+    static std::set<Transition> const ALLOWED_TRANSITIONS;
+};
+}
