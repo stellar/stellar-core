cf3627d6932c1cbc3e64ea4f407ca338b4bc9087
MonsieurNicolas <nicolas@stellar.org>
2019-02-15 15:43:24 -0800

    SCP flood test: ensure we don't close ledger during SCP message flooding test

--- src/overlay/FloodTests.cpp
+++ src/overlay/FloodTests.cpp
@@ -207,9 +207,12 @@ TEST_CASE("Flooding", "[flood][overlay]")
         // a valid transaction set
 
         std::vector<SecretKey> keys;
+        std::unordered_map<PublicKey, SecretKey> keysMap;
         for (int i = 0; i < nbTx; i++)
         {
             keys.emplace_back(SecretKey::random());
+            auto& k = keys.back();
+            keysMap.insert(std::make_pair(k.getPublicKey(), k));
         }
 
         auto injectSCP = [&](int i) {
@@ -256,7 +259,6 @@ TEST_CASE("Flooding", "[flood][overlay]")
             prep.ballot.counter = 1;
             prep.quorumSetHash = qSetHash;
 
-            // use the sources to sign the message
             st.nodeID = keys[i].getPublicKey();
             envelope.signature = keys[i].sign(xdr::xdr_to_opaque(
                 inApp->getNetworkID(), ENVELOPE_TYPE_SCP, st));
@@ -275,16 +277,9 @@ TEST_CASE("Flooding", "[flood][overlay]")
             HerderImpl& herder = *static_cast<HerderImpl*>(&app->getHerder());
             auto state =
                 herder.getSCP().getCurrentState(lcl.header.ledgerSeq + 1);
-            for (auto const& s : keys)
-            {
-                if (std::find_if(
-                        state.begin(), state.end(), [&](SCPEnvelope const& e) {
-                            return e.statement.nodeID == s.getPublicKey();
-                        }) != state.end())
-                {
-                    okCount += 1;
-                }
-            }
+            okCount = std::count_if(state.begin(), state.end(), [&](auto& e) {
+                return keysMap.find(e.statement.nodeID) != keysMap.end();
+            });
             bool res = okCount == sources.size();
             LOG(DEBUG) << app->getConfig().PEER_PORT
                        << (res ? " OK " : " BEHIND ") << okCount << " / "
@@ -298,11 +293,13 @@ TEST_CASE("Flooding", "[flood][overlay]")
             SCPQuorumSet sub;
             for (auto const& k : keys)
             {
-                sub.threshold++;
                 sub.validators.emplace_back(k.getPublicKey());
             }
-            resQSet.threshold++;
+            sub.threshold = static_cast<uint32>(sub.validators.size());
             resQSet.innerSets.emplace_back(sub);
+            // threshold causes all nodes to be stuck on current ledger
+            resQSet.threshold = static_cast<uint32>(resQSet.validators.size() +
+                                                    resQSet.innerSets.size());
             return resQSet;
         };
 
@@ -311,14 +308,14 @@ TEST_CASE("Flooding", "[flood][overlay]")
             SECTION("loopback")
             {
                 simulation =
-                    Topologies::core(4, .666f, Simulation::OVER_LOOPBACK,
+                    Topologies::core(4, 1.0f, Simulation::OVER_LOOPBACK,
                                      networkID, cfgGen, quorumAdjuster);
                 test(injectSCP, ackedSCP);
             }
             SECTION("tcp")
             {
                 simulation =
-                    Topologies::core(4, .666f, Simulation::OVER_TCP, networkID,
+                    Topologies::core(4, 1.0f, Simulation::OVER_TCP, networkID,
                                      cfgGen, quorumAdjuster);
                 test(injectSCP, ackedSCP);
             }
