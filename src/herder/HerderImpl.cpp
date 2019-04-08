5b62701bea4b1da56cabde2bbbd722d3f9096a96
Rafał Malinowski <rafal.przemyslaw.malinowski@gmail.com>
2019-04-08 10:39:38 +0200

    improve HerderImpl::triggerNextLedger comment
    Signed-off-by: Rafał Malinowski <rafal.przemyslaw.malinowski@gmail.com>

--- src/herder/HerderImpl.cpp
+++ src/herder/HerderImpl.cpp
@@ -739,7 +739,7 @@ HerderImpl::triggerNextLedger(uint32_t ledgerSeqToTrigger)
     }
 
     // our first choice for this round's set is all the tx we have collected
-    // during last ledger close
+    // during last few ledger closes
     auto const& lcl = mLedgerManager.getLastClosedLedgerHeader();
     auto proposedSet = std::make_shared<TxSetFrame>(lcl.hash);
 
