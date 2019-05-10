stellar/stellar-core

commit 9356a10fe7a535156323f51a314935d1b60984d3

changes on /quorum?fullkeys=true

src/scp/SCPDriver.cpp
@@ -24,9 +24,9 @@ SCPDriver::getValueString(Value const& v) const
 }
 
 std::string
-SCPDriver::toStrKey(PublicKey const& pk) const
+SCPDriver::toStrKey(PublicKey const& pk, bool fullKey) const
 {
-    return KeyUtils::toStrKey(pk);
+    return fullKey ? KeyUtils::toStrKey(pk) : SCPDriver::toShortString(pk);
 }
 
 std::string

src/scp/BallotProtocol.cpp
@@ -2036,7 +2036,7 @@ BallotProtocol::getJsonQuorumInfo(NodeID const& id, bool summary, bool fullKeys)
         {
             if (!summary)
             {
-                missing.append(mSlot.getSCPDriver().toShortString(n));
+                missing.append(mSlot.getSCPDriver().toStrKey(n, fullKeys));
             }
             n_missing++;
         }
@@ -2053,7 +2053,8 @@ BallotProtocol::getJsonQuorumInfo(NodeID const& id, bool summary, bool fullKeys)
                 {
                     if (!summary)
                     {
-                        delayed.append(mSlot.getSCPDriver().toShortString(n));
+                        delayed.append(
+                            mSlot.getSCPDriver().toStrKey(n, fullKeys));
                     }
                     n_delayed++;
                 }
@@ -2062,7 +2063,7 @@ BallotProtocol::getJsonQuorumInfo(NodeID const& id, bool summary, bool fullKeys)
             {
                 if (!summary)
                 {
-                    disagree.append(mSlot.getSCPDriver().toShortString(n));
+                    disagree.append(mSlot.getSCPDriver().toStrKey(n, fullKeys));
                 }
                 n_disagree++;
             }
@@ -2088,10 +2089,7 @@ BallotProtocol::getJsonQuorumInfo(NodeID const& id, bool summary, bool fullKeys)
         auto& f_ex = ret["fail_with"];
         for (auto const& n : f)
         {
-            std::string nodeID = fullKeys
-                                     ? mSlot.getSCPDriver().toStrKey(n)
-                                     : mSlot.getSCPDriver().toShortString(n);
-            f_ex.append(nodeID);
+            f_ex.append(mSlot.getSCPDriver().toStrKey(n, fullKeys));
         }
         ret["value"] = getLocalNode()->toJson(*qSet, fullKeys);
     }

src/scp/LocalNode.cpp
@@ -378,9 +378,7 @@ LocalNode::toJson(SCPQuorumSet const& qSet, bool fullKeys) const
     auto& entries = ret["v"];
     for (auto const& v : qSet.validators)
     {
-        std::string nodeID = fullKeys ? mSCP->getDriver().toStrKey(v)
-                                      : mSCP->getDriver().toShortString(v);
-        entries.append(nodeID);
+        entries.append(mSCP->getDriver().toStrKey(v, fullKeys));
     }
     for (auto const& s : qSet.innerSets)
     {

src/scp/SCP.cpp
@@ -318,8 +318,7 @@ SCP::envToStr(SCPStatement const& st, bool fullKeys) const
 
     Hash const& qSetHash = Slot::getCompanionQuorumSetHashFromStatement(st);
 
-    std::string nodeId = fullKeys ? mDriver.toStrKey(st.nodeID)
-                                  : mDriver.toShortString(st.nodeID);
+    std::string nodeId = mDriver.toStrKey(st.nodeID, fullKeys);
 
     oss << "{ENV@" << nodeId << " | "
         << " i: " << st.slotIndex;

src/scp/SCPDriver.h
@@ -87,7 +87,8 @@ class SCPDriver
     virtual std::string getValueString(Value const& v) const;
 
     // `toStrKey` returns StrKey encoded string representation
-    virtual std::string toStrKey(PublicKey const& pk) const;
+    virtual std::string toStrKey(PublicKey const& pk,
+                                 bool fullKey = true) const;
 
     // `toShortString` converts to the common name of a key if found
     virtual std::string toShortString(PublicKey const& pk) const;

commit 6ce3c35f3c4aac318b2185dbd2f7d2114b2afdaa

display known keys when displaying short keys
fixes a problem introduced by 9356a10fe7a535156323f51a314935d1b60984d3

src/scp/SCPDriver.cpp
@@ -26,7 +26,7 @@ SCPDriver::getValueString(Value const& v) const
 std::string
 SCPDriver::toStrKey(PublicKey const& pk, bool fullKey) const
 {
-    return fullKey ? KeyUtils::toStrKey(pk) : SCPDriver::toShortString(pk);
+    return fullKey ? KeyUtils::toStrKey(pk) : toShortString(pk);
 }
 
 std::string