af5c49615022433b579357c26114caae53a4b62f
Graydon Hoare <graydon@pobox.com>
2019-06-12 16:07:25 -0700

    SCP: add new quorum intersection checker, with long comment.

---
+++ src/scp/QuorumIntersectionChecker.h
@@ -0,0 +1,28 @@
+#pragma once
+
+// Copyright 2019 Stellar Development Foundation and contributors. Licensed
+// under the Apache License, Version 2.0. See the COPYING file at the root
+// of this distribution or at http://www.apache.org/licenses/LICENSE-2.0
+
+#include "herder/QuorumTracker.h"
+#include <memory>
+
+namespace stellar
+{
+
+class Config;
+
+class QuorumIntersectionChecker
+{
+  public:
+    static std::shared_ptr<QuorumIntersectionChecker>
+    create(stellar::QuorumTracker::QuorumMap const& qmap,
+           stellar::Config const& cfg);
+
+    virtual ~QuorumIntersectionChecker(){};
+    virtual bool networkEnjoysQuorumIntersection() const = 0;
+    virtual size_t getMaxQuorumsFound() const = 0;
+    virtual std::pair<std::vector<PublicKey>, std::vector<PublicKey>>
+    getPotentialSplit() const = 0;
+};
+}
