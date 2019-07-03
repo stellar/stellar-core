6133e50f7695c51e80870c210c30887077a5b293
robertDurst <me@robdurst.com>
2019-07-03 16:40:36 -0700

    allow for deterministic randombytes_buf generation

--- src/crypto/Random.cpp
+++ src/crypto/Random.cpp
@@ -3,6 +3,7 @@
 // of this distribution or at http://www.apache.org/licenses/LICENSE-2.0
 
 #include "crypto/Random.h"
+#include "util/Math.h"
 #include <sodium.h>
 
 #ifdef MSAN_ENABLED
@@ -13,7 +14,16 @@ std::vector<uint8_t>
 randomBytes(size_t length)
 {
     std::vector<uint8_t> vec(length);
+
+#ifdef FUZZING_BUILD_MODE_UNSAFE_FOR_PRODUCTION
+    std::uniform_int_distribution<unsigned short> dist(0, 255);
+    std::generate(vec.begin(), vec.end(), [&]() {
+        return static_cast<uint8_t>(dist(stellar::gRandomEngine));
+    });
+#else
     randombytes_buf(vec.data(), length);
+#endif
+
 #ifdef MSAN_ENABLED
     __msan_unpoison(out.key.data(), out.key.size());
 #endif
