d030098b17967ded968e1f98ad552970741ddd89
Graydon Hoare <graydon@pobox.com>
2019-07-08 19:26:17 -0700

    bucket: add MergeKey type for recording and fetching merges.

---
+++ src/bucket/MergeKey.h
@@ -0,0 +1,46 @@
+#pragma once
+
+// Copyright 2019 Stellar Development Foundation and contributors. Licensed
+// under the Apache License, Version 2.0. See the COPYING file at the root
+// of this distribution or at http://www.apache.org/licenses/LICENSE-2.0
+#include "bucket/Bucket.h"
+#include "xdr/Stellar-types.h"
+#include <cstdint>
+#include <iosfwd>
+#include <vector>
+
+namespace stellar
+{
+// Key type for cache of merges-in-progress. These only exist to enable
+// re-attaching a deserialized FutureBucket to a std::shared_future, or (if the
+// merge is finished and has been promoted to a live bucket) to identify which
+// _output_ was produced from a given set of _inptus_ so we can recreate a
+// pre-resolved std::shared_future containing that output.
+struct MergeKey
+{
+    MergeKey(uint32_t maxProtocolVersion, bool keepDeadEntries,
+             std::shared_ptr<Bucket> const& inputCurr,
+             std::shared_ptr<Bucket> const& inputSnap,
+             std::vector<std::shared_ptr<Bucket>> const& inputShadows);
+
+    MergeKey(uint32_t maxProtocolVersion, bool keepDeadEntries, Hash& inputCurr,
+             Hash& inputSnap, std::vector<Hash> const& inputShadows);
+
+    uint32_t mMaxProtocolVersion;
+    bool mKeepDeadEntries;
+    Hash mInputCurrBucket;
+    Hash mInputSnapBucket;
+    std::vector<Hash> mInputShadowBuckets;
+    bool operator==(MergeKey const& other) const;
+};
+
+std::ostream& operator<<(std::ostream& out, MergeKey const& b);
+}
+
+namespace std
+{
+template <> struct hash<stellar::MergeKey>
+{
+    size_t operator()(stellar::MergeKey const& k) const noexcept;
+};
+}
