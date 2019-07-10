86d9eca1bf911d0c9942c07507d5f425590a0416
Graydon Hoare <graydon@pobox.com>
2019-07-09 17:08:16 -0700

    bucket: add BucketMergeMap type to record weak input/output merge mappings.

---
+++ src/bucket/BucketMergeMap.cpp
@@ -0,0 +1,127 @@
+// Copyright 2019 Stellar Development Foundation and contributors. Licensed
+// under the Apache License, Version 2.0. See the COPYING file at the root
+// of this distribution or at http://www.apache.org/licenses/LICENSE-2.0
+
+#include "bucket/BucketMergeMap.h"
+#include "crypto/Hex.h"
+#include "util/Logging.h"
+
+namespace
+{
+std::unordered_set<stellar::Hash>
+getMergeKeyHashes(stellar::MergeKey const& key)
+{
+    std::unordered_set<stellar::Hash> hashes;
+    hashes.emplace(key.mInputCurrBucket);
+    hashes.emplace(key.mInputSnapBucket);
+    for (auto const& in : key.mInputShadowBuckets)
+    {
+        hashes.emplace(in);
+    }
+    return hashes;
+}
+}
+
+namespace stellar
+{
+
+void
+BucketMergeMap::recordMerge(MergeKey const& input, Hash const& output)
+{
+    mMergeKeyToOutput.emplace(input, output);
+    mOutputToMergeKey.emplace(output, input);
+    for (auto const& in : getMergeKeyHashes(input))
+    {
+        CLOG(TRACE, "Bucket") << "BucketMergeMap retaining mapping for "
+                              << hexAbbrev(in) << " -> " << hexAbbrev(output);
+        mInputToOutput.emplace(in, output);
+    }
+}
+
+std::unordered_set<MergeKey>
+BucketMergeMap::forgetAllMergesProducing(Hash const& outputBeingDropped)
+{
+    std::unordered_set<MergeKey> ret;
+    auto mergesProducingOutput =
+        mOutputToMergeKey.equal_range(outputBeingDropped);
+    for (auto mergeProducingOutput = mergesProducingOutput.first;
+         mergeProducingOutput != mergesProducingOutput.second;
+         mergeProducingOutput = mOutputToMergeKey.erase(mergeProducingOutput))
+    {
+        auto const& output = mergeProducingOutput->first;
+        auto const& mergeKeyProducingOutput = mergeProducingOutput->second;
+        assert(output == outputBeingDropped);
+        ret.emplace(mergeKeyProducingOutput);
+
+        // It's possible for the same output to occur for multiple
+        // merge keys (eg. a+b and b+a). And the set of per-input
+        // entries should always be larger than the per-key entries.
+        if ((mOutputToMergeKey.size() > mMergeKeyToOutput.size()) ||
+            (mMergeKeyToOutput.size() > mInputToOutput.size()))
+        {
+            CLOG(WARNING, "Bucket")
+                << "BucketMergeMap inconsistent map sizes: "
+                << "out->in:" << mOutputToMergeKey.size()
+                << ", coarse in->out:" << mMergeKeyToOutput.size()
+                << ", fine in->out:" << mInputToOutput.size();
+        }
+
+        CLOG(TRACE, "Bucket")
+            << "BucketMergeMap forgetting mappings for merge "
+            << mergeKeyProducingOutput
+            << " <-> output=" << hexAbbrev(outputBeingDropped);
+
+        // We first remove all the (in,out) pairs from the decomposed
+        // mapping mInputToOutput, used for rooting the
+        // publish queue.
+        for (auto const& input : getMergeKeyHashes(mergeKeyProducingOutput))
+        {
+            auto mergesUsingInput = mInputToOutput.equal_range(input);
+            for (auto mergeUsingInput = mergesUsingInput.first;
+                 mergeUsingInput != mergesUsingInput.second; ++mergeUsingInput)
+            {
+                auto const& outputUsingInput = mergeUsingInput->second;
+                if (outputUsingInput == outputBeingDropped)
+                {
+                    CLOG(TRACE, "Bucket")
+                        << "BucketMergeMap forgetting mapping for "
+                        << hexAbbrev(input) << " -> "
+                        << hexAbbrev(outputUsingInput);
+                    mInputToOutput.erase(mergeUsingInput);
+                    break;
+                }
+            }
+        }
+        // Then we erase the forward mapping mergeKey => output;
+        // the for-loop-step erases reverse mapping output => mergeKey.
+        mMergeKeyToOutput.erase(mergeKeyProducingOutput);
+    }
+    return ret;
+}
+
+bool
+BucketMergeMap::findMergeFor(MergeKey const& input, Hash& output)
+{
+    auto i = mMergeKeyToOutput.find(input);
+    if (i != mMergeKeyToOutput.end())
+    {
+        output = i->second;
+        return true;
+    }
+    return false;
+}
+
+void
+BucketMergeMap::getOutputsUsingInput(Hash const& input,
+                                     std::set<Hash>& outputs) const
+{
+    auto pair = mInputToOutput.equal_range(input);
+    for (auto i = pair.first; i != pair.second; ++i)
+    {
+        outputs.emplace(i->second);
+        CLOG(TRACE, "Bucket")
+            << hexAbbrev(i->second) << " referenced as output of merge of "
+            << hexAbbrev(input);
+    }
+}
+}
---
+++ src/bucket/BucketMergeMap.h
@@ -0,0 +1,49 @@
+#pragma once
+
+#include "bucket/MergeKey.h"
+#include "util/HashOfHash.h"
+#include "xdr/Stellar-types.h"
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+
+// Copyright 2019 Stellar Development Foundation and contributors. Licensed
+// under the Apache License, Version 2.0. See the COPYING file at the root
+// of this distribution or at http://www.apache.org/licenses/LICENSE-2.0
+
+namespace stellar
+{
+
+// Helper type for BucketManager. Stores a bi-directional (weak, multi)mapping
+// relating merge inputs and outputs, along with a decomposition of the map to a
+// finer-grained one that associates each individual input in each MergeKey with
+// all the outputs it's used by.
+class BucketMergeMap
+{
+    // Records bucket input-output relationships for any buckets in that were
+    // built from a merge during the lifetime of this BucketMergeMap (some were
+    // not -- they just showed up from catchup or state reloading). Entries in
+    // this map will be cleared when their _output_ is dropped from the owning
+    // BucketManager's mSharedBuckets, typically due to being unreferenced.
+    std::unordered_map<MergeKey, Hash> mMergeKeyToOutput;
+
+    // Unfortunately to use this correctly in the bucket GC path, we also need
+    // a different version of the same map, keyed by each input separately. And
+    // since one input may be used in _many_ merges, it has to be a multimap.
+    std::unordered_multimap<Hash, Hash> mInputToOutput;
+
+    // Finally we need _another_ map, the opposite direction, to allow us to
+    // erase entries from the previous two maps when we finally decide to drop
+    // a bucket because nobody's going to reattach to it as far as we can tell.
+    //
+    // This also has to be a multimap because the same output can be produced
+    // by multiple MergeKeys.
+    std::unordered_multimap<Hash, MergeKey> mOutputToMergeKey;
+
+  public:
+    void recordMerge(MergeKey const& input, Hash const& output);
+    std::unordered_set<MergeKey> forgetAllMergesProducing(Hash const& output);
+    bool findMergeFor(MergeKey const& input, Hash& output);
+    void getOutputsUsingInput(Hash const& input, std::set<Hash>& outputs) const;
+};
+}
