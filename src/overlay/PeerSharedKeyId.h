d7abc221669c5cd5dd79723e970939ad7a53c35e
Siddharth Suresh <siddharth@stellar.org>
2019-11-26 15:09:54 -0800

    Implemented encrypt/decrypt methods in Curve25519(formerly known as ECDH)

--- src/overlay/PeerSharedKeyId.h
+++ src/overlay/PeerSharedKeyId.h
@@ -4,7 +4,7 @@
 // under the Apache License, Version 2.0. See the COPYING file at the root
 // of this distribution or at http://www.apache.org/licenses/LICENSE-2.0
 
-#include "crypto/ECDH.h"
+#include "crypto/Curve25519.h"
 #include "overlay/Peer.h"
 
 namespace stellar
