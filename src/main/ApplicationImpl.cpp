2bdc933b040ad51182dc308c00138390ce60f39d
Graydon Hoare <graydon@pobox.com>
2019-12-06 11:19:49 -0800

    Delete buckets on shutdown when !modeHasDatabase

--- src/main/ApplicationImpl.cpp
+++ src/main/ApplicationImpl.cpp
@@ -354,6 +354,18 @@ ApplicationImpl::~ApplicationImpl()
     reportCfgMetrics();
     shutdownMainIOContext();
     joinAllThreads();
+    if (!modeHasDatabase(mAppMode))
+    {
+        // Note: BucketManager::dropAll will delete the $BUCKETDIR/tmp which is
+        // where process and history manager write _their_ temp files to, so we
+        // make sure to tear them down first here.
+        mProcessManager = nullptr;
+        mHistoryManager = nullptr;
+        if (mBucketManager)
+        {
+            mBucketManager->dropAll();
+        }
+    }
     LOG(INFO) << "Application destroyed";
 }
 
