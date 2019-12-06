53ba37932760a1ccba8df9b349c7874601cb3d21
Graydon Hoare <graydon@pobox.com>
2019-12-06 11:19:43 -0800

    Add InMemoryLedgerTxnRoot.{cpp,h}

---
+++ src/ledger/InMemoryLedgerTxnRoot.cpp
@@ -0,0 +1,130 @@
+#include "ledger/InMemoryLedgerTxnRoot.h"
+#include "ledger/LedgerRange.h"
+#include "ledger/LedgerTxn.h"
+#include "util/XDROperators.h"
+#include "util/types.h"
+#include "xdr/Stellar-ledger-entries.h"
+#include "xdrpp/marshal.h"
+#include <algorithm>
+
+namespace stellar
+{
+
+InMemoryLedgerTxnRoot::InMemoryLedgerTxnRoot()
+    : mHeader(std::make_unique<LedgerHeader>())
+{
+}
+
+void
+InMemoryLedgerTxnRoot::addChild(AbstractLedgerTxn& child)
+{
+}
+
+void
+InMemoryLedgerTxnRoot::commitChild(EntryIterator iter,
+                                   LedgerTxnConsistency cons)
+{
+    throw std::runtime_error("committing to stub InMemoryLedgerTxnRoot");
+}
+
+void
+InMemoryLedgerTxnRoot::rollbackChild()
+{
+}
+
+std::unordered_map<LedgerKey, LedgerEntry>
+InMemoryLedgerTxnRoot::getAllOffers()
+{
+    return std::unordered_map<LedgerKey, LedgerEntry>();
+}
+
+std::shared_ptr<LedgerEntry const>
+InMemoryLedgerTxnRoot::getBestOffer(Asset const& buying, Asset const& selling)
+{
+    return nullptr;
+}
+
+std::shared_ptr<LedgerEntry const>
+InMemoryLedgerTxnRoot::getBestOffer(Asset const& buying, Asset const& selling,
+                                    OfferDescriptor const& worseThan)
+{
+    return nullptr;
+}
+
+std::unordered_map<LedgerKey, LedgerEntry>
+InMemoryLedgerTxnRoot::getOffersByAccountAndAsset(AccountID const& account,
+                                                  Asset const& asset)
+{
+    return std::unordered_map<LedgerKey, LedgerEntry>();
+}
+
+LedgerHeader const&
+InMemoryLedgerTxnRoot::getHeader() const
+{
+    return *mHeader;
+}
+
+std::vector<InflationWinner>
+InMemoryLedgerTxnRoot::getInflationWinners(size_t maxWinners,
+                                           int64_t minBalance)
+{
+    return std::vector<InflationWinner>();
+}
+
+std::shared_ptr<LedgerEntry const>
+InMemoryLedgerTxnRoot::getNewestVersion(LedgerKey const& key) const
+{
+    return nullptr;
+}
+
+uint64_t
+InMemoryLedgerTxnRoot::countObjects(LedgerEntryType let) const
+{
+    return 0;
+}
+
+uint64_t
+InMemoryLedgerTxnRoot::countObjects(LedgerEntryType let,
+                                    LedgerRange const& ledgers) const
+{
+    return 0;
+}
+
+void
+InMemoryLedgerTxnRoot::deleteObjectsModifiedOnOrAfterLedger(
+    uint32_t ledger) const
+{
+}
+
+void
+InMemoryLedgerTxnRoot::dropAccounts()
+{
+}
+
+void
+InMemoryLedgerTxnRoot::dropData()
+{
+}
+
+void
+InMemoryLedgerTxnRoot::dropOffers()
+{
+}
+
+void
+InMemoryLedgerTxnRoot::dropTrustLines()
+{
+}
+
+double
+InMemoryLedgerTxnRoot::getPrefetchHitRate() const
+{
+    return 0.0;
+}
+
+uint32_t
+InMemoryLedgerTxnRoot::prefetch(std::unordered_set<LedgerKey> const& keys)
+{
+    return 0;
+}
+}
---
+++ src/ledger/InMemoryLedgerTxnRoot.h
@@ -0,0 +1,60 @@
+#include "ledger/LedgerTxn.h"
+#include "ledger/LedgerTxnImpl.h"
+#include "xdr/Stellar-ledger-entries.h"
+#include <map>
+#include <set>
+#include <unordered_map>
+#include <vector>
+
+// This is a stub helper class that pretends to implements a "root"
+// AbstractLedgerTxnParent like LedgerTxnRoot but returns empty/null values for
+// any query made of it, and throws if anyone ever tries to commit to it.
+//
+// This is used to anchor a live-but-never-committed LedgerTxn when doing
+// strictly-in-memory fast history replay.
+
+namespace stellar
+{
+
+class InMemoryLedgerTxnRoot : public AbstractLedgerTxnParent
+{
+    std::unique_ptr<LedgerHeader> mHeader;
+
+  public:
+    InMemoryLedgerTxnRoot();
+    void addChild(AbstractLedgerTxn& child) override;
+    void commitChild(EntryIterator iter, LedgerTxnConsistency cons) override;
+    void rollbackChild() override;
+
+    std::unordered_map<LedgerKey, LedgerEntry> getAllOffers() override;
+    std::shared_ptr<LedgerEntry const>
+    getBestOffer(Asset const& buying, Asset const& selling) override;
+    std::shared_ptr<LedgerEntry const>
+    getBestOffer(Asset const& buying, Asset const& selling,
+                 OfferDescriptor const& worseThan) override;
+    std::unordered_map<LedgerKey, LedgerEntry>
+    getOffersByAccountAndAsset(AccountID const& account,
+                               Asset const& asset) override;
+
+    LedgerHeader const& getHeader() const override;
+
+    std::vector<InflationWinner>
+    getInflationWinners(size_t maxWinners, int64_t minBalance) override;
+
+    std::shared_ptr<LedgerEntry const>
+    getNewestVersion(LedgerKey const& key) const override;
+
+    uint64_t countObjects(LedgerEntryType let) const override;
+    uint64_t countObjects(LedgerEntryType let,
+                          LedgerRange const& ledgers) const override;
+
+    void deleteObjectsModifiedOnOrAfterLedger(uint32_t ledger) const override;
+
+    void dropAccounts() override;
+    void dropData() override;
+    void dropOffers() override;
+    void dropTrustLines() override;
+    double getPrefetchHitRate() const override;
+    uint32_t prefetch(std::unordered_set<LedgerKey> const& keys) override;
+};
+}
