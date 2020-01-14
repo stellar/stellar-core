27e19f1dc972ae6ddd06468a360c25110df43105
Graydon Hoare <graydon@pobox.com>
2020-01-14 13:27:36 -0800

    warning police: remove stray pragma once from .cpp file

--- src/util/FileSystemException.cpp
+++ src/util/FileSystemException.cpp
@@ -1,5 +1,3 @@
-#pragma once
-
 // Copyright 2019 Stellar Development Foundation and contributors. Licensed
 // under the Apache License, Version 2.0. See the COPYING file at the root
 // of this distribution or at http://www.apache.org/licenses/LICENSE-2.0
