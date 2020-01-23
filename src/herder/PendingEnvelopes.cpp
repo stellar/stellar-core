stellar/stellar-core

commit 88de5b743a9f96dd9587b0daa491e62bc80c5986

herder: only refresh metrics when converting to Json
counters don't care when they get updated

src/herder/PendingEnvelopes.cpp
@@ -123,8 +123,6 @@ PendingEnvelopes::discardSCPEnvelopesWithQSet(Hash const& hash)
     auto envelopes = mQuorumSetFetcher.fetchingFor(hash);
     for (auto& envelope : envelopes)
         discardSCPEnvelope(envelope);
-
-    updateMetrics();
 }
 
 void
@@ -253,11 +251,9 @@ PendingEnvelopes::recvSCPEnvelope(SCPEnvelope const& envelope)
             fetching.erase(fetchIt);
 
             envelopeReady(envelope);
-            updateMetrics();
             return Herder::ENVELOPE_STATUS_READY;
         } // else just keep waiting for it to come in
 
-        updateMetrics();
         return Herder::ENVELOPE_STATUS_FETCHING;
     }
     catch (xdr::xdr_runtime_error& e)
@@ -286,7 +282,6 @@ PendingEnvelopes::discardSCPEnvelope(SCPEnvelope const& envelope)
         envs.mFetchingEnvelopes.erase(envelope);
 
         stopFetch(envelope);
-        updateMetrics();
     }
     catch (xdr::xdr_runtime_error& e)
     {
@@ -446,8 +441,6 @@ PendingEnvelopes::eraseBelow(uint64 slotIndex)
     mTxSetCache.erase_if([&](TxSetFramCacheItem const& i) {
         return i.first != 0 && i.first < slotIndex;
     });
-
-    updateMetrics();
 }
 
 void
@@ -471,8 +464,6 @@ PendingEnvelopes::slotClosed(uint64 slotIndex)
         mTxSetCache.erase_if(
             [&](TxSetFramCacheItem const& i) { return i.first == slotIndex; });
     }
-
-    updateMetrics();
 }
 
 TxSetFramePtr

commit ca0cde692f7096274bc40af0474468806b58a26e

refresh pending envelopes metrics
broken by 88de5b743a9f96dd9587b0daa491e62bc80c5986

metrics have to be updated every time we change something

src/herder/PendingEnvelopes.cpp
@@ -286,6 +286,7 @@ PendingEnvelopes::recvSCPEnvelope(SCPEnvelope const& envelope)
                     fetching.emplace(envelope, std::chrono::steady_clock::now())
                         .first;
                 startFetch(envelope);
+                updateMetrics();
             }
             else
             {
@@ -312,6 +313,7 @@ PendingEnvelopes::recvSCPEnvelope(SCPEnvelope const& envelope)
             fetching.erase(fetchIt);
 
             envelopeReady(envelope);
+            updateMetrics();
             return Herder::ENVELOPE_STATUS_READY;
         } // else just keep waiting for it to come in
 
@@ -350,6 +352,7 @@ PendingEnvelopes::discardSCPEnvelope(SCPEnvelope const& envelope)
             << "PendingEnvelopes::discardSCPEnvelope got corrupt message: "
             << e.what();
     }
+    updateMetrics();
 }
 
 bool
@@ -498,6 +501,7 @@ PendingEnvelopes::pop(uint64 slotIndex)
             auto ret = v.back();
             v.pop_back();
 
+            updateMetrics();
             return ret;
         }
         it++;
@@ -535,6 +539,8 @@ PendingEnvelopes::eraseBelow(uint64 slotIndex)
     mTxSetCache.erase_if([&](TxSetFramCacheItem const& i) {
         return i.first != 0 && i.first < slotIndex;
     });
+
+    updateMetrics();
 }
 
 void
@@ -560,6 +566,7 @@ PendingEnvelopes::slotClosed(uint64 slotIndex)
     }
 
     cleanKnownData();
+    updateMetrics();
 }
 
 TxSetFramePtr