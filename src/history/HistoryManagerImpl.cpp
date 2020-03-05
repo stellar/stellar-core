28985e35e8b237828a7c5c26afeacdf25e2df7ad
Graydon Hoare <graydon@pobox.com>
2020-03-04 16:14:31 -0800

    Redefine checkpointContainingLedger to not use nextCheckpointLedger.

--- src/history/HistoryManagerImpl.cpp
+++ src/history/HistoryManagerImpl.cpp
@@ -97,7 +97,11 @@ HistoryManagerImpl::getCheckpointFrequency() const
 uint32_t
 HistoryManagerImpl::checkpointContainingLedger(uint32_t ledger) const
 {
-    return nextCheckpointLedger(ledger + 1) - 1;
+    uint32_t freq = getCheckpointFrequency();
+    // Round-up to next multiple of freq, then subtract 1 since checkpoints are
+    // numbered for (and cover ledgers up to) the last ledger in them, which is
+    // one-before the next multiple of freq.
+    return (((ledger / freq) + 1) * freq) - 1;
 }
 
 uint32_t
