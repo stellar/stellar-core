901e6bbc2d23dc9826d92d30305b1bf941b47e1c
Jon Jove <jon@stellar.org>
2020-06-30 12:53:16 -0700

    Update LedgerHashUtils for ClaimableBalance

--- src/ledger/LedgerHashUtils.h
+++ src/ledger/LedgerHashUtils.h
@@ -11,66 +11,6 @@
 // implements a default hasher for "LedgerKey"
 namespace std
 {
-template <> class hash<stellar::LedgerKey>
-{
-  public:
-    size_t
-    operator()(stellar::LedgerKey const& lk) const
-    {
-        size_t res;
-        switch (lk.type())
-        {
-        case stellar::ACCOUNT:
-            res = stellar::shortHash::computeHash(
-                stellar::ByteSlice(lk.account().accountID.ed25519().data(), 8));
-            break;
-        case stellar::TRUSTLINE:
-        {
-            auto& tl = lk.trustLine();
-            res = stellar::shortHash::computeHash(
-                stellar::ByteSlice(tl.accountID.ed25519().data(), 8));
-            switch (lk.trustLine().asset.type())
-            {
-            case stellar::ASSET_TYPE_NATIVE:
-                break;
-            case stellar::ASSET_TYPE_CREDIT_ALPHANUM4:
-            {
-                auto& tl4 = tl.asset.alphaNum4();
-                res ^= stellar::shortHash::computeHash(
-                    stellar::ByteSlice(tl4.issuer.ed25519().data(), 8));
-                res ^= tl4.assetCode[0];
-                break;
-            }
-            case stellar::ASSET_TYPE_CREDIT_ALPHANUM12:
-            {
-                auto& tl12 = tl.asset.alphaNum12();
-                res ^= stellar::shortHash::computeHash(
-                    stellar::ByteSlice(tl12.issuer.ed25519().data(), 8));
-                res ^= tl12.assetCode[0];
-                break;
-            }
-            default:
-                abort();
-            }
-            break;
-        }
-        case stellar::DATA:
-            res = stellar::shortHash::computeHash(
-                stellar::ByteSlice(lk.data().accountID.ed25519().data(), 8));
-            res ^= stellar::shortHash::computeHash(stellar::ByteSlice(
-                lk.data().dataName.data(), lk.data().dataName.size()));
-            break;
-        case stellar::OFFER:
-            res = stellar::shortHash::computeHash(stellar::ByteSlice(
-                &lk.offer().offerID, sizeof(lk.offer().offerID)));
-            break;
-        default:
-            abort();
-        }
-        return res;
-    }
-};
-
 template <> class hash<stellar::Asset>
 {
   public:
@@ -98,6 +38,48 @@ template <> class hash<stellar::Asset>
             res ^= a12.assetCode[0];
             break;
         }
+        }
+        return res;
+    }
+};
+
+template <> class hash<stellar::LedgerKey>
+{
+  public:
+    size_t
+    operator()(stellar::LedgerKey const& lk) const
+    {
+        size_t res;
+        switch (lk.type())
+        {
+        case stellar::ACCOUNT:
+            res = stellar::shortHash::computeHash(
+                stellar::ByteSlice(lk.account().accountID.ed25519().data(), 8));
+            break;
+        case stellar::TRUSTLINE:
+        {
+            auto& tl = lk.trustLine();
+            res = stellar::shortHash::computeHash(
+                stellar::ByteSlice(tl.accountID.ed25519().data(), 8));
+            res ^= hash<stellar::Asset>()(tl.asset);
+            break;
+        }
+        case stellar::DATA:
+            res = stellar::shortHash::computeHash(
+                stellar::ByteSlice(lk.data().accountID.ed25519().data(), 8));
+            res ^= stellar::shortHash::computeHash(stellar::ByteSlice(
+                lk.data().dataName.data(), lk.data().dataName.size()));
+            break;
+        case stellar::OFFER:
+            res = stellar::shortHash::computeHash(stellar::ByteSlice(
+                &lk.offer().offerID, sizeof(lk.offer().offerID)));
+            break;
+        case stellar::CLAIMABLE_BALANCE:
+            res = stellar::shortHash::computeHash(stellar::ByteSlice(
+                lk.claimableBalance().balanceID.v0().data(), 8));
+            break;
+        default:
+            abort();
         }
         return res;
     }
