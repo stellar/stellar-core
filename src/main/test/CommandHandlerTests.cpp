b91dd1b360073ee153669696d9c0214f117db2b3
Terence Rokop <rokopt@sharpears.net>
2020-09-09 15:36:48 -0700

    Tests for enhanced "manualclose"
    Validate that:
    
    - "manualclose" may be issued only when MANUAL_CLOSE
    is configured
    - "manualclose" may be issued with parameters only when
    RUN_STANDALONE is additionally configured
    - By default, "manual close" increments the sequence
    number and increases the close time by a default interval
    (currently 5 seconds)
    - Parameters passed to "manualclose" can override default
    sequence number and close time
    - Sequence numbers and close times can not overflow the
    maximum values that `stellar-core` can handle internally
    - "manualclose" leads to the application of transactions
    previously submitted via the "tx" endpoint
    - A close time specified to "manualclose" affects whether
    a transaction in the queue is txTOO_LATE

--- src/main/test/CommandHandlerTests.cpp
+++ src/main/test/CommandHandlerTests.cpp
@@ -2,6 +2,8 @@
 // under the Apache License, Version 2.0. See the COPYING file at the root
 // of this distribution or at http://www.apache.org/licenses/LICENSE-2.0
 
+#include "ledger/LedgerTxn.h"
+#include "ledger/test/LedgerTestUtils.h"
 #include "lib/catch.hpp"
 #include "main/Application.h"
 #include "main/CommandHandler.h"
@@ -12,7 +14,13 @@
 #include "transactions/SignatureUtils.h"
 #include "transactions/TransactionUtils.h"
 #include "util/Decoder.h"
+#include "util/Math.h"
+#include "util/optional.h"
+#include "xdr/Stellar-ledger-entries.h"
+#include "xdr/Stellar-transaction.h"
 #include "xdrpp/marshal.h"
+#include <fmt/format.h>
+#include <stdexcept>
 
 using namespace stellar;
 using namespace stellar::txtest;
@@ -135,5 +143,362 @@ TEST_CASE("transaction envelope bridge", "[commandhandler]")
             closeLedgerOn(*app, 2, 1, 1, 2017);
             REQUIRE(submit(createFeeBump()) == PENDING_RESULT);
         });
+    }
+}
+
+TEST_CASE("manualclose", "[commandhandler]")
+{
+    auto testManualCloseConfig = [](auto configure, auto issue) {
+        VirtualClock clock(VirtualClock::VIRTUAL_TIME);
+        Config cfg = getTestConfig();
+        configure(cfg);
+        auto app = createTestApplication(clock, cfg);
+        app->start();
+        auto& commandHandler = app->getCommandHandler();
+        std::string retStr;
+        issue(commandHandler, retStr);
+    };
+
+    SECTION("'manualclose' is forbidden if MANUAL_CLOSE is not configured")
+    {
+        testManualCloseConfig(
+            [](Config& cfg) {
+                cfg.MANUAL_CLOSE = false;
+                cfg.RUN_STANDALONE = false;
+            },
+            [](CommandHandler& handler, std::string& retStr) {
+                REQUIRE_THROWS_AS(handler.manualClose("", retStr),
+                                  std::invalid_argument);
+            });
+        testManualCloseConfig(
+            [](Config& cfg) {
+                cfg.MANUAL_CLOSE = false;
+                cfg.RUN_STANDALONE = true;
+            },
+            [](CommandHandler& handler, std::string& retStr) {
+                REQUIRE_THROWS_AS(handler.manualClose("", retStr),
+                                  std::invalid_argument);
+            });
+    }
+
+    SECTION("'manualclose' command without parameters is allowed if "
+            "MANUAL_CLOSE is configured")
+    {
+        testManualCloseConfig(
+            [](Config& cfg) {
+                cfg.MANUAL_CLOSE = true;
+                cfg.RUN_STANDALONE = false;
+            },
+            [](CommandHandler& handler, std::string& retStr) {
+                handler.manualClose("", retStr);
+            });
+    }
+
+    SECTION("'manualclose' command with parameters is forbidden if "
+            "RUN_STANDALONE is not configured")
+    {
+        testManualCloseConfig(
+            [](Config& cfg) {
+                cfg.MANUAL_CLOSE = true;
+                cfg.RUN_STANDALONE = false;
+            },
+            [](CommandHandler& handler, std::string& retStr) {
+                CHECK_THROWS_AS(handler.manualClose("ledgerSeq=10", retStr),
+                                std::invalid_argument);
+                REQUIRE_THROWS_AS(handler.manualClose("closeTime=10", retStr),
+                                  std::invalid_argument);
+            });
+    }
+
+    SECTION("'manualclose' command with parameters is allowed if "
+            "RUN_STANDALONE is configured")
+    {
+        testManualCloseConfig(
+            [](Config& cfg) {
+                cfg.MANUAL_CLOSE = true;
+                cfg.RUN_STANDALONE = true;
+            },
+            [](CommandHandler& handler, std::string& retStr) {
+                handler.manualClose("ledgerSeq=20&closeTime=20", retStr);
+            });
+    }
+
+    VirtualClock clock(VirtualClock::VIRTUAL_TIME);
+    auto app = createTestApplication(clock, getTestConfig());
+    auto& commandHandler = app->getCommandHandler();
+    app->start();
+
+    REQUIRE(app->getConfig().MANUAL_CLOSE);
+    REQUIRE(app->getConfig().NODE_IS_VALIDATOR);
+    REQUIRE(app->getConfig().RUN_STANDALONE);
+    REQUIRE(app->getConfig().FORCE_SCP);
+
+    auto const defaultManualCloseTimeInterval =
+        app->getConfig().getExpectedLedgerCloseTime().count();
+
+    auto lastLedgerNum = [&]() {
+        return app->getLedgerManager().getLastClosedLedgerNum();
+    };
+
+    auto lastCloseTime = [&]() {
+        return app->getLedgerManager()
+            .getLastClosedLedgerHeader()
+            .header.scpValue.closeTime;
+    };
+
+    auto submitClose = [&](optional<uint32_t> const& ledgerSeq,
+                           optional<TimePoint> const& closeTime,
+                           std::string& retStr) {
+        std::string ledgerSeqParam, closeTimeParam;
+        if (ledgerSeq)
+        {
+            std::stringstream stream;
+            stream << *ledgerSeq;
+            ledgerSeqParam =
+                fmt::format(FMT_STRING("?ledgerSeq={}"), stream.str());
+        }
+        if (closeTime)
+        {
+            std::stringstream stream;
+            stream << *closeTime;
+            closeTimeParam =
+                fmt::format(FMT_STRING("?closeTime={}"), stream.str());
+        }
+        std::string const params = ledgerSeqParam + closeTimeParam;
+        commandHandler.manualClose(params, retStr);
+    };
+
+    auto const noLedgerSeq = nullopt<uint32_t>();
+    auto const noCloseTime = nullopt<TimePoint>();
+
+    SECTION("manual close with no parameters increments sequence number and "
+            "increases close time")
+    {
+        std::string retStr;
+        auto const expectedNewLedgerSeq = lastLedgerNum() + 1;
+        auto const minimumExpectedNewCloseTime = lastCloseTime() + 1;
+        submitClose(noLedgerSeq, noCloseTime, retStr);
+        REQUIRE(lastLedgerNum() == expectedNewLedgerSeq);
+        REQUIRE(lastCloseTime() >= minimumExpectedNewCloseTime);
+    }
+
+    SECTION("manual close with explicit sequence number parameter sets new "
+            "sequence number and increases close time")
+    {
+        std::vector<uint32_t> const seqNumIncrements{1, 2, 3, 10, 100, 1000000};
+        for (auto const& seqNumIncrement : seqNumIncrements)
+        {
+            std::string retStr;
+            auto const curLedgerSeq = lastLedgerNum();
+            auto const expectedNewLedgerSeq = curLedgerSeq + seqNumIncrement;
+            auto const minimumExpectedNewCloseTime = lastCloseTime() + 1;
+            submitClose(make_optional<uint32_t>(expectedNewLedgerSeq),
+                        noCloseTime, retStr);
+            CAPTURE(curLedgerSeq);
+            CAPTURE(expectedNewLedgerSeq);
+            CAPTURE(retStr);
+            REQUIRE(lastLedgerNum() == expectedNewLedgerSeq);
+            REQUIRE(lastCloseTime() >= minimumExpectedNewCloseTime);
+        }
+    }
+
+    SECTION("manual close with explicit close time parameter sets new close "
+            "time and current virtual time, and increments sequence number")
+    {
+        std::vector<TimePoint> const closeTimeIncrements{1,  2,   3,
+                                                         10, 100, 1000000};
+        for (auto const& closeTimeIncrement : closeTimeIncrements)
+        {
+            std::string retStr;
+            auto const curLedgerSeq = lastLedgerNum();
+            auto const curLedgerCloseTime = lastCloseTime();
+            auto const expectedNewCloseTime =
+                curLedgerCloseTime + closeTimeIncrement;
+            submitClose(noLedgerSeq,
+                        make_optional<TimePoint>(expectedNewCloseTime), retStr);
+            CAPTURE(curLedgerCloseTime);
+            CAPTURE(expectedNewCloseTime);
+            CAPTURE(retStr);
+            REQUIRE(lastLedgerNum() == curLedgerSeq + 1);
+            REQUIRE(lastCloseTime() == expectedNewCloseTime);
+            REQUIRE(lastCloseTime() ==
+                    VirtualClock::to_time_t(app->getClock().system_now()));
+        }
+    }
+
+    SECTION("manual close sequence number parameter that just barely does not "
+            "overflow int32_t is accepted")
+    {
+        std::string retStr;
+        REQUIRE(lastLedgerNum() == LedgerManager::GENESIS_LEDGER_SEQ);
+        submitClose(make_optional<uint32_t>(static_cast<uint32_t>(
+                        std::numeric_limits<int32_t>::max())),
+                    noCloseTime, retStr);
+        CAPTURE(retStr);
+        REQUIRE(lastLedgerNum() == std::numeric_limits<int32_t>::max());
+    }
+
+    SECTION("manual close sequence number parameter that overflows int32_t is "
+            "detected and rejected")
+    {
+        std::string retStr;
+        REQUIRE_THROWS_AS(
+            submitClose(
+                make_optional<uint32_t>(
+                    static_cast<uint32_t>(std::numeric_limits<int32_t>::max()) +
+                    1),
+                noCloseTime, retStr),
+            std::invalid_argument);
+    }
+
+    SECTION("manual close sequence number parameter that would not increase "
+            "ledger sequence number is detected and rejected")
+    {
+        uint32_t initialSequenceNumber = LedgerManager::GENESIS_LEDGER_SEQ + 2;
+        std::string retStr;
+
+        submitClose(make_optional<uint32_t>(initialSequenceNumber), noCloseTime,
+                    retStr);
+
+        CHECK_THROWS_AS(
+            submitClose(make_optional<uint32_t>(initialSequenceNumber - 1),
+                        noCloseTime, retStr),
+            std::invalid_argument);
+
+        REQUIRE_THROWS_AS(
+            submitClose(make_optional<uint32_t>(initialSequenceNumber),
+                        noCloseTime, retStr),
+            std::invalid_argument);
+    }
+
+    uint64_t const firstSecondOfYear2200GMT = 7'258'118'400;
+
+    SECTION(
+        "manual close time that does not overflow documented limit is accepted")
+    {
+        std::string retStr;
+        REQUIRE(lastLedgerNum() == LedgerManager::GENESIS_LEDGER_SEQ);
+        submitClose(noLedgerSeq,
+                    make_optional<TimePoint>(firstSecondOfYear2200GMT), retStr);
+        CAPTURE(retStr);
+        REQUIRE(lastCloseTime() == firstSecondOfYear2200GMT);
+    }
+
+    SECTION("manual close time that overflows documented limit is detected and "
+            "rejected")
+    {
+        uint64_t const overflowingCloseTime = firstSecondOfYear2200GMT + 1ULL;
+        std::string retStr;
+        REQUIRE(lastLedgerNum() == LedgerManager::GENESIS_LEDGER_SEQ);
+        CAPTURE(retStr);
+        REQUIRE_THROWS_AS(
+            submitClose(noLedgerSeq,
+                        make_optional<TimePoint>(overflowingCloseTime), retStr),
+            std::invalid_argument);
+    }
+
+    SECTION("manual close induces expected transaction applications")
+    {
+        auto submitTx = [&](TransactionEnvelope const& envelope,
+                            std::string& retStr) {
+            commandHandler.tx(
+                fmt::format(FMT_STRING("?blob={}"),
+                            decoder::encode_b64(xdr::xdr_to_opaque(envelope))),
+                retStr);
+        };
+
+        auto const initialCloseTime = 100;
+        std::string retStr;
+
+        submitClose(noLedgerSeq, make_optional<TimePoint>(initialCloseTime),
+                    retStr);
+        REQUIRE(lastLedgerNum() == LedgerManager::GENESIS_LEDGER_SEQ + 1);
+        REQUIRE(lastCloseTime() == initialCloseTime);
+        REQUIRE(VirtualClock::to_time_t(app->getClock().system_now()) ==
+                initialCloseTime);
+        auto root = TestAccount::createRoot(*app);
+
+        SECTION("A single transaction is applied by a manual close")
+        {
+            LedgerEntry dle;
+            dle.lastModifiedLedgerSeq = lastLedgerNum();
+            dle.data.type(DATA);
+            auto de = LedgerTestUtils::generateValidDataEntry();
+            de.accountID = root.getPublicKey();
+            dle.data.data() = de;
+
+            auto dataOp = txtest::manageData(de.dataName, &de.dataValue);
+            auto txFrame = root.tx({dataOp});
+            submitTx(txFrame->getEnvelope(), retStr);
+
+            {
+                LedgerTxn lookupTx(app->getLedgerTxnRoot());
+                auto entry = lookupTx.load(LedgerEntryKey(dle));
+                REQUIRE(!entry);
+            }
+
+            submitClose(noLedgerSeq, noCloseTime, retStr);
+
+            LedgerTxn lookupTx(app->getLedgerTxnRoot());
+            auto entry = lookupTx.load(LedgerEntryKey(dle));
+            REQUIRE(entry);
+        }
+
+        SECTION("A transaction which is submitted on time can become late by "
+                "the time of a ledger close")
+        {
+            LedgerEntry dle;
+            dle.lastModifiedLedgerSeq = lastLedgerNum();
+            dle.data.type(DATA);
+            auto de = LedgerTestUtils::generateValidDataEntry();
+            de.accountID = root.getPublicKey();
+            dle.data.data() = de;
+
+            auto dataOp = txtest::manageData(de.dataName, &de.dataValue);
+            auto txFrame = root.tx({dataOp});
+            REQUIRE(txFrame->getEnvelope().type() == stellar::ENVELOPE_TYPE_TX);
+            txFrame->getEnvelope().v1().tx.timeBounds.activate();
+            txFrame->getEnvelope().v1().tx.timeBounds->minTime = 0;
+            TimePoint const maxTime =
+                lastCloseTime() + defaultManualCloseTimeInterval +
+                getUpperBoundCloseTimeOffset(*app, lastCloseTime());
+            txFrame->getEnvelope().v1().tx.timeBounds->maxTime = maxTime;
+            txFrame->getEnvelope().v1().signatures.clear();
+            txFrame->addSignature(root);
+
+            {
+                LedgerTxn checkLtx(app->getLedgerTxnRoot());
+                auto valid = txFrame->checkValid(checkLtx, 0, 0, 0);
+                REQUIRE(valid);
+            }
+
+            submitTx(txFrame->getEnvelope(), retStr);
+
+            {
+                LedgerTxn lookupTx(app->getLedgerTxnRoot());
+                auto entry = lookupTx.load(LedgerEntryKey(dle));
+                REQUIRE(!entry);
+            }
+
+            SECTION("Ledger closes on time; transaction is applied")
+            {
+                submitClose(noLedgerSeq, make_optional<TimePoint>(maxTime),
+                            retStr);
+
+                LedgerTxn lookupTx(app->getLedgerTxnRoot());
+                auto entry = lookupTx.load(LedgerEntryKey(dle));
+                REQUIRE(entry);
+            }
+
+            SECTION("Ledger closes too late; transaction is not applied")
+            {
+                submitClose(noLedgerSeq, make_optional<TimePoint>(maxTime + 1),
+                            retStr);
+
+                LedgerTxn lookupTx(app->getLedgerTxnRoot());
+                auto entry = lookupTx.load(LedgerEntryKey(dle));
+                REQUIRE(!entry);
+            }
+        }
     }
 }
