65e6b491995b32da8ae945afd7ff06f09ef5c08d
Hidenori <hidenori@stellar.org>
2020-09-15 15:54:17 -0400

    Add networkPassphrase to test files
    This ensures that the current code can consume HAS files with
    networkPassphrase

--- src/history/test/SerializeTests.cpp
+++ src/history/test/SerializeTests.cpp
@@ -12,27 +12,41 @@ using namespace stellar;
 
 TEST_CASE("Serialization round trip", "[history]")
 {
-    std::vector<std::string> testFiles = {
+    std::vector<std::string> inputFiles = {
         "stellar-history.testnet.6714239.json",
-        "stellar-history.livenet.15686975.json"};
-    for (auto const& fn : testFiles)
+        "stellar-history.livenet.15686975.json",
+        "stellar-history.testnet.6714239.networkPassphrase.json"};
+    std::vector<std::string> expectedOutputFiles = {
+        "stellar-history.testnet.6714239.json",
+        "stellar-history.livenet.15686975.json",
+        "expected-output-stellar-history.testnet.6714239.networkPassphrase.json"};
+    for (int i = 0; i < inputFiles.size(); i++)
     {
         std::string fnPath = "testdata/";
-        fnPath += fn;
-        SECTION("Serialize " + fnPath)
+        std::string inputPath = fnPath + inputFiles[i];
+        std::string expectedOutputPath = fnPath + expectedOutputFiles[i];
+        SECTION("Serialize " + inputPath)
         {
-            std::ifstream in(fnPath);
+            std::ifstream in(inputPath);
             in.exceptions(std::ios::badbit);
-            std::string fromFile((std::istreambuf_iterator<char>(in)),
-                                 std::istreambuf_iterator<char>());
+            std::string input((std::istreambuf_iterator<char>(in)),
+                              std::istreambuf_iterator<char>());
 
+            std::ifstream expectedOutputStream(expectedOutputPath);
+            expectedOutputStream.exceptions(std::ios::badbit);
+            std::string expectedOutput(
+                (std::istreambuf_iterator<char>(expectedOutputStream)),
+                std::istreambuf_iterator<char>());
+
+            // Test fromString
             HistoryArchiveState has;
-            has.fromString(fromFile);
-            REQUIRE(fromFile == has.toString());
+            has.fromString(input);
+            REQUIRE(expectedOutput == has.toString());
 
+            // Test load
             HistoryArchiveState hasLoad;
-            hasLoad.load(fnPath);
-            REQUIRE(fromFile == hasLoad.toString());
+            hasLoad.load(inputPath);
+            REQUIRE(expectedOutput == hasLoad.toString());
         }
     }
 }
