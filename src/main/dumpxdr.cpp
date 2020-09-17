c1e0beeda4542779967ab565996551fed06c9247
Hidenori <hidenori@stellar.org>
2020-09-17 13:20:58 -0400

    Move cereal_override's to its own header

--- src/main/dumpxdr.cpp
+++ src/main/dumpxdr.cpp
@@ -7,6 +7,7 @@
 #include "transactions/TransactionUtils.h"
 #include "util/Decoder.h"
 #include "util/Fs.h"
+#include "util/XDRCereal.h"
 #include "util/XDROperators.h"
 #include "util/XDRStream.h"
 #include "util/types.h"
@@ -37,104 +38,6 @@ extern "C" {
 #endif // _WIN32
 
 using namespace std::placeholders;
-
-template <uint32_t N>
-void
-cereal_override(cereal::JSONOutputArchive& ar, const xdr::opaque_array<N>& s,
-                const char* field)
-{
-    xdr::archive(ar, stellar::binToHex(stellar::ByteSlice(s.data(), s.size())),
-                 field);
-}
-
-// We still need one explicit composite-container override because cereal
-// appears to process arrays-of-arrays internally, without calling back through
-// an NVP adaptor.
-template <uint32_t N>
-void
-cereal_override(cereal::JSONOutputArchive& ar,
-                const xdr::xarray<stellar::Hash, N>& s, const char* field)
-{
-    std::vector<std::string> tmp;
-    for (auto const& h : s)
-    {
-        tmp.emplace_back(
-            stellar::binToHex(stellar::ByteSlice(h.data(), h.size())));
-    }
-    xdr::archive(ar, tmp, field);
-}
-
-template <uint32_t N>
-void
-cereal_override(cereal::JSONOutputArchive& ar, const xdr::opaque_vec<N>& s,
-                const char* field)
-{
-    xdr::archive(ar, stellar::binToHex(stellar::ByteSlice(s.data(), s.size())),
-                 field);
-}
-void
-cereal_override(cereal::JSONOutputArchive& ar, const stellar::PublicKey& s,
-                const char* field)
-{
-    xdr::archive(ar, stellar::KeyUtils::toStrKey<stellar::PublicKey>(s), field);
-}
-
-void
-cereal_override(cereal::JSONOutputArchive& ar,
-                const stellar::MuxedAccount& muxedAccount, const char* field)
-{
-    switch (muxedAccount.type())
-    {
-    case stellar::KEY_TYPE_ED25519:
-        xdr::archive(ar, stellar::KeyUtils::toStrKey(toAccountID(muxedAccount)),
-                     field);
-        return;
-    case stellar::KEY_TYPE_MUXED_ED25519:
-        xdr::archive(
-            ar,
-            std::make_tuple(
-                cereal::make_nvp("id", muxedAccount.med25519().id),
-                cereal::make_nvp("accountID", stellar::KeyUtils::toStrKey(
-                                                  toAccountID(muxedAccount)))),
-            field);
-        return;
-    default:
-        // this would be a bug
-        abort();
-    }
-}
-
-void
-cereal_override(cereal::JSONOutputArchive& ar, const stellar::Asset& s,
-                const char* field)
-{
-    xdr::archive(ar, stellar::assetToString(s), field);
-}
-
-template <typename T>
-void
-cereal_override(cereal::JSONOutputArchive& ar, const xdr::pointer<T>& t,
-                const char* field)
-{
-    // We tolerate a little information-loss here collapsing *T into T for
-    // the non-null case, and use JSON 'null' for the null case. This reads
-    // much better than the thing JSONOutputArchive does with PtrWrapper.
-    if (t)
-    {
-        xdr::archive(ar, *t, field);
-    }
-    else
-    {
-        ar.setNextName(field);
-        ar.writeName();
-        ar.saveValue(nullptr);
-    }
-}
-
-// This has to be included _after_ the cereal_override overloads above,
-// otherwise some interplay of name lookup and visibility during the
-// enable_if call in the cereal adaptor fails to find them.
-#include <xdrpp/cereal.h>
 
 namespace stellar
 {
