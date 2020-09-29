0f45655eb09e910cea7a574632765b0e3f51e118
Hidenori Shinohara <hidenori@stellar.org>
2020-09-29 14:34:45 -0400

    Add the license notice to XDRCereal.h

--- src/util/XDRCereal.h
+++ src/util/XDRCereal.h
@@ -1,3 +1,7 @@
+// Copyright 2020 Stellar Development Foundation and contributors. Licensed
+// under the Apache License, Version 2.0. See the COPYING file at the root
+// of this distribution or at http://www.apache.org/licenses/LICENSE-2.0
+
 #pragma once
 
 #ifdef _XDRPP_CEREAL_H_HEADER_INCLUDED_
