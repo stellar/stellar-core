f87899eb6760cad6bc85f03d26a8b0bd9ccc5ec7
Jon Jove <jon@stellar.org>
2020-10-13 09:57:54 -0500

    Update includes for new file names

--- src/transactions/EndSponsoringFutureReservesOpFrame.cpp
+++ src/transactions/EndSponsoringFutureReservesOpFrame.cpp
@@ -2,7 +2,7 @@
 // under the Apache License, Version 2.0. See the COPYING file at the root
 // of this distribution or at http://www.apache.org/licenses/LICENSE-2.0
 
-#include "transactions/ConfirmAndClearSponsorOpFrame.h"
+#include "transactions/EndSponsoringFutureReservesOpFrame.h"
 #include "ledger/LedgerTxn.h"
 #include "ledger/LedgerTxnEntry.h"
 #include "transactions/TransactionUtils.h"
