fb45dd8d0aae77f23eb46ae44104e2708887ba42
Graydon Hoare <graydon@pobox.com>
2020-11-20 21:42:38 -0800

    Add backtrace-printing to std::terminate.

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -8,6 +8,7 @@
 
 #include "crypto/ShortHash.h"
 #include <cstdlib>
+#include <exception>
 #include <sodium/core.h>
 #include <xdrpp/marshal.h>
 
@@ -16,11 +17,18 @@ INITIALIZE_EASYLOGGINGPP
 namespace stellar
 {
 static void
+printBacktraceAndAbort()
+{
+    printCurrentBacktrace();
+    std::abort();
+}
+
+static void
 outOfMemory()
 {
     std::fprintf(stderr, "Unable to allocate memory\n");
     std::fflush(stderr);
-    std::abort();
+    printBacktraceAndAbort();
 }
 }
 
@@ -28,10 +36,13 @@ int
 main(int argc, char* const* argv)
 {
     using namespace stellar;
+    BacktraceManager btGuard;
 
     // Abort when out of memory
     std::set_new_handler(outOfMemory);
-    BacktraceManager btGuard;
+    // At least print a backtrace in any circumstance
+    // that would call std::terminate
+    std::set_terminate(printBacktraceAndAbort);
 
     Logging::init();
     if (sodium_init() != 0)
