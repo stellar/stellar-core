5b90faa590d4bd5c8952b7cada1b7dff527bf512
Terence Rokop <rokopt@sharpears.net>
2021-04-01 00:16:38 -0700

    Extend live metastream test to test safe meta emit
    Check that:
    - A live node with !UNSAFE_EMIT_META_EARLY always emits meta
    one ledger delayed
    - A live node with !UNSAFE_EMIT_META_EARLY whose ledger is
    corrupted during close never emits meta for that ledger
    (unlike a node with UNSAFE_EMIT_META_EARLY)

--- src/ledger/test/LedgerCloseMetaStreamTests.cpp
+++ src/ledger/test/LedgerCloseMetaStreamTests.cpp
@@ -6,6 +6,8 @@
 #include "crypto/Random.h"
 #include "history/HistoryArchiveManager.h"
 #include "history/test/HistoryTestsUtils.h"
+#include "ledger/LedgerTxn.h"
+#include "ledger/test/LedgerTestUtils.h"
 #include "lib/catch.hpp"
 #include "main/Application.h"
 #include "main/ApplicationUtils.h"
@@ -25,15 +27,28 @@ TEST_CASE("LedgerCloseMetaStream file descriptor - LIVE_NODE",
     // Live reqires a multinode simulation, as we're not allowed to run a
     // validator and record metadata streams at the same time (to avoid the
     // unbounded-latency stream-write step): N nodes participating in consensus,
-    // and one watching and streamnig metadata.
+    // and two watching and streaming metadata -- the second one using
+    // !UNSAFE_EMIT_META_EARLY.
 
-    Hash hash;
+    Hash expectedLastUnsafeHash, expectedLastSafeHash;
     TmpDirManager tdm(std::string("streamtmp-") + binToHex(randomBytes(8)));
     TmpDir td = tdm.tmpDir("streams");
     std::string path = td.getName() + "/stream.xdr";
+    std::string pathSafe = td.getName() + "/streamSafe.xdr";
+
+    auto const ledgerToWaitFor = 10;
+
+    bool const induceOneLedgerFork = GENERATE(false, true);
+    CAPTURE(induceOneLedgerFork);
+    auto const ledgerToCorrupt = 5;
+    static_assert(ledgerToCorrupt < ledgerToWaitFor,
+                  "must wait beyond corrupt ledger");
+
+    auto const expectedLastWatcherLedger =
+        induceOneLedgerFork ? ledgerToCorrupt : ledgerToWaitFor;
 
     {
-        // Step 1: Set up a 4 node simulation with 3 validators and 1 watcher.
+        // Step 1: Set up a 5 node simulation with 3 validators and 2 watchers.
         auto networkID = sha256(getTestConfig().NETWORK_PASSPHRASE);
         auto simulation =
             std::make_shared<Simulation>(Simulation::OVER_LOOPBACK, networkID);
@@ -42,7 +57,8 @@ TEST_CASE("LedgerCloseMetaStream file descriptor - LIVE_NODE",
         SIMULATION_CREATE_NODE(Node2); // Validator
         SIMULATION_CREATE_NODE(Node3); // Validator
 
-        SIMULATION_CREATE_NODE(Node4); // Watcher
+        SIMULATION_CREATE_NODE(Node4); // Watcher, UNSAFE_EMIT_META_EARLY
+        SIMULATION_CREATE_NODE(Node5); // Watcher, !UNSAFE_EMIT_META_EARLY
 
         SCPQuorumSet qSet;
         qSet.threshold = 3;
@@ -54,51 +70,132 @@ TEST_CASE("LedgerCloseMetaStream file descriptor - LIVE_NODE",
         Config const& cfg2 = getTestConfig(2);
         Config const& cfg3 = getTestConfig(3);
         Config cfg4 = getTestConfig(4);
+        Config cfg5 = getTestConfig(5);
 
-        // Step 2: open a writable file and pass it to config 4 (watcher).
+        // Step 2: open writable files and pass them to configs 4 and 5
+        // (watchers).
         cfg4.NODE_IS_VALIDATOR = false;
         cfg4.FORCE_SCP = false;
+        cfg5.NODE_IS_VALIDATOR = false;
+        cfg5.FORCE_SCP = false;
 #ifdef _WIN32
         cfg4.METADATA_OUTPUT_STREAM = path;
+        cfg5.METADATA_OUTPUT_STREAM = pathSafe;
 #else
         int fd = ::open(path.c_str(), O_CREAT | O_WRONLY, 0644);
         REQUIRE(fd != -1);
         cfg4.METADATA_OUTPUT_STREAM = fmt::format("fd:{}", fd);
+        int fdSafe = ::open(pathSafe.c_str(), O_CREAT | O_WRONLY, 0644);
+        REQUIRE(fdSafe != -1);
+        cfg5.METADATA_OUTPUT_STREAM = fmt::format("fd:{}", fdSafe);
 #endif
+
+        cfg4.UNSAFE_EMIT_META_EARLY = true;
+        cfg5.UNSAFE_EMIT_META_EARLY = false;
+        cfg5.MODE_USES_IN_MEMORY_LEDGER =
+            true; // required by !UNSAFE_EMIT_META_EARLY
 
         // Step 3: Run simulation a few steps to stream metadata.
         auto app1 = simulation->addNode(vNode1SecretKey, qSet, &cfg1);
         auto app2 = simulation->addNode(vNode2SecretKey, qSet, &cfg2);
         auto app3 = simulation->addNode(vNode3SecretKey, qSet, &cfg3);
         auto app4 = simulation->addNode(vNode4SecretKey, qSet, &cfg4);
+        auto app5 = simulation->addNode(vNode5SecretKey, qSet, &cfg5);
 
         simulation->addPendingConnection(vNode1NodeID, vNode2NodeID);
         simulation->addPendingConnection(vNode1NodeID, vNode3NodeID);
         simulation->addPendingConnection(vNode1NodeID, vNode4NodeID);
+        simulation->addPendingConnection(vNode1NodeID, vNode5NodeID);
 
         simulation->startAllNodes();
+        bool watchersAreCorrupted = false;
         simulation->crankUntil(
             [&]() {
-                return app4->getLedgerManager().getLastClosedLedgerNum() == 10;
+                // As long as the watchers are in sync, wait for them to get the
+                // news of all the ledgers closed by the validators.  But once
+                // the watchers are corrupt, they won't be able to close more
+                // ledgers, so at that point we start waiting only for the
+                // validators to do so.
+                if (watchersAreCorrupted)
+                {
+                    return app1->getLedgerManager().getLastClosedLedgerNum() ==
+                           ledgerToWaitFor;
+                }
+
+                auto const lastClosedLedger =
+                    app4->getLedgerManager().getLastClosedLedgerNum();
+                REQUIRE(app5->getLedgerManager().getLastClosedLedgerNum() ==
+                        lastClosedLedger);
+
+                if (lastClosedLedger == expectedLastWatcherLedger - 1)
+                {
+                    expectedLastSafeHash = app5->getLedgerManager()
+                                               .getLastClosedLedgerHeader()
+                                               .hash;
+
+                    if (induceOneLedgerFork)
+                    {
+                        for (auto& app : {app4, app5})
+                        {
+                            txtest::closeLedgerOn(
+                                *app, ledgerToCorrupt,
+                                app->getLedgerManager()
+                                        .getLastClosedLedgerHeader()
+                                        .header.scpValue.closeTime +
+                                    1);
+                        }
+
+                        expectedLastUnsafeHash =
+                            app4->getLedgerManager()
+                                .getLastClosedLedgerHeader()
+                                .hash;
+
+                        watchersAreCorrupted = true;
+                        return false;
+                    }
+                }
+
+                return lastClosedLedger == ledgerToWaitFor;
             },
             std::chrono::seconds{200}, false);
 
-        REQUIRE(app4->getLedgerManager().getLastClosedLedgerNum() == 10);
-        hash = app4->getLedgerManager().getLastClosedLedgerHeader().hash;
+        REQUIRE(app4->getLedgerManager().getLastClosedLedgerNum() ==
+                expectedLastWatcherLedger);
+        REQUIRE(app5->getLedgerManager().getLastClosedLedgerNum() ==
+                expectedLastWatcherLedger);
+
+        if (!induceOneLedgerFork)
+        {
+            expectedLastUnsafeHash =
+                app4->getLedgerManager().getLastClosedLedgerHeader().hash;
+        }
     }
 
-    // Step 4: reopen the file as an XDR stream and read back the LCMs
+    // Step 4: reopen the files as XDR streams and read back the LCMs
     // and check they have the expected content.
-    XDRInputFileStream stream;
-    stream.open(path);
-    LedgerCloseMeta lcm;
-    size_t nLcm = 1;
-    while (stream && stream.readOne(lcm))
-    {
-        ++nLcm;
-    }
-    REQUIRE(nLcm == 10);
-    REQUIRE(lcm.v0().ledgerHeader.hash == hash);
+    auto readLcms = [](std::string const& lcmPath) {
+        std::vector<LedgerCloseMeta> lcms;
+        XDRInputFileStream stream;
+        stream.open(lcmPath);
+        LedgerCloseMeta lcm;
+        while (stream && stream.readOne(lcm))
+        {
+            lcms.push_back(lcm);
+        }
+        return lcms;
+    };
+
+    auto lcms = readLcms(path);
+    auto lcmsSafe = readLcms(pathSafe);
+    // The "- 1" is because we don't stream meta for the genesis ledger.
+    REQUIRE(lcms.size() == expectedLastWatcherLedger - 1);
+    REQUIRE(lcms.back().v0().ledgerHeader.hash == expectedLastUnsafeHash);
+    // The node with !UNSAFE_EMIT_META_EARLY should not have streamed the meta
+    // for the latest ledger (or the latest ledger before the corrupt one) yet.
+    REQUIRE(lcmsSafe.size() == lcms.size() - 1);
+    REQUIRE(lcmsSafe.back().v0().ledgerHeader.hash == expectedLastSafeHash);
+    REQUIRE(lcmsSafe ==
+            std::vector<LedgerCloseMeta>(lcms.begin(), lcms.end() - 1));
 }
 
 TEST_CASE("LedgerCloseMetaStream file descriptor - REPLAY_IN_MEMORY",
