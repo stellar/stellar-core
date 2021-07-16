0ff16d2529f47c2157bb1e92d20f1521f7c762ad
Siddharth Suresh <siddharth@stellar.org>
2021-07-15 17:50:39 -0700

    CAP-0038 - Make the allowTrust test helper accept alphanum12 assets

--- src/test/TxTests.cpp
+++ src/test/TxTests.cpp
@@ -595,8 +595,18 @@ allowTrust(PublicKey const& trustor, Asset const& asset, uint32_t authorize)
 
     op.body.type(ALLOW_TRUST);
     op.body.allowTrustOp().trustor = trustor;
-    op.body.allowTrustOp().asset.type(ASSET_TYPE_CREDIT_ALPHANUM4);
-    op.body.allowTrustOp().asset.assetCode4() = asset.alphaNum4().assetCode;
+    op.body.allowTrustOp().asset.type(asset.type());
+
+    if (asset.type() == ASSET_TYPE_CREDIT_ALPHANUM4)
+    {
+        op.body.allowTrustOp().asset.assetCode4() = asset.alphaNum4().assetCode;
+    }
+    else
+    {
+        op.body.allowTrustOp().asset.assetCode12() =
+            asset.alphaNum12().assetCode;
+    }
+
     op.body.allowTrustOp().authorize = authorize;
 
     return op;
