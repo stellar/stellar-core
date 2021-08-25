faeb8a33315af82aff3c5ea36527af6f27cb8e04
Siddharth Suresh <siddharth@stellar.org>
2021-08-25 11:26:29 -0700

    CAP-0038 - Change existing OperationIDs to HashIDPreImage

--- src/transactions/CreateClaimableBalanceOpFrame.cpp
+++ src/transactions/CreateClaimableBalanceOpFrame.cpp
@@ -298,12 +298,12 @@ CreateClaimableBalanceOpFrame::insertLedgerKeysToPrefetch(
 Hash
 CreateClaimableBalanceOpFrame::getBalanceID()
 {
-    OperationID operationID;
-    operationID.type(ENVELOPE_TYPE_OP_ID);
-    operationID.id().sourceAccount = mParentTx.getSourceID();
-    operationID.id().seqNum = mParentTx.getSeqNum();
-    operationID.id().opNum = mOpIndex;
+    HashIDPreimage hashPreimage;
+    hashPreimage.type(ENVELOPE_TYPE_OP_ID);
+    hashPreimage.operationID().sourceAccount = mParentTx.getSourceID();
+    hashPreimage.operationID().seqNum = mParentTx.getSeqNum();
+    hashPreimage.operationID().opNum = mOpIndex;
 
-    return xdrSha256(operationID);
+    return xdrSha256(hashPreimage);
 }
 }
