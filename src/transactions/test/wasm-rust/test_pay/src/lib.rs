6a46e32eb81615f48b0619e509b4c2a2d7f86fda
Graydon Hoare <graydon@pobox.com>
2022-04-11 17:13:18 -0700

    Remvoe src/transactions/test/wasm-rust since it moved to its own repo

--- src/transactions/test/wasm-rust/test_pay/src/lib.rs
+++
@@ -1,8 +0,0 @@
-#![no_std]
-use sdk::Val;
-use stellar_contract_sdk as sdk;
-
-#[no_mangle]
-pub fn pay(src: Val, dst: Val, asset: Val, amount: Val) -> Val {
-    sdk::pay(src, dst, asset, amount)
-}
