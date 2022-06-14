890e9375f1793a085012ad21818b3880713a55c6
Jonathan Jove <jon@stellar.org>
2022-06-14 14:12:40 -0500

    Implement InvokeHostFunctionOpFrame

---
+++ src/transactions/InvokeHostFunctionOpFrame.h
@@ -0,0 +1,41 @@
+#pragma once
+
+// Copyright 2022 Stellar Development Foundation and contributors. Licensed
+// under the Apache License, Version 2.0. See the COPYING file at the root
+// of this distribution or at http://www.apache.org/licenses/LICENSE-2.0
+
+#include "transactions/OperationFrame.h"
+
+namespace stellar
+{
+class AbstractLedgerTxn;
+
+class InvokeHostFunctionOpFrame : public OperationFrame
+{
+    InvokeHostFunctionResult&
+    innerResult()
+    {
+        return mResult.tr().invokeHostFunctionResult();
+    }
+
+    InvokeHostFunctionOp const& mInvokeHostFunction;
+
+  public:
+    InvokeHostFunctionOpFrame(Operation const& op, OperationResult& res,
+                              TransactionFrame& parentTx);
+
+    bool isOpSupported(LedgerHeader const& header) const override;
+
+    bool doApply(AbstractLedgerTxn& ltx) override;
+    bool doCheckValid(uint32_t ledgerVersion) override;
+
+    void
+    insertLedgerKeysToPrefetch(UnorderedSet<LedgerKey>& keys) const override;
+
+    static InvokeHostFunctionResultCode
+    getInnerCode(OperationResult const& res)
+    {
+        return res.tr().invokeHostFunctionResult().code();
+    }
+};
+}
--- src/transactions/OperationFrame.cpp
+++ src/transactions/OperationFrame.cpp
@@ -15,6 +15,7 @@
 #include "transactions/CreatePassiveSellOfferOpFrame.h"
 #include "transactions/EndSponsoringFutureReservesOpFrame.h"
 #include "transactions/InflationOpFrame.h"
+#include "transactions/InvokeHostFunctionOpFrame.h"
 #include "transactions/LiquidityPoolDepositOpFrame.h"
 #include "transactions/LiquidityPoolWithdrawOpFrame.h"
 #include "transactions/ManageBuyOfferOpFrame.h"
@@ -113,6 +114,8 @@ OperationFrame::makeHelper(Operation const& op, OperationResult& res,
         return std::make_shared<LiquidityPoolDepositOpFrame>(op, res, tx);
     case LIQUIDITY_POOL_WITHDRAW:
         return std::make_shared<LiquidityPoolWithdrawOpFrame>(op, res, tx);
+    case INVOKE_HOST_FUNCTION:
+        return std::make_shared<InvokeHostFunctionOpFrame>(op, res, tx);
     default:
         ostringstream err;
         err << "Unknown Tx type: " << op.body.type();
