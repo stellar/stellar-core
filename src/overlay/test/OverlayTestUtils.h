0b4c1640f1a030e83a4ea2e27089b4197d8e0cca
marta-lokhova <marta@stellar.org>
2023-04-05 17:28:22 -0700

    Refactor: move useful overlay testing functions to OverlayTestUtils

--- src/overlay/test/OverlayTestUtils.h
+++ src/overlay/test/OverlayTestUtils.h
@@ -4,6 +4,7 @@
 // under the Apache License, Version 2.0. See the COPYING file at the root
 // of this distribution or at http://www.apache.org/licenses/LICENSE-2.0
 
+#include "overlay/PeerManager.h"
 #include <memory>
 #include <string>
 
@@ -11,6 +12,7 @@ namespace stellar
 {
 
 class Application;
+class Simulation;
 
 namespace overlaytestutils
 {
@@ -23,7 +25,21 @@ uint64_t getUnfulfilledDemandCount(std::shared_ptr<Application> app);
 uint64_t getUnknownDemandCount(std::shared_ptr<Application> app);
 
 uint64_t getSentDemandCount(std::shared_ptr<Application> app);
+
 uint64_t getOverlayFloodMessageCount(std::shared_ptr<Application> app,
                                      std::string const& name);
+
+bool knowsAs(Application& knowingApp, Application& knownApp, PeerType peerType);
+
+bool doesNotKnow(Application& knowingApp, Application& knownApp);
+
+bool knowsAsInbound(Application& knowingApp, Application& knownApp);
+
+bool knowsAsOutbound(Application& knowingApp, Application& knownApp);
+
+int numberOfAppConnections(Application& app);
+
+int numberOfSimulationConnections(std::shared_ptr<Simulation> simulation);
+
 }
 }
