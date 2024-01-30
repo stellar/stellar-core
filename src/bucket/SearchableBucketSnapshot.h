06e2d2536ecee7858f7bb525823a775b79275a0d
Garand Tyson <garand@stellar.org>
2024-01-29 16:04:18 -0800

    Added concurrency locks for BucketListDB

--- src/bucket/SearchableBucketSnapshot.h
+++ src/bucket/SearchableBucketSnapshot.h
@@ -36,12 +36,15 @@ class SearchableBucketSnapshot : public NonMovable
                                                 std::streamoff pos,
                                                 size_t pageSize) const;
 
-  public:
+    // Warning: constructor not thread safe
     SearchableBucketSnapshot(std::shared_ptr<Bucket const> const b);
+
+    // Only allow copy constructor, is threadsafe
     SearchableBucketSnapshot(SearchableBucketSnapshot const& b);
     SearchableBucketSnapshot&
     operator=(SearchableBucketSnapshot const& b) = delete;
 
+  public:
     bool isEmpty() const;
     std::shared_ptr<Bucket const> getRawBucket() const;
 
@@ -63,5 +66,7 @@ class SearchableBucketSnapshot : public NonMovable
         AccountID const& accountID, UnorderedSet<LedgerKey>& deadTrustlines,
         UnorderedMap<LedgerKey, LedgerEntry>& liquidityPoolKeyToTrustline,
         LedgerKeySet& liquidityPoolKeys) const;
+
+    friend struct SearchableBucketLevelSnapshot;
 };
 }
--- src/ledger/LedgerManagerImpl.cpp
+++ src/ledger/LedgerManagerImpl.cpp
@@ -497,12 +497,14 @@ LedgerManagerImpl::getDatabase()
 uint32_t
 LedgerManagerImpl::getLastMaxTxSetSize() const
 {
+    std::lock_guard<std::mutex> lock(mLedgerPointerLock);
     return mLastClosedLedger.header.maxTxSetSize;
 }
 
 uint32_t
 LedgerManagerImpl::getLastMaxTxSetSizeOps() const
 {
+    std::lock_guard<std::mutex> lock(mLedgerPointerLock);
     auto n = mLastClosedLedger.header.maxTxSetSize;
     return protocolVersionStartsFrom(mLastClosedLedger.header.ledgerVersion,
                                      ProtocolVersion::V_11)
@@ -554,6 +556,7 @@ LedgerManagerImpl::maxSorobanTransactionResources()
 int64_t
 LedgerManagerImpl::getLastMinBalance(uint32_t ownerCount) const
 {
+    std::lock_guard<std::mutex> lock(mLedgerPointerLock);
     auto const& lh = mLastClosedLedger.header;
     if (protocolVersionIsBefore(lh.ledgerVersion, ProtocolVersion::V_9))
         return (2 + ownerCount) * lh.baseReserve;
@@ -564,18 +567,21 @@ LedgerManagerImpl::getLastMinBalance(uint32_t ownerCount) const
 uint32_t
 LedgerManagerImpl::getLastReserve() const
 {
+    std::lock_guard<std::mutex> lock(mLedgerPointerLock);
     return mLastClosedLedger.header.baseReserve;
 }
 
 uint32_t
 LedgerManagerImpl::getLastTxFee() const
 {
+    std::lock_guard<std::mutex> lock(mLedgerPointerLock);
     return mLastClosedLedger.header.baseFee;
 }
 
 LedgerHeaderHistoryEntry const&
 LedgerManagerImpl::getLastClosedLedgerHeader() const
 {
+    std::lock_guard<std::mutex> lock(mLedgerPointerLock);
     return mLastClosedLedger;
 }
 
@@ -594,12 +600,14 @@ LedgerManagerImpl::getLastClosedLedgerHAS()
 uint32_t
 LedgerManagerImpl::getLastClosedLedgerNum() const
 {
+    std::lock_guard<std::mutex> lock(mLedgerPointerLock);
     return mLastClosedLedger.header.ledgerSeq;
 }
 
 SorobanNetworkConfig&
 LedgerManagerImpl::getSorobanNetworkConfigInternal()
 {
+    std::lock_guard<std::mutex> lock(mLedgerPointerLock);
     releaseAssert(mSorobanNetworkConfig);
     return *mSorobanNetworkConfig;
 }
@@ -1348,6 +1356,7 @@ LedgerManagerImpl::advanceLedgerPointers(LedgerHeader const& header,
                    ledgerAbbrev(header, ledgerHash));
     }
 
+    std::lock_guard<std::mutex> lock(mLedgerPointerLock);
     mLastClosedLedger.hash = ledgerHash;
     mLastClosedLedger.header = header;
 }
