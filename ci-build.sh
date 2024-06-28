5df912150964f2fd57ea1e81f40d0e85f4efa8f7
Graydon Hoare <graydon@pobox.com>
2024-06-27 22:31:38 -0700

    Remove the bors-auto-branch short circuit from ci-build.sh

--- ci-build.sh
+++ ci-build.sh
@@ -68,18 +68,6 @@ NPROCS=$(getconf _NPROCESSORS_ONLN)
 echo "Found $NPROCS processors"
 date
 
-# Short-circuit transient 'auto-initialization' builds
-git fetch origin master
-MASTER=$(git describe --always FETCH_HEAD)
-HEAD=$(git describe --always HEAD)
-echo $MASTER
-echo $HEAD
-if [ $HEAD == $MASTER ]
-then
-    echo "HEAD SHA1 equals master; probably just establishing merge, exiting build early"
-    exit 1
-fi
-
 # Try to ensure we're using the real g++ and clang++ versions we want
 mkdir bin
 
