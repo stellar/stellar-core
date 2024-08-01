513eaa51183999f2c7549ac3aea2f5a235e0eef4
Graydon Hoare <graydon@pobox.com>
2024-07-31 21:04:43 -0700

    Update ci-build.sh

--- ci-build.sh
+++ ci-build.sh
@@ -119,8 +119,8 @@ export CCACHE_CPP2=true
 # periodically check to see if caches are old and purge them if so
 if [ -d "$CCACHE_DIR" ] ; then
     if [ -n "$(find $CCACHE_DIR -mtime +$CACHE_MAX_DAYS -print -quit)" ] ; then
-        echo Purging old cache dirs $CCACHE_DIR ./target
-        rm -rf $CCACHE_DIR ./target
+        echo Purging old cache dirs $CCACHE_DIR ./target $HOME/.cargo/registry $HOME/.cargo/git
+        rm -rf $CCACHE_DIR ./target $HOME/.cargo/registry $HOME/.cargo/git
     fi
 fi
 
