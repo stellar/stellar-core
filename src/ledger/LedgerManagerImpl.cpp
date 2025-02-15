48fec107081ab5ee4c59e946066f222d6107624a
Graydon Hoare <graydon@pobox.com>
2025-02-14 19:45:43 -0800

    Add some tracy zones

--- src/ledger/LedgerManagerImpl.cpp
+++ src/ledger/LedgerManagerImpl.cpp
@@ -2082,6 +2082,7 @@ void
 LedgerManagerImpl::ApplyState::evictFromModuleCache(
     uint32_t ledgerVersion, EvictedStateVectors const& evictedState)
 {
+    ZoneScoped;
     std::vector<Hash> keys;
     for (auto const& key : evictedState.deletedKeys)
     {
@@ -2116,6 +2117,7 @@ void
 LedgerManagerImpl::ApplyState::addAnyContractsToModuleCache(
     uint32_t ledgerVersion, std::vector<LedgerEntry> const& le)
 {
+    ZoneScoped;
     for (auto const& e : le)
     {
         if (e.data.type() == CONTRACT_CODE)
