60accc8ae63747d987d024ef1547725cd0698347
Dmytro Kozhevin <dmytro@stellar.org>
2025-07-21 20:47:28 -0400

    Settings upgrade utility improvements:
    - Allow specifying the additional resource fee in order to cover the higher resource fee without rebuilding the Core
    - Add Futurenet support to the upgrade script

--- src/main/SettingsUpgradeUtils.h
+++ src/main/SettingsUpgradeUtils.h
@@ -6,18 +6,22 @@ namespace stellar
 {
 
 std::pair<TransactionEnvelope, LedgerKey>
-getWasmRestoreTx(PublicKey const& publicKey, SequenceNumber seqNum);
+getWasmRestoreTx(PublicKey const& publicKey, SequenceNumber seqNum,
+                 int64_t addResourceFee);
 
 std::pair<TransactionEnvelope, LedgerKey>
-getUploadTx(PublicKey const& publicKey, SequenceNumber seqNum);
+getUploadTx(PublicKey const& publicKey, SequenceNumber seqNum,
+            int64_t addResourceFee);
 
 std::tuple<TransactionEnvelope, LedgerKey, Hash>
 getCreateTx(PublicKey const& publicKey, LedgerKey const& contractCodeLedgerKey,
-            std::string const& networkPassphrase, SequenceNumber seqNum);
+            std::string const& networkPassphrase, SequenceNumber seqNum,
+            int64_t addResourceFee);
 
 std::pair<TransactionEnvelope, ConfigUpgradeSetKey>
 getInvokeTx(PublicKey const& publicKey, LedgerKey const& contractCodeLedgerKey,
             LedgerKey const& contractSourceRefLedgerKey, Hash const& contractID,
-            ConfigUpgradeSet const& upgradeSet, SequenceNumber seqNum);
+            ConfigUpgradeSet const& upgradeSet, SequenceNumber seqNum,
+            int64_t addResourceFee);
 
 }
