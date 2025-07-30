7d91c8a2afcf9b242dc7cc41bea608ffbe2e8c76
Dmytro Kozhevin <dmytro@stellar.org>
2025-07-30 10:45:24 -0400

    Parallel tx set building test optimization

--- src/test/TxTests.cpp
+++ src/test/TxTests.cpp
@@ -2041,6 +2041,7 @@ isSuccessResult(TransactionResult const& res)
 TestAccount
 getGenesisAccount(Application& app, uint32_t accountIndex)
 {
+    REQUIRE(accountIndex < app.getConfig().GENESIS_TEST_ACCOUNT_COUNT);
     return TestAccount(
         app, getAccount("TestAccount-" + std::to_string(accountIndex)));
 }
