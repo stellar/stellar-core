9a0cb443f629a0af7782c5d309d26394001de2fc
Daniel Rebelsky <4641927+drebelsky@users.noreply.github.com>
2025-08-04 12:48:45 -0700

    Change reportError to be a macro

--- src/overlay/ReportError.h
+++ src/overlay/ReportError.h
@@ -1,26 +1,22 @@
 #pragma once
 
-#include <string>
-#include <string_view>
-
 #include "util/Logging.h"
 
-namespace stellar
-{
-
-// Function to call when something has gone wrong. Logs an error (errorMessage,
-// errorExtra...). In testing, also throw an exception. Otherwise, add a note to
-// report the bug.
-template <typename... Ts>
-void
-reportError(std::string_view exceptionMessage, std::string_view errorMessage,
-            Ts... errorExtra)
-{
-    CLOG_ERROR(Overlay, errorMessage, errorExtra...);
+// Function-like macro to call when something has gone wrong. Logs an error
+// (__VA_ARGS__). In testing, also throw an exception. Otherwise, add a
+// note to report the bug.
 #ifdef BUILD_TESTS
-    throw std::runtime_error(std::string{exceptionMessage});
+#define reportError(exceptionMessage, ...) \
+    do \
+    { \
+        CLOG_ERROR(Overlay, __VA_ARGS__); \
+        throw std::runtime_error(exceptionMessage); \
+    } while (0)
 #else
-    CLOG_ERROR(Overlay, "{}", REPORT_INTERNAL_BUG);
+#define reportError(exceptionMessage, errorMessage, ...) \
+    do \
+    { \
+        CLOG_ERROR(Overlay, __VA_ARGS__); \
+        CLOG_ERROR(Overlay, "{}", REPORT_INTERNAL_BUG); \
+    } while (0)
 #endif
-}
-}
