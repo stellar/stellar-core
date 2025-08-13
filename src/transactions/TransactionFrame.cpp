ce6fc8a5b4a6b81781e579a6c0c4edc9fd9e1547
Daniel Rebelsky <4641927+drebelsky@users.noreply.github.com>
2025-08-13 09:54:45 -0700

    Narrow case for allowed soroban extension on classic TX

--- src/transactions/TransactionFrame.cpp
+++ src/transactions/TransactionFrame.cpp
@@ -1251,8 +1251,9 @@ TransactionFrame::commonValidPreSeqNum(
             if (mEnvelope.type() == ENVELOPE_TYPE_TX &&
                 mEnvelope.v1().tx.ext.v() != 0
 #ifdef BUILD_TESTS
-                // For testing, allow sorobanData extension for loadgen padding
-                && mEnvelope.v1().tx.ext.sorobanData().ext.v() != 1
+                // For loadgen testing, allow sorobanData extension for padding
+                && (mEnvelope.v1().tx.ext.sorobanData().ext.v() != 1 ||
+                    !app.getConfig().ARTIFICIALLY_GENERATE_LOAD_FOR_TESTING)
 #endif
             )
             {
