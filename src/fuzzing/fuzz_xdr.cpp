ff4511a6f11de84d53921893c04dbb1b17a185e6
Reandle <d.aecherli@gmail.com>
2025-08-26 22:03:20 +0200

    Update fuzz_xdr.cpp
    obaque update

--- src/fuzzing/fuzz_xdr.cpp
+++ src/fuzzing/fuzz_xdr.cpp
@@ -5,7 +5,7 @@
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
     try {
         stellar::LedgerEntry entry;
-        xdr::xdr_from_opaque(data, size, entry);
+        xdr::xdr_from_opaque(data, entry); // ohne size, wenn data ein std::vector<uint8_t> ist
     } catch (...) {
         // Ignoriere Parsing-Fehler
     }
