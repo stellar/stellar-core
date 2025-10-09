85490a73771881d9babce525eaef322a78309778
Daniel Rebelsky <4641927+drebelsky@users.noreply.github.com>
2025-10-09 15:57:20 -0700

    Restore old metric name

--- src/herder/TransactionQueue.cpp
+++ src/herder/TransactionQueue.cpp
@@ -1085,9 +1085,9 @@ SorobanTransactionQueue::SorobanTransactionQueue(
         app.getMetrics().NewCounter(
             {"herder", "pending-soroban-txs", "banned"}),
         SimpleTimer<std::chrono::milliseconds>(app.getMetrics(), "herder",
-                                               "pending-txs", ""),
+                                               "pending-soroban-txs", ""),
         SimpleTimer<std::chrono::milliseconds>(app.getMetrics(), "herder",
-                                               "pending-txs", "self-"),
+                                               "pending-soroban-txs", "self-"),
         app.getMetrics().NewCounter(
             {"herder", "pending-soroban-txs", "evicted-due-too-low-fee-count"}),
         app.getMetrics().NewCounter(
