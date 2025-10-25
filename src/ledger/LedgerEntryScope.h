fa9fe12d6d6072b1db39fd908daf88bf9a1d5874
Graydon Hoare <graydon@pobox.com>
2025-10-24 18:56:35 -0700

    Add and use LedgerEntryScope and ScopedLedgerEntry

---
+++ src/ledger/LedgerEntryScope.h
@@ -0,0 +1,210 @@
+#pragma once
+
+// Copyright 2025 Stellar Development Foundation and contributors. Licensed
+// under the Apache License, Version 2.0. See the COPYING file at the root
+// of this distribution or at http://www.apache.org/licenses/LICENSE-2.0
+
+#include "xdr/Stellar-ledger-entries.h"
+#include <cstdint>
+#include <functional>
+#include <iosfwd>
+#include <optional>
+
+#define FOREACH_STATIC_LEDGER_ENTRY_SCOPE(MACRO) \
+    MACRO(GLOBAL_PAR_APPLY_STATE) \
+    MACRO(THREAD_PAR_APPLY_STATE) \
+    MACRO(TX_PAR_APPLY_STATE) \
+    MACRO(LIVE_BUCKET_LIST) \
+    MACRO(HOT_ARCHIVE_BUCKET_LIST) \
+    MACRO(EVICTION_SCAN) \
+    MACRO(LIVE_SOROBAN_IN_MEMORY_STATE)
+
+#define FOREACH_STATIC_LEDGER_ENTRY_SCOPE_INNER(OUTER_SCOPE, MACRO) \
+    MACRO(OUTER_SCOPE, GLOBAL_PAR_APPLY_STATE) \
+    MACRO(OUTER_SCOPE, THREAD_PAR_APPLY_STATE) \
+    MACRO(OUTER_SCOPE, TX_PAR_APPLY_STATE) \
+    MACRO(OUTER_SCOPE, LIVE_BUCKET_LIST) \
+    MACRO(OUTER_SCOPE, HOT_ARCHIVE_BUCKET_LIST) \
+    MACRO(OUTER_SCOPE, EVICTION_SCAN) \
+    MACRO(OUTER_SCOPE, LIVE_SOROBAN_IN_MEMORY_STATE)
+
+namespace stellar
+{
+
+enum class StaticLedgerEntryScope : uint16_t
+{
+#define STATIC_SCOPE_MACRO(SCOPE_NAME) SCOPE_NAME,
+    FOREACH_STATIC_LEDGER_ENTRY_SCOPE(STATIC_SCOPE_MACRO)
+#undef STATIC_SCOPE_MACRO
+};
+
+template <StaticLedgerEntryScope> class LedgerEntryScopeID;
+
+}
+
+template <stellar::StaticLedgerEntryScope T>
+std::ostream& operator<<(std::ostream& os,
+                         stellar::LedgerEntryScopeID<T> const& obj);
+
+namespace stellar
+{
+
+// LedgerEntryScopeID uniquely identifies a scope for ledger entries. It is
+// space-optimized to a single 64 bit word since these are stuck on all
+// ScopedLedgerEntry instances and there might be a lot of those.
+template <StaticLedgerEntryScope S> class LedgerEntryScopeID
+{
+  public:
+    static constexpr StaticLedgerEntryScope staticScope = S;
+    uint16_t const mIndex;
+    uint32_t const mLedger;
+
+    // NB: index is limited to 16 bits to keep size down;
+    // the constructor will throw if you exceed that, but
+    // in practice scopes should always be tiny numbers like
+    // the thread cluster index or the bucketlist level.
+    LedgerEntryScopeID(size_t index, uint32_t ledger);
+
+    bool operator==(LedgerEntryScopeID const& other) const;
+    bool operator!=(LedgerEntryScopeID const& other) const;
+
+    friend std::ostream& ::operator<<(std::ostream& os,
+                                      LedgerEntryScopeID const& obj);
+};
+
+static_assert(
+    sizeof(
+        LedgerEntryScopeID<StaticLedgerEntryScope::GLOBAL_PAR_APPLY_STATE>) ==
+        8,
+    "Unexpected size for LedgerEntryScopeID");
+
+template <StaticLedgerEntryScope T> class LedgerEntryScope;
+
+template <StaticLedgerEntryScope S> class ScopedLedgerEntry
+{
+    static constexpr StaticLedgerEntryScope staticScope = S;
+    using ScopeIdT = LedgerEntryScopeID<S>;
+    using ScopeT = LedgerEntryScope<S>;
+    LedgerEntry mEntry;
+
+    friend class LedgerEntryScope<S>;
+#define FRIEND_MACRO(OUTER_SCOPE, INNER_SCOPE) \
+    friend class LedgerEntryScope<StaticLedgerEntryScope::INNER_SCOPE>;
+    FOREACH_STATIC_LEDGER_ENTRY_SCOPE_INNER(S, FRIEND_MACRO)
+#undef FRIEND_MACRO
+
+    ScopedLedgerEntry(ScopeIdT scopeID, LedgerEntry const& entry);
+    ScopedLedgerEntry(ScopeIdT scopeID, LedgerEntry&& entry);
+
+  public:
+    LedgerEntryScopeID<S> const mScopeID;
+
+    ScopedLedgerEntry(ScopedLedgerEntry<S> const& other);
+    ScopedLedgerEntry(ScopedLedgerEntry<S>&& other);
+    ScopedLedgerEntry<S>& operator=(ScopedLedgerEntry<S> const& other);
+    ScopedLedgerEntry<S>& operator=(ScopedLedgerEntry<S>&& other);
+
+    LedgerKey key() const;
+
+    LedgerEntry const& read_in_scope(LedgerEntryScope<S> const& scope) const;
+    LedgerEntry& modify_in_scope(LedgerEntryScope<S> const& scope);
+
+    bool operator==(ScopedLedgerEntry const& other) const;
+    bool operator<(ScopedLedgerEntry const& other) const;
+};
+
+using GlobalLedgerEntry =
+    ScopedLedgerEntry<StaticLedgerEntryScope::GLOBAL_PAR_APPLY_STATE>;
+using ThreadLedgerEntry =
+    ScopedLedgerEntry<StaticLedgerEntryScope::THREAD_PAR_APPLY_STATE>;
+using TxLedgerEntry =
+    ScopedLedgerEntry<StaticLedgerEntryScope::TX_PAR_APPLY_STATE>;
+
+template <StaticLedgerEntryScope S> class LedgerEntryScope
+{
+    mutable bool mActive{true};
+
+#define FRIEND_MACRO(OUTER_SCOPE, INNER_SCOPE) \
+    friend class LedgerEntryScope<StaticLedgerEntryScope::INNER_SCOPE>;
+    FOREACH_STATIC_LEDGER_ENTRY_SCOPE_INNER(S, FRIEND_MACRO)
+#undef FRIEND_MACRO
+
+  public:
+    static constexpr StaticLedgerEntryScope staticScope = S;
+    using ScopeIdT = LedgerEntryScopeID<S>;
+    using EntryT = ScopedLedgerEntry<S>;
+    LedgerEntryScopeID<S> const mScopeID;
+
+    LedgerEntryScope(ScopeIdT scopeID);
+
+    // These two methods are marked const even though technically they modify
+    // the mActive bit; this is to allow usage patterns where the scope is
+    // logically immutable (e.g. a const ref to a scope passed into a function)
+    // but we want to go a step further and dynamically deactivate _reading_
+    // from it to prevent accidental access to stale data.
+    void scope_activate() const;
+    void scope_deactivate() const;
+
+    LedgerEntry const& scope_read_entry(EntryT const& w) const;
+    LedgerEntry& scope_modify_entry(EntryT& w) const;
+
+    EntryT scope_adopt_entry(LedgerEntry&& entry) const;
+    EntryT scope_adopt_entry(LedgerEntry const& entry) const;
+    std::optional<EntryT>
+    scope_adopt_entry(std::optional<LedgerEntry> const& entry) const;
+
+    template <StaticLedgerEntryScope OtherScope>
+    EntryT
+    scope_adopt_entry_from(ScopedLedgerEntry<OtherScope> const& entry,
+                           LedgerEntryScope<OtherScope> const& scope) const;
+
+    template <StaticLedgerEntryScope OtherScope>
+    std::optional<EntryT> scope_adopt_entry_from(
+        std::optional<ScopedLedgerEntry<OtherScope>> const& entry,
+        LedgerEntryScope<OtherScope> const& scope) const;
+};
+
+template <StaticLedgerEntryScope S> class DeactivateScopeGuard
+{
+    LedgerEntryScope<S> const& mScope;
+
+  public:
+    DeactivateScopeGuard(LedgerEntryScope<S> const& scope);
+    ~DeactivateScopeGuard();
+};
+
+#define ADOPT_OTHER_SCOPE_METHODS(OUTER_SCOPE, INNER_SCOPE) \
+    extern template ScopedLedgerEntry<StaticLedgerEntryScope::OUTER_SCOPE> \
+    LedgerEntryScope<StaticLedgerEntryScope::OUTER_SCOPE>:: \
+        scope_adopt_entry_from<StaticLedgerEntryScope::INNER_SCOPE>( \
+            ScopedLedgerEntry<StaticLedgerEntryScope::INNER_SCOPE> const&, \
+            LedgerEntryScope<StaticLedgerEntryScope::INNER_SCOPE> const& \
+                scope) const; \
+\
+    extern template std::optional< \
+        ScopedLedgerEntry<StaticLedgerEntryScope::OUTER_SCOPE>> \
+    LedgerEntryScope<StaticLedgerEntryScope::OUTER_SCOPE>:: \
+        scope_adopt_entry_from<StaticLedgerEntryScope::INNER_SCOPE>( \
+            std::optional<ScopedLedgerEntry< \
+                StaticLedgerEntryScope::INNER_SCOPE>> const&, \
+            LedgerEntryScope<StaticLedgerEntryScope::INNER_SCOPE> const& \
+                scope) const;
+
+#define STATIC_SCOPE_MACRO(OUTER_SCOPE) \
+    extern template class LedgerEntryScopeID< \
+        StaticLedgerEntryScope::OUTER_SCOPE>; \
+    extern template class LedgerEntryScope< \
+        StaticLedgerEntryScope::OUTER_SCOPE>; \
+    extern template class ScopedLedgerEntry< \
+        StaticLedgerEntryScope::OUTER_SCOPE>; \
+    extern template class DeactivateScopeGuard< \
+        StaticLedgerEntryScope::OUTER_SCOPE>; \
+    FOREACH_STATIC_LEDGER_ENTRY_SCOPE_INNER(OUTER_SCOPE, \
+                                            ADOPT_OTHER_SCOPE_METHODS)
+
+FOREACH_STATIC_LEDGER_ENTRY_SCOPE(STATIC_SCOPE_MACRO)
+
+#undef STATIC_SCOPE_MACRO
+#undef ADOPT_OTHER_SCOPE_METHODS
+
+}
