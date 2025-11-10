d7530f43fa7671f04ad81802fd7b2533d45ef8b6
Daniel Rebelsky <4641927+drebelsky@users.noreply.github.com>
2025-11-10 13:29:38 -0800

    Log autorestore event as xdr hex

--- src/transactions/InvokeHostFunctionOpFrame.cpp
+++ src/transactions/InvokeHostFunctionOpFrame.cpp
@@ -792,7 +792,7 @@ class InvokeHostFunctionApplyHelper : virtual LedgerAccessHelper
                 CLOG_INFO(
                     Ledger,
                     "Event Reconciliation - autorestore event, Entry = {}",
-                    xdrToCerealString(events.back(), "event"));
+                    binToHex(xdr::xdr_to_opaque(events.back())));
             }
 
             std::move(success.events.begin(), success.events.end(),
