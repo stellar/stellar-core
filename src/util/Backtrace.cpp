65c25f7fb33af9913dc8ebeb3b5073c50373bfb6
Graydon Hoare <graydon@pobox.com>
2025-11-25 00:57:24 -0800

    make libunwind configury a little better

--- src/util/Backtrace.cpp
+++ src/util/Backtrace.cpp
@@ -3,6 +3,7 @@
 // of this distribution or at http://www.apache.org/licenses/LICENSE-2.0
 
 #include "util/Backtrace.h"
+#include "config.h"
 #include "util/GlobalChecks.h"
 #include <cstdio>
 #include <cstdlib>
