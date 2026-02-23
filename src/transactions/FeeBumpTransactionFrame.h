77ec41901f6ebf15a40daf68a1905c8e9123d43a
marta-lokhova <marta@stellar.org>
2026-02-23 14:09:02 -0800

    UnorderedSet --> std::set for more intuitive reporting

--- src/transactions/FeeBumpTransactionFrame.h
+++ src/transactions/FeeBumpTransactionFrame.h
@@ -128,7 +128,7 @@ class FeeBumpTransactionFrame : public TransactionFrameBase
     bool validateSorobanTxForFlooding(
         UnorderedSet<LedgerKey> const& keysToFilter) const override;
     bool validateAccountFilterForFlooding(
-        UnorderedSet<AccountID> const& filteredAccounts) const override;
+        std::set<AccountID> const& filteredAccounts) const override;
     bool validateSorobanMemo() const override;
     bool validateHostFn() const override;
 
