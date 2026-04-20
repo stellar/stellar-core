eb661ec6168204a3e7edd4c254acc4a0f8145846
dmkozh <dmytro@stellar.org>
2026-04-20 12:29:27 -0400

    Reapply "Reapply "perf: replace InMemoryBucketEntry virtual set with unordered_map""
    This reverts commit a0cfe2a533e8487addc36088bf4e245eb6957716.

--- src/bucket/InMemoryIndex.h
+++ src/bucket/InMemoryIndex.h
@@ -9,150 +9,31 @@
 #include "xdr/Stellar-ledger-entries.h"
 
 #include "ledger/LedgerHashUtils.h"
-#include <unordered_set>
+#include <unordered_map>
 
 namespace stellar
 {
 
 class SHA256;
 
-// LedgerKey sizes usually dominate LedgerEntry size, so we don't want to
-// store a key-value map to be memory efficient. Instead, we store a set of
-// InternalInMemoryBucketEntry objects, which is a wrapper around either a
-// LedgerKey or cached BucketEntry. This allows us to use std::unordered_set to
-// efficiently store cache entries, but allows lookup by key only.
-// Note that C++20 allows heterogeneous lookup in unordered_set, so we can
-// simplify this class once we upgrade.
-class InternalInMemoryBucketEntry
-{
-  private:
-    struct AbstractEntry
-    {
-        virtual ~AbstractEntry() = default;
-        virtual LedgerKey copyKey() const = 0;
-        virtual size_t hash() const = 0;
-        virtual IndexPtrT const& get() const = 0;
-
-        virtual bool
-        operator==(AbstractEntry const& other) const
-        {
-            return copyKey() == other.copyKey();
-        }
-    };
-
-    // "Value" entry type used for storing BucketEntry in cache
-    struct ValueEntry : public AbstractEntry
-    {
-      private:
-        IndexPtrT entry;
-
-      public:
-        ValueEntry(IndexPtrT entry) : entry(entry)
-        {
-        }
-
-        LedgerKey
-        copyKey() const override
-        {
-            return getBucketLedgerKey(*entry);
-        }
-
-        size_t
-        hash() const override
-        {
-            return std::hash<LedgerKey>{}(getBucketLedgerKey(*entry));
-        }
-
-        IndexPtrT const&
-        get() const override
-        {
-            return entry;
-        }
-    };
-
-    // "Key" entry type only used for querying the cache
-    struct QueryKey : public AbstractEntry
-    {
-      private:
-        LedgerKey ledgerKey;
-
-      public:
-        QueryKey(LedgerKey const& ledgerKey) : ledgerKey(ledgerKey)
-        {
-        }
-
-        LedgerKey
-        copyKey() const override
-        {
-            return ledgerKey;
-        }
-
-        size_t
-        hash() const override
-        {
-            return std::hash<LedgerKey>{}(ledgerKey);
-        }
-
-        IndexPtrT const&
-        get() const override
-        {
-            throw std::runtime_error("Called get() on QueryKey");
-        }
-    };
-
-    std::unique_ptr<AbstractEntry> impl;
-
-  public:
-    InternalInMemoryBucketEntry(IndexPtrT entry)
-        : impl(std::make_unique<ValueEntry>(entry))
-    {
-    }
-
-    InternalInMemoryBucketEntry(LedgerKey const& ledgerKey)
-        : impl(std::make_unique<QueryKey>(ledgerKey))
-    {
-    }
-
-    size_t
-    hash() const
-    {
-        return impl->hash();
-    }
-
-    bool
-    operator==(InternalInMemoryBucketEntry const& other) const
-    {
-        return impl->operator==(*other.impl);
-    }
-
-    IndexPtrT const&
-    get() const
-    {
-        return impl->get();
-    }
-};
-
-struct InternalInMemoryBucketEntryHash
-{
-    size_t
-    operator()(InternalInMemoryBucketEntry const& entry) const
-    {
-        return entry.hash();
-    }
-};
-
 // For small Buckets, we can cache all contents in memory. Because we cache all
 // entries, the index is just as large as the Bucket itself, so we never persist
 // this index type. It is always recreated on startup.
+//
+// Uses an unordered_map<LedgerKey, IndexPtrT> for O(1) lookups without
+// virtual dispatch or heap allocation per query. The LedgerKey is stored
+// separately from the BucketEntry, trading a small amount of memory for
+// significantly faster lookups (no heap allocation per find(), no virtual
+// dispatch for hash/equality).
 class InMemoryBucketState : public NonMovableOrCopyable
 {
-    using InMemorySet = std::unordered_set<InternalInMemoryBucketEntry,
-                                           InternalInMemoryBucketEntryHash>;
+    using InMemoryMap =
+        std::unordered_map<LedgerKey, IndexPtrT, std::hash<LedgerKey>>;
 
-    InMemorySet mEntries;
+    InMemoryMap mEntries;
 
   public:
-    using IterT = InMemorySet::const_iterator;
+    using IterT = InMemoryMap::const_iterator;
 
     // Insert a LedgerEntry (INIT/LIVE) into the cache.
     void insert(BucketEntry const& be);
@@ -175,11 +56,7 @@ class InMemoryBucketState : public NonMovableOrCopyable
     }
 
 #ifdef BUILD_TESTS
-    bool
-    operator==(InMemoryBucketState const& in) const
-    {
-        return mEntries == in.mEntries;
-    }
+    bool operator==(InMemoryBucketState const& in) const;
 #endif
 };
 
