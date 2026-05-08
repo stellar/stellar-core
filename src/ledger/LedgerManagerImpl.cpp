4547a54ffaf8641af53ce866c6feb232c6aa384b
Graydon Hoare <graydon@pobox.com>
2026-05-07 22:51:12 -0700

    Minor build fixes

--- src/ledger/LedgerManagerImpl.cpp
+++ src/ledger/LedgerManagerImpl.cpp
@@ -2410,7 +2410,7 @@ LedgerManagerImpl::updateCanonicalStateForTesting(LedgerHeader const& header)
     mApp.getCommandHandler().addSnapshot(state);
 }
 #endif
-}
+//}
 
 std::vector<MutableTxResultPtr>
 LedgerManagerImpl::processFeesSeqNums(
