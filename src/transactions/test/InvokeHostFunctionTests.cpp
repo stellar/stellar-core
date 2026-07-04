273c2c784cbd8dbe19022da53e6532b6d4a7c7c8
Graydon Hoare <graydon@pobox.com>
2026-07-03 21:22:50 -0700

    enable p28 submodule in makefile and main build as well

--- src/transactions/test/InvokeHostFunctionTests.cpp
+++ src/transactions/test/InvokeHostFunctionTests.cpp
@@ -7745,7 +7745,7 @@ TEST_CASE("Module cache across protocol versions", "[tx][soroban][modulecache]")
     // work-in-progress next host, in which case there _is_ a separate module
     // cache and the following line of code should be commented-out.
     //
-    moduleCacheProtocolCount -= 1;
+    // moduleCacheProtocolCount -= 1;
 #endif
     REQUIRE(app->getLedgerManager()
                 .getSorobanMetrics()
