42ac1e18e1027d60aa500c661b33e5ad4a274276
Graydon Hoare <graydon@pobox.com>
2026-08-18 22:57:31 -0700

    Fix fastdev divergence bug

--- src/rust/src/soroban_fuzz.rs
+++ src/rust/src/soroban_fuzz.rs
@@ -21,18 +21,18 @@
 use crate::rust_bridge::FuzzResultCode;
 
 // Stub implementation when testutils feature is disabled
-#[cfg(not(feature = "testutils"))]
+#[cfg(not(feature = "fuzz_targets"))]
 pub fn run_soroban_fuzz_target(_name: &str, _data: &[u8]) -> FuzzResultCode {
     FuzzResultCode::FUZZ_DISABLED
 }
 
 // When testutils feature is enabled, import and use the actual implementations
-#[cfg(feature = "testutils")]
+#[cfg(feature = "fuzz_targets")]
 use soroban_fuzz_targets::{self as fuzz, FuzzResult};
 
 /// Run a Soroban fuzz target with the given input bytes.
 /// Panics on internal errors (which is what the fuzzer wants to find!)
-#[cfg(feature = "testutils")]
+#[cfg(feature = "fuzz_targets")]
 pub fn run_soroban_fuzz_target(name: &str, data: &[u8]) -> FuzzResultCode {
     let result = match name {
         "soroban_expr" => fuzz::expr::run_fuzz_target(data),
